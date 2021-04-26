82c6a244e870137b166d951537030ca9c9703f0c
JohnJTrump <77506001+JohnJTrump@users.noreply.github.com>
2021-04-26 14:24:31 -0700

    Table header

--- AppBuilder9000/Theaters_and_Features/urls.py
+++ AppBuilder9000/Theaters_and_Features/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.Theater_home, name='Theater_home'),
     path('add/', views.new_Theater, name="new_Theater"),
     path('home/', views.Theater_home, name="Theater_home"),
     path('find/', views.find_Theater, name="find_Theater"),
--- AppBuilder9000/Theaters_and_Features/views.py
+++ AppBuilder9000/Theaters_and_Features/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 from .forms import TheaterForm
 
 # Create your views here.
+from .models import Theaters
+
+
 def Theater_home(request):
     return render(request, 'Theaters_and_Features/Theaters_and_Features_home.html')
 
@@ -19,6 +22,8 @@ def new_Theater(request):
 
 
 def find_Theater(request):
+    all_theaters = Theaters.objects.all()
+    content = {'all_theaters': all_theaters}
     return render(request, 'Theaters_and_Features/Theaters_and_Features_find.html')
 
 
