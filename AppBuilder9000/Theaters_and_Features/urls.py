3e7ec5232598db348e231c55a6a1e9c98512f7b4
JohnJTrump <77506001+JohnJTrump@users.noreply.github.com>
2021-04-28 22:37:18 -0700

    Edit page start

--- AppBuilder9000/Theaters_and_Features/urls.py
+++ AppBuilder9000/Theaters_and_Features/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('home/', views.Theater_home, name="Theater_home"),
     path('add/', views.new_Theater, name="new_Theater"),
     path('find/', views.find_Theater, name="find_Theater"),
     path('detail/<int:pk>', views.Theater_details, name='Theater_details'),
+    path('edit/<int:pk>', views.edit_details, name='edit_details'),
 ]
 
