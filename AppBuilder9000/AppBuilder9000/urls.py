33f763be2aaa82d6be498a35e771558d9d84051f
tenchamp31 <ygandhi191@gmail.com>
2021-04-29 18:54:11 -0400

    Story 1

--- AppBuilder9000/AppBuilder9000/urls.py
+++ AppBuilder9000/AppBuilder9000/urls.py
@@ -32,6 +32,7 @@ urlpatterns = [
     path('StockApp/', include('StockApp.urls')),
     path('Theaters_and_Features/', include('Theaters_and_Features.urls')),
     path('TheAnimeApp/', include('TheAnimeApp.urls')),
+    path('TravelDestinations/', include('TravelDestinations.urls'))
 ]
 
 #urlpatterns +=staticfiles_urlpatterns()
--- AppBuilder9000/TravelDestinations/views.py
+++ AppBuilder9000/TravelDestinations/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def TravelDestinationshome(request):
+    context = {}
+    return render(request, 'TravelDestinations/TravelDestinations_home.html', context)
