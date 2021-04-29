c44206a9f9e68740bbbc400fb4ef5e741459adf0
ashtn15kimsey <ashtn15@gmail.com>
2021-04-29 16:02:45 -0700

    creating model

--- AppBuilder9000/trackApp/models.py
+++ AppBuilder9000/trackApp/models.py
@@ -1,2 +1,22 @@
+from django.db import models
 
+TRACK_ME = [
+    ('startTracking', 'startTracking'),
+    ('dontTrack', 'dontTrack'),
+]
+CHOICES = [
+    ('yes', 'yes'),
+    ('no', 'no'),
+]
 
+class TrackApp(models.Model):
+    location = models.CharField(max_length=50, choices=TRACK_ME)
+
+class CHOICES(models.Model):
+    YES = models.CharField(max_length=20)
+    NO = models.CharField(max_length=20)
+
+Object  = models.Manager()
+
+def __str__(self):
+    return self.TrackMe
--- AppBuilder9000/trackApp/views.py
+++ AppBuilder9000/trackApp/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
+from .models import TrackForm
+from .models import TrackApp
 
 
 
 def TrackApp_home(request):
     return render(request,"TrackApp\TrackApp_home.html")
+
+def TrackApp_Add(request):
+    if request.method == 'POST':
+        form = TrackAppForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('TrackApp_Add')
+    else:
+        form = TrackAppForm(request.POST)
+        return render(request, 'TrackApp/TrackApp_Add.html',
+                      {'form': form})
+
+
 
 
 
