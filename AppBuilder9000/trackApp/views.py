3d81f9f2f2311440a32d242961061af78da2afa7
ashtn15kimsey <ashtn15@gmail.com>
2021-05-03 15:09:32 -0700

    creating a model

--- AppBuilder9000/trackApp/views.py
+++ AppBuilder9000/trackApp/views.py
@@ -8,6 +8,7 @@ from .forms import User
 
 
 
+
 def TrackApp_home(request):
     return render(request,"TrackApp\TrackApp_home.html")
 
@@ -22,15 +23,13 @@ def TrackApp_Add(request):
         return render(request, 'TrackApp/TrackApp_Add.html',
                       {'form': form})
 
-def TrackApp_create(request):
-    if request.method == 'POST':
-        form = createForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('TrackApp_create')
-    else:
-        form = createForm(request.POST)
-        return render(request, 'TrackApp/TrackApp_create.html')
+
+
+
+
+
+
+
 
 
 
