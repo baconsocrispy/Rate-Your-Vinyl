6026cc3645cab1ab27ce6a23146dc1057bb2a412
ashtn15kimsey <ashtn15@gmail.com>
2021-05-03 14:45:56 -0700

    creating a model

--- AppBuilder9000/trackApp/urls.py
+++ AppBuilder9000/trackApp/urls.py
@@ -3,13 +3,11 @@ from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path
 from . import views
-from .models import Location
-from .models import create
+
+
 
 urlpatterns = [
     path('', views.TrackApp_home, name='TrackApp_home'),
-    path('',views.TrackApp_Add, name='TrackApp_Add'),
-    path('',views.TrackApp_create, name='TrackApp_create'),
-    path('', views.TrackApp_nav, name='TrackApp_nav')
-
+    path('TrackApp_Add',views.TrackApp_Add, name='TrackApp_Add'),
+    path('TrackApp_display', views.TrackApp_display, name='TrackApp_display')
 ]
