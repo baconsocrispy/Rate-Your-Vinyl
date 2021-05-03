928e1b9f3bdabbe415bccf70c92ad8fa2013e9d9
DeepakBandhu <deepakbandhu12@gmail.com>
2021-05-03 15:36:39 -0700

    done (re edited)

--- AppBuilder9000/CharacterApp/urls.py
+++ AppBuilder9000/CharacterApp/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.Character_home, name="Character_home"),
     path('add', views.Character_add, name="Character_add"),
     path('list_all', views.Character_list, name="Character_list"),
-    path('detail_all', views.Character_details, name="Character_details"),
+    path('show_all', views.Character_link, name="Character_link"),
+    path('Character_details/<int:pk>/', views.Character_details, name="Character_details"),
 ]
