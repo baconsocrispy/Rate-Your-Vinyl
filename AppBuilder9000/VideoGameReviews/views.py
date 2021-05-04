783b7c6b0575cb16d4800860acf2e118b5080e7e
Carlos-Briceno <73920679+Carlos-Briceno@users.noreply.github.com>
2021-05-03 18:04:48 -0600

    Created my model and updated it

--- AppBuilder9000/VideoGameReviews/views.py
+++ AppBuilder9000/VideoGameReviews/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from .models import VideoReviews
+from .forms import VideoReviewsForm
 
 
 def videogamereviews(request):
     content = {}
     return render(request, 'VideoGameReviews/VideoGamesReviews_Home.html', content)
+
+
+def videoreviews(request):
+    context = {}
+    all_reviews = videoreviews().objects.all
+    return render(request, 'VideoGameReviews/VideoGamesReviews_Reviews.html', {'all': all_reviews}, context)
