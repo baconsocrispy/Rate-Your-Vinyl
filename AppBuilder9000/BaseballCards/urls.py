fed2f1e4fe79e631acc1907a01ba1d837b393e70
timothydimasi <timothydimasi@gmail.com>
2021-05-04 17:24:12 -0700

    created new HTML template for the catalog that has a dynamic table that will populate from the database. added some basic bootstrap styling. created a views function that allows for this and renders the page.

--- AppBuilder9000/BaseballCards/urls.py
+++ AppBuilder9000/BaseballCards/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.cards_home, name='BaseballCards_home'),
     path('add/', views.add_card, name='BaseballCards_add'),
+    path('catalog/', views.catalog, name='BaseballCards_catalog'),
 ]
--- AppBuilder9000/BaseballCards/views.py
+++ AppBuilder9000/BaseballCards/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import BaseballCardForm
+from .models import BaseballCard
+
 
 def cards_home(request):
     return render(request, 'BaseballCards/BaseballCards_home.html')
+
 
 def add_card(request):
     form = BaseballCardForm(data=request.POST or None)
@@ -12,3 +15,10 @@ def add_card(request):
             return redirect('BaseballCards_home')
     content = {'form': form}
     return render(request, 'BaseballCards/BaseballCards_add.html', content)
+
+
+def catalog(request):
+    cards = BaseballCard.BaseballCards.all()
+    content = {'cards': cards}
+    return render(request, 'BaseballCards/BaseballCards_catalog.html', content)
+
