9e1f2030d2b1607c3706152ab1c3021acf1e6711
Carlos-Briceno <73920679+Carlos-Briceno@users.noreply.github.com>
2021-05-06 17:51:13 -0600

    Created my model and updated it

--- AppBuilder9000/VideoGameReviews/forms.py
+++ AppBuilder9000/VideoGameReviews/forms.py
@@ -6,3 +6,8 @@ class VideoReviewsForm(forms.ModelForm):
     class Meta:
         model = VideoReviews
         fields = ['GameName', 'GameGenre', 'GameReview', 'GameRating']
+        widgets = {
+            'GameName': forms.TextInput(attrs={'class': 'form-control'}),
+            'GameGenre': forms.TextInput(attrs={'class': 'form-control'}),
+            'GameReview': forms.Textarea(attrs={'class': 'form-control'})
+        }
--- AppBuilder9000/VideoGameReviews/models.py
+++ AppBuilder9000/VideoGameReviews/models.py
@@ -5,19 +5,21 @@ class VideoReviews(models.Model):
     GameName = models.CharField(max_length=60)
     GameGenre = models.CharField(max_length=60)
     GameReview = models.TextField(max_length=700)
-    GameRating = models.IntegerField(default=0)
+    RATING_CHOICES = [
+        (1, 1),
+        (2, 2),
+        (3, 3),
+        (4, 4),
+        (5, 5),
+        (6, 6),
+        (7, 7),
+        (8, 8),
+        (9, 9),
+        (10, 10),
+    ]
+    GameRating = models.IntegerField(default=0, choices=RATING_CHOICES)
 
     objects = models.Manager()
 
-    RATING_CHOICES = (
-        ("1", "1"),
-        ("2", "2"),
-        ("3", "3"),
-        ("4", "4"),
-        ("5", "5"),
-        ("6", "6"),
-        ("7", "7"),
-        ("8", "8"),
-        ("9", "9"),
-        ("10", "10"),
-    )
+    def __str__(self):
+        return self.GameName
