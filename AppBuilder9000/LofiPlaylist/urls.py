7813bbe7de822a9a13e9e82a6d64e3454e308e87
pjamsa <Paul_Jamsa@msn.com>
2021-05-12 12:54:45 -0700

    Basic Display build for App Updated CSS, New Display HTML

--- AppBuilder9000/LofiPlaylist/urls.py
+++ AppBuilder9000/LofiPlaylist/urls.py
@@ -3,6 +3,7 @@ from .import views
 
 urlpatterns = [
     path('', views.lofi_home, name="lofi_home"),
-    path('lofi_add/', views.lofi_add, name="lofi_add")
+    path('lofi_add/', views.lofi_add, name="lofi_add"),
+    path('lofi_display/', views.lofi_display, name="lofi_display")
 ]
 
