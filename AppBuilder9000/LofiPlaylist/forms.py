223c209cc4d136d98757d791bfe74ed3a1a75a7d
pjamsa <Paul_Jamsa@msn.com>
2021-05-19 16:50:17 -0700

    Connected to API
    updated url, and view function

--- AppBuilder9000/LofiPlaylist/forms.py
+++ AppBuilder9000/LofiPlaylist/forms.py
@@ -1,9 +1,19 @@
 from django.forms import ModelForm, TextInput
 from .models import Song
+from django import forms
 
 
 class SongForm(ModelForm):
     class Meta:
         model = Song
         fields = '__all__'
+
+#API
+
+class SearchForm(forms.Form):
+   artist = forms.CharField(max_length=100)
+   title = forms.CharField(max_length=100)
+
+
+
 
