819b7c0f6f592afce4a0bbf3e374d8b9b9a6611a
Kaia F <kaialforrester@gmail.com>
2021-05-28 15:41:56 -0700

    Story #4: With some help I was able to get the details page to work in coordination with the user display page.

--- AppBuilder9000/ArtGallery/urls.py
+++ AppBuilder9000/ArtGallery/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='ArtGallery_home'),
     path('ArtGallery_login/', views.apply, name="ArtGallery_login"),
     path('ArtGallery_users/', views.users, name="ArtGallery_users"),
+    path('ArtGallery_details/<int:pk>/', views.user_details, name="ArtGallery_details"),
 ]
