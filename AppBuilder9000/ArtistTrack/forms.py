203a7d900693961caf0a5945494e54d8fb46f73e
StevenFralix <steveaf7@gmail.com>
2021-06-01 16:12:58 -0700

    create models, add pages for adding songs and playlists to database,

---
+++ AppBuilder9000/ArtistTrack/forms.py
@@ -0,0 +1,14 @@
+from django.forms import ModelForm
+from .models import Song, Playlist
+
+
+class SongForm(ModelForm):
+    class Meta:
+        model = Song
+        fields = '__all__'
+
+
+class PlaylistForm(ModelForm):
+    class Meta:
+        model = Playlist
+        fields = '__all__'
--- AppBuilder9000/ArtistTrack/views.py
+++ AppBuilder9000/ArtistTrack/views.py
@@ -1,6 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from . forms import SongForm, PlaylistForm
 
 
 # Create your views here.
 def at_home(request):
     return render(request, 'ArtistTrack_home.html')
+
+
+def add_song(request):
+    form = SongForm(data=request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return redirect('add_song')
+    content = {'form': form}
+    return render(request, 'ArtistTrack_addSong.html', content)
+
+
+def add_playlist(request):
+    form = PlaylistForm(data=request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return redirect('add_playlist')
+    content = {'form': form}
+    return render(request, 'ArtistTrack_addPlaylist.html', content)
