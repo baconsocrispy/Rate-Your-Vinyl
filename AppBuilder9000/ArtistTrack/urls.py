5d00dba2a4e8115bbe95de83d7440aeb70aad1a0
StevenFralix <steveaf7@gmail.com>
2021-06-10 14:09:08 -0700

    added all songs page, populates with all songs from the and all available database information about that song. updated navbar, urls and views accordingly.

--- AppBuilder9000/ArtistTrack/urls.py
+++ AppBuilder9000/ArtistTrack/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('add_song', views.add_song, name='add_song'),
     path('add_playlist', views.add_playlist, name='add_playlist'),
     path('library', views.at_library, name='at_library'),
+    path('at_all_songs', views.at_all_songs, name='at_all_songs'),
     path('<int:pk>/details', views.at_details, name='at_details'),
     path('<int:pk>/delete', views.at_delete, name='at_delete'),
     path('<int:pk>/playlist_details', views.at_playlist_details, name='at_playlist_details'),
