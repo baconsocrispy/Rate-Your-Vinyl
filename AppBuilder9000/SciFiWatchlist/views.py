58131ea02c4257efa212ed4c4f623f34116330f1
Brittany Barnett <barnett.brittanym@gmail.com>
2021-06-16 14:53:39 -0400

    Create the Edit and Delete function

--- AppBuilder9000/SciFiWatchlist/views.py
+++ AppBuilder9000/SciFiWatchlist/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, redirect, get_object_or_404
 from .forms import MovieForm
 from .models import Movie
+from django.shortcuts import render, redirect, get_object_or_404, HttpResponseRedirect
 
 
 def SciFihome(request):
@@ -12,10 +12,12 @@ def AddMovies(request):
         if form.is_valid():
             form.save()
             return redirect('Add-Movies')
+    else:
+        form=MovieForm()
     return render(request, 'SciFiWatchlist/SciFiWatchlist_add.html', {'form': form})
 
 def ListMovies(request):
-    Movies = Movie.objects.all()
+    Movies = Movie.objects.all().order_by("FilmName")
     return render(request,"SciFiWatchlist/SciFiWatchlist_movielist.html", {'Movies': Movies})
 
 def MovieDetails(request, id):
@@ -27,3 +29,19 @@ def MovieDetails(request, id):
         'Directors': Movies.DirectorName,
         'Summary': Movies.MovieSummary,
     })
+
+def EditMovies(request, id):
+    Movies = get_object_or_404(Movie, id=id)
+    form = MovieForm(request.POST or None, instance=Movies)
+    if form.is_valid():
+        form.save()
+        return HttpResponseRedirect('/'+ 'SciFiWatchlist//'+ id)
+    return render(request, "SciFiWatchlist/SciFiWatchlist_Update.html", {"form": form })
+
+def DeleteMovies(request, id):
+    Movies = get_object_or_404(Movie, id=id)
+    form = MovieForm(request.POST or None, instance=Movies)
+    if request.method == "POST":
+        Movies.delete()
+        return HttpResponseRedirect('/'+ 'SciFiWatchlist/List-Movies/')
+    return render(request, "SciFiWatchlist/SciFiWatchlist_Delete.html", {"form": form })
