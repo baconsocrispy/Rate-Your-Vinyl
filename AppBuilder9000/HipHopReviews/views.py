91dd7055b5f4d75b28dbf3a87bdb32ea3cf9be26
npranno <77467876+npranno@users.noreply.github.com>
2021-06-21 10:24:43 -0700

    Create new model with object manager, along with a form for user inputs. New template for model. Add views function to render and save new item to database. Minor updates to styling

--- AppBuilder9000/HipHopReviews/views.py
+++ AppBuilder9000/HipHopReviews/views.py
@@ -1,4 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ReviewsForm
 
 def home(request):
     return render(request, 'HipHopReviews_home.html')
+
+def request(request):
+    form = ReviewsForm(request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return redirect("HipHopReviews_home")
+    return render(request, 'HipHopReviews_request.html', {'form': form})
+
+def reviews(request):
+    return render(request, 'HipHopReviews_reviews.html')
+
+
