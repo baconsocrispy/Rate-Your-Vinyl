8b0d90eab7f484930790d5270ec700b5e9039071
Jeremy Munshower <jdmunshower@gmail.com>
2021-06-22 15:04:29 -0700

    Story 2 add models, form

--- AppBuilder9000/StarWarsUniverse/models.py
+++ AppBuilder9000/StarWarsUniverse/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+WHICH_AFFILIATION = {
+    ('light', 'light'),
+    ('dark', 'dark'),
+    ('neutral', 'neutral')
+}
+
+
+class Character(models.Model):
+    Name_of_Character = models.CharField(max_length=100)
+    First_Appearance = models.CharField(max_length=60, default='', blank=True)
+    Race_Type = models.CharField(max_length=60, default='', blank=True)
+    Affiliation = models.CharField(max_length=60, default='neutral', choices=WHICH_AFFILIATION)
+    Additional_Details = models.TextField(blank=True)
+
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.Name_of_Character
