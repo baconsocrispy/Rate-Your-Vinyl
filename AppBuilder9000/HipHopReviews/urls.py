d80a68db3b62d908dc768c3b5a1060e0f4209b7f
npranno <77467876+npranno@users.noreply.github.com>
2021-06-22 12:57:52 -0700

    Create new template html to display items saved in the database. Add function to display items in database and send to new template. Minor styling changes

--- AppBuilder9000/HipHopReviews/urls.py
+++ AppBuilder9000/HipHopReviews/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('home', views.home, name='HipHopReviews_home'),
     path('request', views.request, name='HipHopReviews_request'),
     path('reviews', views.reviews, name='HipHopReviews_reviews'),
+
 ]
