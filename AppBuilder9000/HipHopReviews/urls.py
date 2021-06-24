9cdcfd5ae8a58880e0d39942fc17e96fe31e63c4
npranno <77467876+npranno@users.noreply.github.com>
2021-06-24 12:13:48 -0700

    Create details page (error)

--- AppBuilder9000/HipHopReviews/urls.py
+++ AppBuilder9000/HipHopReviews/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('home', views.home, name='HipHopReviews_home'),
     path('request', views.request, name='HipHopReviews_request'),
     path('reviews', views.reviews, name='HipHopReviews_reviews'),
-
+    path('artist_details/<int:pk>/', views.artist_details, name='HipHopReviews_details'),
 ]
