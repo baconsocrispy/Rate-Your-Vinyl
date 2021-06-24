bb6896177f4153194800da65776fc2be2fbef4e6
Joshua Ramos <veliciquent@gmail.com>
2021-06-24 04:35:47 -0600

    Display All Items from Database

--- AppBuilder9000/Quotes/urls.py
+++ AppBuilder9000/Quotes/urls.py
@@ -5,5 +5,9 @@ from .import views
 urlpatterns = [
     path('', views.home, name='Quotes_home'),
     path('Quotes_add/', views.Quotes_add, name="Quotes_add"),
+    path('Quotes_display/', views.Quotes_display, name="Quotes_display"),
+    path('Quotes_details/<int:pk>/', views.Quotes_details, name="Quotes_details"),
+    path('Quotes_edit/<int:pk>/', views.Quotes_edit, name="Quotes_edit"),
+    path('Quotes_validate/<int:pk>/', views.Quotes_validate, name="Quotes_validate"),
 
 ]
