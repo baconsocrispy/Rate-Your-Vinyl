f53cb1118bb02140dfc5ab674903d1daa57131a0
Joshua Ramos <veliciquent@gmail.com>
2021-06-24 04:46:28 -0600

    Display All Items from Database

--- AppBuilder9000/Quotes/urls.py
+++ AppBuilder9000/Quotes/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     path('', views.home, name='Quotes_home'),
     path('Quotes_add/', views.Quotes_add, name="Quotes_add"),
     path('Quotes_display/', views.Quotes_display, name="Quotes_display"),
-    path('Quotes_details/<int:pk>/', views.Quotes_details, name="Quotes_details"),
 ]
