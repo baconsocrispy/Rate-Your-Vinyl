63b7399958e2e4ed6ba8ec0f28047d144633bac5
npranno <77467876+npranno@users.noreply.github.com>
2021-06-25 12:48:04 -0700

    Create edit/delete buttons (error)

--- AppBuilder9000/HipHopReviews/urls.py
+++ AppBuilder9000/HipHopReviews/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('request', views.request, name='HipHopReviews_request'),
     path('reviews', views.reviews, name='HipHopReviews_reviews'),
     path('artist_details/<int:pk>/', views.artist_details, name='HipHopReviews_details'),
+    path('edit/<int:pk>/', views.edit, name='HipHopReviews_edit'),
+    path('delete/<int:pk>', views.delete, name='HipHopReviews_delete'),
 ]
