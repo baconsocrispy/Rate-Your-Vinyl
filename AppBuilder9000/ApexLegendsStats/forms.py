10ea876f18316304448bd38b0106461577c17949
ZShick <zshick@pdx.edu>
2021-06-27 21:42:10 -0700

    Finished polishing Story 2. My idea is that people would use the site more often on their cell phone. I checked the database and it is recording the values that get posted. Used Crispy Forms to get it looking a bit better.

--- AppBuilder9000/ApexLegendsStats/forms.py
+++ AppBuilder9000/ApexLegendsStats/forms.py
@@ -1,8 +1,10 @@
 import crispy_forms
 from django.forms import ModelForm
-from .models import Entry
+from .models import Entry, LEGEND_CHOICES
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit, Layout, Row, Column
 
 class EntryForm(ModelForm):
     class Meta:
         model = Entry
-        fields = ['name', 'legend', 'gp', 'kills', 'tpt3']
+        fields = "__all__"
