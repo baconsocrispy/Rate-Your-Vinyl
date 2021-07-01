90164f919dc5586de69985911de889820c789d9d
ZShick <zshick@pdx.edu>
2021-06-30 23:40:36 -0700

    Finished with the functionality and some basic styling for Story 4! It works but I may want to change up how I collect data. In a perfect world I'd like to display all the details that share a name on the same page rather than how it currently works (like two seperate "Holly" pages depending on the legend). Happy with the functionality though!

--- AppBuilder9000/ApexLegendsStats/views.py
+++ AppBuilder9000/ApexLegendsStats/views.py
@@ -18,5 +18,15 @@ def EnterStats(request):
     return render(request, 'ApexLegendsStats/ApexLegendsStats_enterStats.html', {'form': form})
 
 def ViewStats(request):
-    statsList = Entry.objects.all()
+    statsList = Entry.objects.all().order_by("name")
     return render(request, 'ApexLegendsStats/ApexLegendsStats_viewStats.html', {'statsList': statsList})
+
+def StatsDetails(request, id):
+    Details = get_object_or_404(Entry, id=id)
+    return render(request, 'ApexLegendsStats/ApexLegendsStats_statsDetails.html', {
+        'Name': Details.name,
+        'Legend': Details.legend,
+        'Games_Played': Details.gp,
+        'Kills': Details.kills,
+        'Times_Placed_Top_3': Details.tpt3,
+    })
