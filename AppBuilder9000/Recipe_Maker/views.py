16eefaeafe4a8c78e2dacc78302b53fe7c6ac201
caboosecodes <ricardolopez413@hotmail.com>
2021-07-05 17:34:38 -0700

    Add home function to render Recipe_maker_home.html

--- AppBuilder9000/Recipe_Maker/views.py
+++ AppBuilder9000/Recipe_Maker/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+# first instruction on what to do if "home" get invoked
+def home(request):
+    # renders in the browser
+    return render(request, 'Recipe_Maker/Recipe_Maker_home.html')
