c4da8a95679cd5e68e46d71b2369bcd5ff67c50a
Chris <79873408+govtchris@users.noreply.github.com>
2021-07-06 17:40:37 -0500

    Update Model

--- AppBuilder9000/ZooAnimals/models.py
+++ AppBuilder9000/ZooAnimals/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+###Creating the choice for SSP
+SSP_Choices = (
+    (True, 'Yes'),
+    (False, 'No')
+)
+
+###Creating the location choices
+Location = (
+    ('Africa', 'Africa'),
+    ('The Arctics', 'The Arctics'),
+    ('Asia', 'Asia'),
+    ('Australia', 'Australia'),
+    ('Europe','Europe'),
+    ('Education', 'Education'),
+    ('North America', 'North America'),
+    ('South America', 'South America'),
+    ('World Oceans', 'World Oceans'),
+)
+
+class Species(models.Model):
+    commonName = models.CharField(max_length=120, default="", blank=True, null=False, verbose_name='Common Name')
+    latinName = models.CharField(max_length=120, default="", blank=True, null=False, verbose_name='Latin Name')
+    location = models.CharField(max_length=60, choices= Location)
+    SSP = models.CharField(max_length=60, choices= SSP_Choices)
+
+    object= models.Manager
+
+    def __str__(self):
+        return self.commonName
+
--- AppBuilder9000/ZooAnimals/views.py
+++ AppBuilder9000/ZooAnimals/views.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render, get_object_or_404, HttpResponse
+from .forms import speciesForm
 
 def zoo_home(request):
     return render(request, 'zoo_home.html')
+
+def zoo_add(request):
+    return render(request, 'zoo_add.html')
+
+def add_species(request):
+    form = speciesForm(data=request.Post or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return redirect('zoo_add')
+    content = {'form': form}
+    return render(request, 'ZooAnimals/zoo_add.html', content)
+
