fca4fc37b39aeeceb38d9cab22e8270310a0a9e2
caboosecodes <ricardolopez413@hotmail.com>
2021-07-06 14:07:29 -0700

    Create TYPE_CHOICES dictionary, and models RecipeName, Ingredients, and Description

--- AppBuilder9000/Recipe_Maker/models.py
+++ AppBuilder9000/Recipe_Maker/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+# Dictionary
+TYPE_CHOICES = {
+    ("Appetizers", "Appetizers"),
+    ("Entrees", "Entrees"),
+    ("Drinks", "Drinks"),
+    ("Snacks", "Snacks"),
+}
+
+
+# When a recipe is created these are the fields of info that are filled out
+class RecipeName(models.Model):
+    recipe_type = models.CharField(max_length=60, choices=TYPE_CHOICES)
+    # blank=False does not allow for the forms on the webpage to be left blank
+    # null=False does not allow for the it to be blank in the database
+    recipe_name = models.CharField(max_length=100, default="", blank=False, null=False)
+
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.recipe_name
+
+
+class Ingredients(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    recipe_ingredients = models.CharField(max_length=100, default="", blank=True)
+
+    def __str__(self):
+        return self.recipe_ingredients
+
+
+class Description(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    recipe_description = models.TextField()
+
+    def __str__(self):
+        return self.recipe_description
