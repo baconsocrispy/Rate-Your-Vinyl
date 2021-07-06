13b084a57d60af211f68a7488c077658e1b4ce4a
caboosecodes <ricardolopez413@hotmail.com>
2021-07-06 14:40:21 -0700

    register models in admin

--- AppBuilder9000/Recipe_Maker/admin.py
+++ AppBuilder9000/Recipe_Maker/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import RecipeName, Ingredients, Description
+
+
+# inlines allow models to be edited on the same page as the parent model
+class IngredientsInline(admin.TabularInline):
+    model = Ingredients
+    extra = 5
+
+
+class DescriptionInline(admin.TabularInline):
+    model = Description
+
+
+# add the inlines to the model
+class RecipeAdmin(admin.ModelAdmin):
+    inlines = [
+        IngredientsInline,
+        DescriptionInline,
+    ]
+
+
+admin.site.register(RecipeName, RecipeAdmin)
