e9c0e536ea1e90f9587acdf6bf0106c42799826f
caboosecodes <ricardolopez413@hotmail.com>
2021-07-05 17:56:07 -0700

    rename home function to recipe_home

--- AppBuilder9000/Recipe_Maker/views.py
+++ AppBuilder9000/Recipe_Maker/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 
 
 # first instruction on what to do if "home" get invoked
-def home(request):
+def recipe_home(request):
     # renders in the browser
     return render(request, 'Recipe_Maker/Recipe_Maker_home.html')
