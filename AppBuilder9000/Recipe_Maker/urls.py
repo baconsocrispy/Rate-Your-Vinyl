9742e5acd9851a2174308bf6d754ab0f66693d21
caboosecodes <ricardolopez413@hotmail.com>
2021-07-07 12:48:47 -0700

    add create_recipe view

--- AppBuilder9000/Recipe_Maker/urls.py
+++ AppBuilder9000/Recipe_Maker/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     # imported recipe_home function from views
     path('', views.recipe_home, name='Recipe_Maker'),
+    path('create_recipe/', views.create_recipe, name='create_recipe')
 ]
