c8a60e57fae06b7a1ded6c6e1bdaa1ff4d07f130
caboosecodes <ricardolopez413@hotmail.com>
2021-07-07 10:43:54 -0700

    comment out old code; import Recipe, Ingredients, and Instructions models

--- AppBuilder9000/Recipe_Maker/forms.py
+++ AppBuilder9000/Recipe_Maker/forms.py
@@ -1,4 +1,34 @@
 from django.forms import ModelForm
+from .models import Recipe, Ingredients, Instructions
+
+
+class RecipeForm(ModelForm):
+    class Meta:
+        model = Recipe
+        # instead of writing out all the fields from the model, use '__all__'
+        # grabs all the fields from within Recipe
+        # passes it into the form
+        fields = '__all__'
+
+
+class IngredientsForm(ModelForm):
+    class Meta:
+        model = Ingredients
+        fields = '__all__'
+
+
+class Instructions(ModelForm):
+    class Meta:
+        model = Instructions
+        fields = '__all__'
+
+
+
+
+
+"""
+ old code to experiment with later
+
 # instead of grabbing a class from model.py, its grabbing from admin.py
 # class RecipeAdmin has models Ingredients and Instructions inline
 from .admin import RecipeAdmin
@@ -11,3 +41,4 @@ class RecipeForm(ModelForm):
         # grabs all the fields from within RecipeAdmin
         # passes it into the form
         fields = '__all__'
+"""
