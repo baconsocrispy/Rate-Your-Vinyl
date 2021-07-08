4c246006b4b5b4d73ca01bec347e6a1deef9ceb8
caboosecodes <ricardolopez413@hotmail.com>
2021-07-08 12:50:26 -0700

    update path

--- AppBuilder9000/Recipe_Maker/urls.py
+++ AppBuilder9000/Recipe_Maker/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     # imported recipe_home function from views
     path('', views.recipe_home, name='Recipe_Maker'),
     path('create_recipe/', views.create_recipe, name='create_recipe'),
-    path('recipes/', views.RecipeListView.as_view(), name='recipes'),
+    path('recipes_display/', views.RecipeListView.as_view(), name='recipes_display'),
 ]
