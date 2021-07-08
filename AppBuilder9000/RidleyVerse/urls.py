f1ecf0cd6d3fea88e65f8d2e4ec90aaef82a7ba0
Joshua Watson <watson0117@gmail.com>
2021-07-08 11:56:13 -0600

    step #5 update. Functional

--- AppBuilder9000/RidleyVerse/urls.py
+++ AppBuilder9000/RidleyVerse/urls.py
@@ -21,4 +21,6 @@ urlpatterns = [
     path('Add-Movies/', views.AddMovies, name='Add-Movies'),
     path('List-Movies/', views.ListMovies, name='List-Movies'),
     path('<id>', views.MovieDetails, name='Movie-Details'),
+    path('<id>/update/', views.EditMovies, name='Update'),
+    path('<id>/delete/', views.DeleteMovies, name='Delete')
     ]
