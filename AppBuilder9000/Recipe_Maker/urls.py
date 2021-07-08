17148121713db248daeaa9beea9fe9b3675260fa
caboosecodes <ricardolopez413@hotmail.com>
2021-07-08 10:55:03 -0700

    rewrite url path to display RecipeListView

--- AppBuilder9000/Recipe_Maker/urls.py
+++ AppBuilder9000/Recipe_Maker/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
+from .views import RecipeListView
 
 urlpatterns = [
     # imported recipe_home function from views
     path('', views.recipe_home, name='Recipe_Maker'),
     path('create_recipe/', views.create_recipe, name='create_recipe'),
-    path('display_recipe', views.display_recipe, name='display_recipe'),
+    path('display_recipes', RecipeListView.as_view(), name='display_recipes'),
 ]
