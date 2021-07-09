27b3b6e54b389ad0df0a9054bb8f3cda7c80976e
caboosecodes <ricardolopez413@hotmail.com>
2021-07-08 18:20:47 -0700

    rewrite path to list_recipes view

--- AppBuilder9000/Recipe_Maker/urls.py
+++ AppBuilder9000/Recipe_Maker/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views
-from .views import RecipeListView
+
 
 urlpatterns = [
     # imported recipe_home function from views
     path('', views.recipe_home, name='Recipe_Maker'),
     path('create_recipe/', views.create_recipe, name='create_recipe'),
-    path('recipes_display/', views.RecipeListView.as_view(), name='recipes_display'),
+    path('list_recipes/', views.list_recipes, name='list_recipes'),
 ]
