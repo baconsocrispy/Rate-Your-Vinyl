cb489c23a704ffb802880c599527dbce06a50a8e
caboosecodes <ricardolopez413@hotmail.com>
2021-07-12 12:01:55 -0700

    write view to display details of items in database

--- AppBuilder9000/Recipe_Maker/views.py
+++ AppBuilder9000/Recipe_Maker/views.py
@@ -10,7 +10,7 @@ def recipe_home(request):
     # renders in the browser
     return render(request, 'Recipe_Maker/Recipe_Maker_home.html')
 
-
+# creates a new entry in the database
 def create_recipe(request):
     recipe_form = RecipeForm(request.POST or None)  # gets information from the form
 
@@ -32,7 +32,7 @@ def create_recipe(request):
     # returns the user to the create webpage with the dictionary
     return render(request, 'Recipe_Maker/Recipe_Maker_create.html', context)
 
-
+# lists out items in the database
 def list_recipes(request):
     # old code to display items in database
     # recipe_list = Recipe.objects.all()
@@ -43,3 +43,20 @@ def list_recipes(request):
     recipes = p.get_page(page)
 
     return render(request, 'Recipe_Maker/Recipe_Maker_display.html', {'recipes': recipes})
+
+
+# function to display a detailed view of items
+def details(request, pk):
+    # info from a url is in a string so it needs to be converted to integer
+    pk = int(pk)
+    item = get_object_or_404(Recipe, pk=pk)
+    form = RecipeForm(data=request.POST or None, instance=item)
+    if request.method == 'POST':
+        if form.is_valid():
+            form2 = form.save(commit=False)
+            form2.save()
+            return redirect(form.errors)
+        else:
+            print(form.errors)
+    else:
+        return render(request, 'Recipe_Maker/Recipe_Maker_details.html', {'form': form}))
