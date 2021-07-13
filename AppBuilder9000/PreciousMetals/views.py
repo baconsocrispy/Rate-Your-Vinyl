46e1f12e282964a29041627e422b4bc7a20ff94f
joelmindset <82834522+joelmindset@users.noreply.github.com>
2021-07-13 12:51:51 -0600

    add inventory html

--- AppBuilder9000/PreciousMetals/views.py
+++ AppBuilder9000/PreciousMetals/views.py
@@ -5,9 +5,14 @@ from .forms import MetalForm
 
 # Create your views here.
 def home(request):
+    return render(request, 'PreciousMetals_home.html', {})
+
+def addMetalItem(request):
     form = MetalForm(data=request.POST or None)
-    if request.method == 'POST':
+        if form.is_valid():
+            from.save()
+            return redirect
         pk = request.POST['PreciousMetalsItem']
         return PreciousMetalsItem(request, pk)
-    content = {'form': form}
-    return render(request, 'PreciousMetals_home.html', content)
+        content = {'form': form}
+        return render(request, 'PreciousMetals_home.html', content)
