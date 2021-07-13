55a3654a2ab31ea606bee51decd3e87546b7cd96
Neil Javier <portlandmmaguy@gmail.com>
2021-07-13 10:27:33 -0700

    fixed 'template syntax' and 'no reverse match' errors

--- AppBuilder9000/Blogs/urls.py
+++ AppBuilder9000/Blogs/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.blogs, name='blog'),
+    path('', views.blogs, name='blogs'),
     path('entry/', views.new_entry, name='entry'),
 ]
