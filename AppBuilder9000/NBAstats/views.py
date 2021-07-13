881d2ffc48acca856c5ea7da9e11fd3c45676756
ericlemiere <83970546+ericlemiere@users.noreply.github.com>
2021-07-13 12:50:47 -0700

    Story2 complete for NBA Stats app

--- AppBuilder9000/NBAstats/views.py
+++ AppBuilder9000/NBAstats/views.py
@@ -1,6 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Defensive_Stats
+from .forms import Def_Stats_Form
 
 
 # Create your views here.
 def nba_home(request):
-    return render(request, "nba-home.html")
+    form = Def_Stats_Form(data=request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return redirect('stats-home')
+    content = {'form': form}
+    return render(request, 'nba-home.html', content)
+
+
+def add_player(request):
+    form = Def_Stats_Form(data=request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return redirect('stats-home')
+    content = {'form': form}
+    return render(request, 'nba-home.html', content)
