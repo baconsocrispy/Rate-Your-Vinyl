88e8e9be14bcb68987016e9f5259c39b0b23fba5
Neil Javier <portlandmmaguy@gmail.com>
2021-07-13 10:15:32 -0700

    erased copied code and rewrote my own

--- AppBuilder9000/Blogs/urls.py
+++ AppBuilder9000/Blogs/urls.py
@@ -1,19 +1,7 @@
-# from django.urls import path, include
-# from Blogs import views
-#
-# urlpatterns = [
-#     path('', views.blogs_home, name='blogs_home'),
-# ]
-#
-from requests import request
-
-from . import views
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='blogs_home'),
-    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
-    # i dont know why this doesnt need a path. maybe it does?
-    # all i know is as_view is a view as a class but i dont know what that means
-    path('', views.new_post(request), name='blogs_new'),
+    path('', views.blogs, name='blog'),
+    path('entry/', views.new_entry, name='entry'),
 ]
