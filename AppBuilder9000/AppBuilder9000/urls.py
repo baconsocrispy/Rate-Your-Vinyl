1ff250f53151ed4b64568fa1e6208f7bc9f5aab0
Sean Hager <smhager87@gmail.com>
2021-07-13 22:44:46 -0400

    committing base app for Story #1

--- AppBuilder9000/AppBuilder9000/urls.py
+++ AppBuilder9000/AppBuilder9000/urls.py
@@ -34,6 +34,7 @@ urlpatterns = [
     path('Blogs/', include('Blogs.urls')),
     path('NBAstats/', include('NBAstats.urls')),
     path('PreciousMetals/', include('PreciousMetals.urls')),
+    path('ControlInventory/', include('ControlInventory.urls'))
 ]
 
 #urlpatterns +=staticfiles_urlpatterns()
