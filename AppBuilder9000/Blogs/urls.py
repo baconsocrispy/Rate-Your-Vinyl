62f1f38262effd2ad6cfd66b50217c16f2a0aa18
Neil Javier <portlandmmaguy@gmail.com>
2021-07-14 14:52:31 -0700

    started from scratch again.

--- AppBuilder9000/Blogs/urls.py
+++ AppBuilder9000/Blogs/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.blogs, name='blogs'),
-    path('entry/', views.new_entry, name='entry'),
+    path('', views.index, name='blogs'),
+    path('Add/', views.Add, name='EntryPage'),
+    path('Display/', views.Display, name='DisplayPage'),
+    path('postdetails/<int:pk>/Details/', views.post_details, name='DetailsPage'),
+    path('editpost/<int:pk>/Edit/', views.edit_post, name='EditPage'),
+    path('deletepost/<int:pk>/Delete/', views.delete_post, name='DeletePage'),
 ]
