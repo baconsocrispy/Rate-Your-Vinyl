4e0283ea15caaa13f6c83fb72f4014973a244a1a
joelmindset <82834522+joelmindset@users.noreply.github.com>
2021-07-15 16:59:06 -0600

    Edit button edit.html edit_item view add url

--- AppBuilder9000/PreciousMetals/urls.py
+++ AppBuilder9000/PreciousMetals/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('home/', views.home, name='PreciousMetals_home'),
     path('inventory/', views.inventory, name='PreciousMetals_inventory'),
-    path('listing/', views.listItems, name='PreciousMetals_listing'),
-    path('<id>', views.itemDetails, name='PreciousMetals_details'),
+    path('listing/', views.list_items, name='PreciousMetals_listing'),
+    path('<id>', views.item_details, name='PreciousMetals_details'),
+    path('edit/<int:id>/', views.edit_item, name='edit_item'),
 ]
