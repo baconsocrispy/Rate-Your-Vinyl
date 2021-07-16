8669da6b4832aaab6728c61299fc5c38d314f218
ericlemiere <83970546+ericlemiere@users.noreply.github.com>
2021-07-15 20:32:09 -0700

    Began data scraping with BeautifulSoup

--- AppBuilder9000/NBAstats/views.py
+++ AppBuilder9000/NBAstats/views.py
@@ -101,25 +101,53 @@ def edit_player(request, pk):
 #           Beautiful Soup
 # ===============================================================================================
 def b_ref(request):
-    # NBA season we will be analyzing
-    year = 2021
-    # URL page we will scraping (see image above)
+    # URL page we will scraping
     brURL = "https://www.basketball-reference.com/leagues/NBA_2021_totals.html"
-    # this is the HTML from the given URL
+
     page = requests.get(brURL)
-    # print(page.status_code) # Print status code to terminal - 200 means no errors
+
     soup = BeautifulSoup(page.content, 'html.parser')
     # print([type(item) for item in list(soup.children)])
 
     # Get all Headers from table and put them in a list
     headers = [th.getText() for th in soup.findAll('tr', limit=1)[0].findAll('th')]
     # Select only the headers I want and put them in a list
-    myHeaders = list(headers[i] for i in [1,4,22,25,26])
-    print(myHeaders)
+    myHeaders = list(headers[i] for i in [1, 22, 25, 26])
+    print(myHeaders)  # This shows which headers I've selected
 
+    # Get all table rows (tr) and put into a list.
+    # Omitted row with index 0 and started at index 1 (that's what [1:] does).
     rows = soup.findAll('tr')[1:]
-    player_stats = [[td.getText() for td in rows[i].findAll('td')]
-                    for i in range(len(rows))]
-    print(player_stats)
+    # Extract table data (td) from the rows and put into list
+    allStats = [[td.getText() for td in rows[i].findAll('td')]
+                for i in range(len(rows))]
 
-    return render(request, 'nba-basketball-reference.html')
+    # This print shows how we can select just the first element (name [0]) in the second row [1]
+    print(allStats[1][0])
+    # I could print all the players and all their stats with print(allStats)
+    # But that is a lot to print to console (over 500 rows)
+
+    ''' Here I declare a dictionary myStats, iterate through the stats I've scraped,
+        declare an instance of my model, and attempt to add each player with specific
+        stats to the dictionary. It does in fact create a dictionary, but I don't know
+        if the model is actually doing anything, and each iteration overwrites the 
+        previous dictionary because of like key names.
+    '''
+    myStats = {}
+    i = 0
+    while i < 10:  # I only iterate 10 times so it doesn't go all the way through the entire data set
+        p = Defensive_Stats()
+        p.playerName = {'playerName': list(allStats[i][0:1])}
+        p.defRebs = {'defRebs': list(allStats[i][21:22])}
+        p.steals = {'steals': list(allStats[i][24:25])}
+        p.blocks = {'blocks': list(allStats[i][25:26])}
+
+        myStats.update(p.playerName)
+        myStats.update(p.defRebs)
+        myStats.update(p.steals)
+        myStats.update(p.blocks)
+        i += 1
+        # Now I can print each player and the associated stats I have chosen
+        print(myStats)
+
+    return render(request, 'nba-basketball-reference.html', myStats)
