ec45eec1290e6a2bc7a3292ec4e6465e37eaeacd
joelmindset <82834522+joelmindset@users.noreply.github.com>
2021-07-19 14:50:33 -0600

    creating api and rates.html

--- AppBuilder9000/PreciousMetals/views.py
+++ AppBuilder9000/PreciousMetals/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import PreciousMetalsItem
 from .forms import MetalForm
+import http.client
+import mimetypes
+import requests
 
 
 # Create your views here.
@@ -51,6 +54,9 @@ def edit_item(request, id):
     return render(request, 'PreciousMetals_edit.html', content)
 
 
+# delete item
+
+
 def delete_item(request, id):
     item_dets = get_object_or_404(PreciousMetalsItem, id=id)
     if request.method == 'POST':
@@ -58,3 +64,31 @@ def delete_item(request, id):
         return redirect('PreciousMetals_listing')
     delete = {'item_dets': item_dets}
     return render(request, 'PreciousMetals_delete.html', delete)
+
+
+# api for rates on metals
+
+def rates(request):
+    conn = http.client.HTTPSConnection("www.goldapi.io")
+    payload = ''
+    headers = {
+        'x-access-token': 'goldapi-6buwskraz9tg4-io',
+        'Content-Type': 'application/json',
+    }
+    response = conn.request(
+        "GET", 'https://www.goldapi.io/api/:metal/:currency/', payload, headers)
+    metalrates = response.json()
+    return render(request, 'PreciousMetals_rates.html', {
+        "timestamp": metalrates[1626721039],
+        "metal": metalrates["XAU"],
+        "currency": metalrates["USD"],
+        "exchange": metalrates["FOREXCOM"],
+        "symbol": metalrates["FOREXCOM:XAUUSD"],
+        "prev_close_price": metalrates[1812.21],
+        "open_price": metalrates[1812.21],
+        "low_price": metalrates[1795.09],
+        "high_price": metalrates[1817.38],
+        "price": metalrates[1807.77],
+        "ask": metalrates[1808.17],
+        "bid": metalrates[1807.42],
+    })
