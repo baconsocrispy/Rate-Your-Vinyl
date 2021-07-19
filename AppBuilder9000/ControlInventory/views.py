08aeff4fc6663a92014993d921b391017e0b66d5
Sean Hager <smhager87@gmail.com>
2021-07-18 22:31:56 -0400

    Commiting Story 2 Changes and Criteria Required.

--- AppBuilder9000/ControlInventory/views.py
+++ AppBuilder9000/ControlInventory/views.py
@@ -1,7 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import AccountForm, ProductForm
+
+
 
 def home(request):
     return render(request, 'ControlInventory/ControlInventory_home.html')
 
+def createuser(request):
+    form = AccountForm(data=request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return redirect('ControlInventory_account.html')
+    content = {'form': form}
+    return render(request, 'ControlInventory/ControlInventory_account.html', content)
 
+def addproduct(request):
+    form = ProductForm(data=request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return redirect('ControlInventory/ControlInventory_addproduct.html')
+    content = {'form': form}
+    return render(request, 'ControlInventory/ControlInventory_addproduct.html', content)
 
+def productinfo(request):
+    return render(request, 'ControlInventory/ControlInventory_productinfo.html')
