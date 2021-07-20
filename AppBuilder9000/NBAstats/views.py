efb41635f1cc829d0bad9f494a39325ebddb64ab
ericlemiere <83970546+ericlemiere@users.noreply.github.com>
2021-07-19 17:07:26 -0700

    Stuck on getting scraped data to front end

--- AppBuilder9000/NBAstats/views.py
+++ AppBuilder9000/NBAstats/views.py
@@ -101,7 +101,7 @@ def edit_player(request, pk):
 #           Beautiful Soup
 # ===============================================================================================
 def b_ref(request):
-    # URL page we will scraping
+    # URL page we will scrape
     brURL = "https://www.basketball-reference.com/leagues/NBA_2021_totals.html"
 
     page = requests.get(brURL)
@@ -113,41 +113,51 @@ def b_ref(request):
     headers = [th.getText() for th in soup.findAll('tr', limit=1)[0].findAll('th')]
     # Select only the headers I want and put them in a list
     myHeaders = list(headers[i] for i in [1, 22, 25, 26])
-    print(myHeaders)  # This shows which headers I've selected
+    # print(myHeaders)  # This shows which headers I've selected
 
     # Get all table rows (tr) and put into a list.
-    # Omitted row with index 0 and started at index 1 (that's what [1:] does).
-    rows = soup.findAll('tr')[1:]
+    # Omitted row with index 0 and started at index 1 through 20
+    rows = soup.findAll('tr')[1:20]
     # Extract table data (td) from the rows and put into list
     allStats = [[td.getText() for td in rows[i].findAll('td')]
                 for i in range(len(rows))]
-
     # This print shows how we can select just the first element (name [0]) in the second row [1]
-    print(allStats[1][0])
+    # print(allStats[0][0])
     # I could print all the players and all their stats with print(allStats)
     # But that is a lot to print to console (over 500 rows)
-
-    ''' Here I declare a dictionary myStats, iterate through the stats I've scraped,
-        declare an instance of my model, and attempt to add each player with specific
-        stats to the dictionary. It does in fact create a dictionary, but I don't know
-        if the model is actually doing anything, and each iteration overwrites the 
-        previous dictionary because of like key names.
-    '''
+    statsList = []
     myStats = {}
+    player_dict = {}
+    check_name = ''
     i = 0
-    while i < 10:  # I only iterate 10 times so it doesn't go all the way through the entire data set
-        p = Defensive_Stats()
-        p.playerName = {'playerName': list(allStats[i][0:1])}
-        p.defRebs = {'defRebs': list(allStats[i][21:22])}
-        p.steals = {'steals': list(allStats[i][24:25])}
-        p.blocks = {'blocks': list(allStats[i][25:26])}
+    while i < len(allStats):  # I only iterate 10 times so it doesn't go all the way through the entire data set
+        ''' Get data from allStats, which comes out as a list'''
+        playerNameValue = allStats[i][0]
+        defRebsValue = allStats[i][21]
+        stealsValue = allStats[i][24]
+        blocksValue = allStats[i][25]
 
-        myStats.update(p.playerName)
-        myStats.update(p.defRebs)
-        myStats.update(p.steals)
-        myStats.update(p.blocks)
-        i += 1
-        # Now I can print each player and the associated stats I have chosen
-        print(myStats)
+        ''' Take data from lists above and convert to type int (except for name) '''
+        playerName = playerNameValue
+        defRebs = int(defRebsValue)
+        steals = int(stealsValue)
+        blocks = int(blocksValue)
 
-    return render(request, 'nba-basketball-reference.html', myStats)
+        # The page I'm scraping has multiple rows for a player who changed teams
+        # during the season, so this if statement checks to see if I have already
+        # gotten the player's total stats
+        if playerName != check_name:
+            keyNum = str(i)
+            # statsList = [playerName, defRebs, steals, blocks]
+            # dict_item = {'key{}'.format(keyNum): statsList}
+            myStats = {'playerName': playerName, 'defRebs': defRebs, 'steals': steals, 'blocks': blocks}
+            dict_item = {'key{}'.format(keyNum): myStats}
+            player_dict.update(dict_item)
+            i += 1
+
+        else:
+            i += 1
+
+        check_name = playerName
+    print(player_dict)
+    return render(request, 'nba-basketball-reference.html', player_dict)
