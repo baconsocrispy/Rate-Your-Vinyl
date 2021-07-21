a5ced5ce541669d70d570cc48423bb993b4581b4
Sean Hager <smhager87@gmail.com>
2021-07-20 23:21:29 -0400

    Commiting Story 2 Changes and Criteria Required. Created seperate HTML page to solely display items in database.

--- AppBuilder9000/ControlInventory/urls.py
+++ AppBuilder9000/ControlInventory/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='ControlInventory_home'),
+    path('', views.home, name='ControlInventory_home.html'),
     path('create/', views.createuser, name='ControlInventory_account.html'),
     path('add', views.addproduct, name='ControlInventory_addproduct.html'),
-    path("info/<int:pk>/", views.productinfo, name='ControlInventory_productinfo.html'),
+    path('info', views.productinfo, name='ControlInventory_productinfo.html'),
 ]
--- AppBuilder9000/ControlInventory/views.py
+++ AppBuilder9000/ControlInventory/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import AccountForm, ProductForm
 from .models import Account, Product
 
@@ -26,10 +26,6 @@ def addproduct(request):
     return render(request, 'ControlInventory/ControlInventory_addproduct.html', content)
 
 def productinfo(request):
-    display_accounts = Account.objects.all()
-    display_products = Product.objects.all()
-    if request.method == 'POST':
-        pk = request.POST['account']
-        return productinfo(request, pk)
-    content = {'display_accounts': display_accounts, 'display_products': display_products}
-    return render(request, 'ControlInventory/ControlInventory_productinfo.html', content)
+    inv_list = Product.objects.all()
+    return render(request, 'ControlInventory/ControlInventory_productinfo.html', {'inv_list': inv_list})
+
