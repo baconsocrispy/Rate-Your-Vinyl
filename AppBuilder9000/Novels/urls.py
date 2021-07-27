cf7c4865f77ad9cf0159cff75efdab483a82581b
David <dcmswim@gmail.com>
2021-07-27 16:01:34 -0700

    add word definition search page that utilizes API. Displays JSON response from API based on word user has input. Create links between all existing pages

--- AppBuilder9000/Novels/urls.py
+++ AppBuilder9000/Novels/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('Novels_display/', views.novelDisplay, name="Novels_display"),
     path('<int:pk>/Novels_details/', views.novelDetails, name="Novels_details"),
     path('<int:pk>/Novels_delete/', views.novelDelete, name="Novels_delete"),
-    path('Novels_define/', views.defineWord, name="Novels_define")
+    path('Novels_define/', views.defineWord, name="Novels_define"),
+    path('test/', views.github, name='test')
 ]
--- AppBuilder9000/Novels/views.py
+++ AppBuilder9000/Novels/views.py
@@ -56,13 +56,39 @@ def novelDelete(request, pk):
     return render(request, 'Novels/Novels_delete.html', context)
 
 
-# testing API
+# =====================================================================================================
+# another API test - lookup GitHub repository count by username
+# mostly used just for self-learning on API response
+# returns first name and number of public repositories listed on GitHub
+def github(request):
+    user = {}
+    if 'username' in request.GET:
+        username = request.GET['username']
+        url = 'https://api.github.com/users/%s' % username
+        response = requests.get(url)
+        user = response.json()
+    return render(request, 'Novels/github.html', {'user': user})
+
+
+# user can input a word and full dictionary response is returned
+# work in progress - need to isolate full JSON response to just the word's meaning
+def defineWord(request):
+    define = {}
+    if 'word' in request.GET:
+        word = request.GET['word']
+        url = 'https://api.dictionaryapi.dev/api/v3/entries/en_US/%s' % word
+        response = requests.get(url)
+        define = response.json()
+    return render(request, 'Novels/Novels_define.html', {'define': define})
+
+
+# =============================================================================================================
+''' This was original API test. Not currently in use but keeping block in comment for future reference if needed
+# testing API (work in progress) - this prints entire JSON response 
 def defineWord(request):
     response = requests.get('https://api.dictionaryapi.dev/api/v3/entries/en_US/amazing/')
     print(response.json())
     definition = response.json()
     context = {'definitions': definition}
     return render(request, 'Novels/Novels_define.html', context)
-
-
-
+'''
