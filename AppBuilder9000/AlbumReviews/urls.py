f765f6d47f07bb7d0af476462c7d82baabca391b
klovett84 <84836870+klovett84@users.noreply.github.com>
2021-07-28 23:20:18 -0500

    Set up add page to allow user to add album objects to database

--- AppBuilder9000/AlbumReviews/urls.py
+++ AppBuilder9000/AlbumReviews/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="AlbumReviews_home.html"),
+    path('', views.home, name="AlbumReviews_home"),
+    path('add/', views.add, name="AlbumReviews_add"),
 ]
