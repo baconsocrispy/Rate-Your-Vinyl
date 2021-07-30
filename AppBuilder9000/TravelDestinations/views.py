43c266b8280a87e5dc2624db34bdbf5d077a663d
Paul Fairbanks <paulbanksinc16@gmail.com>
2021-07-29 23:15:16 -0700

    add edit and delete function

--- AppBuilder9000/TravelDestinations/views.py
+++ AppBuilder9000/TravelDestinations/views.py
@@ -27,3 +27,22 @@ def trip_details(request, id):
     details = destination.destination.get(id=id)
     content = {'details': details}
     return render(request, 'TravelDestinations/TravelDestinations_details.html', content)
+
+
+def trip_edit(request, id):
+    item_details = get_object_or_404(destination, id=id)
+    form = DestinationForm(request.POST or None, instance=item_details)
+    if form.is_valid():
+        form.save()
+        return redirect('TravelDestinations_destinations.html')
+    content = {'form': form}
+    return render(request, 'TravelDestinations/TravelDestinations_edit.html', content)
+
+
+def trip_delete(request, id):
+    data = get_object_or_404(destination, id=id)
+    if request.method == 'POST':
+        data.delete()
+        return redirect('TravelDestinations_destinations.html')
+    delete = {'data': data}
+    return render(request, 'TravelDestinations/TravelDestinations_delete.html', delete)
