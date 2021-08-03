b25831e550dd19c5513336f7e5eeacd54f1fb34e
DForeman <dlydellf@hotmail.com>
2021-08-02 20:25:49 -0500

    Created new app (OnlineWishList); registered app within settings.py

--- AppBuilder9000/AppBuilder9000/settings.py
+++ AppBuilder9000/AppBuilder9000/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'TravelDestinations',
     'AlbumReviews',
     'NeighborhoodReview',
+    'OnlineWishList',
 ]
 
 MIDDLEWARE = [
---
+++ AppBuilder9000/OnlineWishList/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class OnlinewishlistConfig(AppConfig):
+    name = 'OnlineWishList'
