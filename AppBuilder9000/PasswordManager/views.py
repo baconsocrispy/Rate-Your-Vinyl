2c1344b3080726174a56874349ac41dbdc37a108
DForeman <dlydellf@hotmail.com>
2021-08-04 17:57:49 -0500

    Changed file's name to match previous commit

--- AppBuilder9000/PasswordManager/views.py
+++ AppBuilder9000/PasswordManager/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 
 
 # Create your views here:
-def home(request): # renders the 'home page' @ templates/PasswordManager_home.html
-    return render(request, 'PasswordManager/PasswordManager_home.html')
+def home(request): # renders the 'home page' @ templates/PwdMgr_home.html
+    return render(request, 'PasswordManager/PwdMgr_home.html')
 
