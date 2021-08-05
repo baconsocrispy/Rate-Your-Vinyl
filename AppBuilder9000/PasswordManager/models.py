90ce195a6c44824471efdaeb517df149436c5da9
DForeman <dlydellf@hotmail.com>
2021-08-05 00:26:40 -0500

    Preliminary model creation; 8 fields, 4 types

--- AppBuilder9000/PasswordManager/models.py
+++ AppBuilder9000/PasswordManager/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+# Create your models here:
+PasswordTypes = [('Personal', 'Personal'), ('Work', 'Work'), ('Combo', 'Combo'), ('Other', 'Other')]
+
+
+class NewPasswords(models.Model):
+    type = models.CharField(maxlength=8, choices=PasswordTypes)
+    creation_date = models.DateField(auto_now_add=True)
+    last_modified_date = models.DateField(auto_now=True)
+    website = models.Charfield(maxlength=100)
+    email = models.EmailField(max_length=120)
+    username = models.CharField(maxlength=60)
+    password = models.CharField(max_length=60)
+    favorite = models.BooleanField(default=False)
+
+
+
+
