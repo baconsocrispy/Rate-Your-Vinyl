e95960fd26f313bb596d0e695299190ba8765e02
DForeman <dlydellf@hotmail.com>
2021-08-05 03:10:20 -0500

    Added skeleton of generator function (no form added yet)

--- AppBuilder9000/PasswordManager/views.py
+++ AppBuilder9000/PasswordManager/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 def home(request): # renders the 'home page' @ templates/PwdMgr_home.html
     return render(request, 'PasswordManager/PwdMgr_home.html')
 
+
+def generator(request):
+    return render(request, 'PasswordManager/PwdMgr_generator.html')
+
