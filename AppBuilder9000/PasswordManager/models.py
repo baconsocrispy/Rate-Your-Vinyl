c4b34128428041a8857ac8f58b84728abe1cc53a
DForeman <dlydellf@hotmail.com>
2021-08-06 17:31:57 -0500

    Renamed model; use == NEW passwords, not STORED passwords

--- AppBuilder9000/PasswordManager/models.py
+++ AppBuilder9000/PasswordManager/models.py
@@ -5,7 +5,7 @@ from django.db import models
 PasswordTypes = [('Personal', 'Personal'), ('Work', 'Work'), ('Combo', 'Combo'), ('Other', 'Other')]
 
 
-class StoredPasswords(models.Model):
+class NewPasswords(models.Model):
     type = models.CharField(max_length=8, choices=PasswordTypes)
     creation_date = models.DateField(auto_now_add=True)
     last_modified_date = models.DateField(auto_now=True)
