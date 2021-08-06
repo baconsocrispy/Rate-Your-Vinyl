482c4a71c9e473c7c64f3c7804b1d3fe6112c505
DForeman <dlydellf@hotmail.com>
2021-08-06 18:05:04 -0500

    Added a function within views.py to render the 'New Password' page (Generator.html) & save new item to db (via ModelForm)

--- AppBuilder9000/PasswordManager/views.py
+++ AppBuilder9000/PasswordManager/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import NewPasswords
+from forms import NewPasswordsForm
 
 
 # Create your views here:
@@ -7,5 +9,11 @@ def home(request): # renders the 'home page' @ templates/PwdMgr_home.html
 
 
 def generator(request):
-    return render(request, 'PasswordManager/PwdMgr_generator.html')
+    form = NewPasswordsForm(data=request.POST or None) # backfills the form with data from the request.POST
+    if request.method == 'POST': # if the method is POST...
+        if form.is_valid(): # ... and all fields are valid...
+            form.save() # ...save the form's contents to the database
+            return redirect('PwdMgr_home') # return User to this app's Home page
+    content ={'form': form}
+    return render(request, 'PasswordManager/PwdMgr_generator.html', content) # return form's data within the 'Password Generator' page
 
