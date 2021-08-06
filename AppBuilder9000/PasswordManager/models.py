7b82bb9b75bc0fa040e9569e0d3d3745f8062721
DForeman <dlydellf@hotmail.com>
2021-08-06 17:08:03 -0500

    Renamed model

--- AppBuilder9000/PasswordManager/models.py
+++ AppBuilder9000/PasswordManager/models.py
@@ -5,7 +5,7 @@ from django.db import models
 PasswordTypes = [('Personal', 'Personal'), ('Work', 'Work'), ('Combo', 'Combo'), ('Other', 'Other')]
 
 
-class NewPasswords(models.Model):
+class StoredPasswords(models.Model):
     type = models.CharField(max_length=8, choices=PasswordTypes)
     creation_date = models.DateField(auto_now_add=True)
     last_modified_date = models.DateField(auto_now=True)
