ef5ba96ec3a1eba777d25c3aed41c9f4b760c890
DForeman <dlydellf@hotmail.com>
2021-08-07 07:27:29 -0500

    1st draft of database function; will change 'pk' to 'unique' next

--- AppBuilder9000/PasswordManager/views.py
+++ AppBuilder9000/PasswordManager/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import NewPasswords
-from .forms import NewPasswordsForm
+from .models import NewPassword
+from .forms import NewPasswordForm
 
 
 # Create your views here:
@@ -9,7 +9,7 @@ def home(request): # renders the 'home page' @ templates/PwdMgr_home.html
 
 
 def generator(request):
-    form = NewPasswordsForm(data=request.POST or None) # backfills the form with data from the request.POST
+    form = NewPasswordForm(data=request.POST or None) # backfills the form with data from the request.POST
     if request.method == 'POST': # if the method is POST...
         if form.is_valid(): # ... and all fields are valid...
             form.save() # ...save the form's contents to the database
@@ -19,6 +19,10 @@ def generator(request):
 
 
 def database(request):
-    passwords_table = get_object_or_404(NewPasswords)
+    pwdDetails = get_object_or_404(NewPassword) # if any passwords exist, get them
+    # allPasswords = NewPassword.NewPasswords.filter(pwdDetails.password) # filter ALL passwords according to the pk
+    passwords_table = {pwdDetails}
+    # for pwd in allPasswords:
+    #    passwords_table.update({allPasswords})
     content = {'passwords_table': passwords_table}
     return render(request, 'PasswordManager/PwdMgr_database.html', content)
