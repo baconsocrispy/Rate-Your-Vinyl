7cc2da2f32374fa5c3543e54776b938f177bf81f
DForeman <dlydellf@hotmail.com>
2021-08-10 17:40:38 -0500

    Gave 'database' & 'generator' functions more descriptive names; reordered functions

--- AppBuilder9000/PasswordManager/views.py
+++ AppBuilder9000/PasswordManager/views.py
@@ -9,16 +9,6 @@ def home(request): # renders the 'home page' @ templates/PwdMgr_home.html
     return render(request, 'PasswordManager/PwdMgr_home.html')
 
 
-def database(request):
-    allPasswords = NewPassword.NewPasswords.filter(id__gt=0) # returns a filtered dictionary of key:value pairs from all 9 db fields ('id' through 'Favorite')
-    # content = {'allPasswords': allPasswords} # the QuerySet (from above) becomes the k:v pair needed within 'content' (thanks Forest!)
-    paginator = Paginator(allPasswords, 10) # displays 10 passwords per page; overflow == a new, 'next' page
-
-    page_number = request.GET.get('page')
-    content = paginator.get_page(page_number)
-    return render(request, 'PasswordManager/PwdMgr_database.html', {'content': content})
-
-
 def details(request):
     allPasswords = NewPassword.NewPasswords.all()
     content = {'allPasswords': allPasswords}
@@ -28,15 +18,19 @@ def details(request):
     return render(request, 'PasswordManager/PwdMgr_details.html', {'content': content})
 
 
-
-def generator(request):
+def passwordInput(request):
     form = NewPasswordForm(data=request.POST or None) # backfills the form with data from the request.POST
     if request.method == 'POST': # if the method is POST...
         if form.is_valid(): # ... and all fields are valid...
             form.save() # ...save the form's contents to the database
             return redirect('PwdMgr_home') # return User to this app's Home page
     content = {'form': form}
-    return render(request, 'PasswordManager/PwdMgr_generator.html', content) # return form's data within the 'Password Generator' page
+    return render(request, 'PasswordManager/PwdMgr_pwdInput.html', content) # return form's data within the 'Password Generator' page
 
 
-
+def passwordsList(request):
+    allPasswords = NewPassword.NewPasswords.filter(id__gt=0) # returns a filtered dictionary of key:value pairs from all 9 db fields ('id' through 'Favorite')
+    paginator = Paginator(allPasswords, 10) # displays 10 passwords per page; overflow == a new, 'next' page
+    page_number = request.GET.get('page')
+    content = paginator.get_page(page_number)
+    return render(request, 'PasswordManager/PwdMgr_pwdsList.html', {'content': content})
