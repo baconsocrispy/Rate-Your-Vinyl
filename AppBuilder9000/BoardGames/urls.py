8dd96e8fe767bbb5eca7ce9be831d7680291f925
jasonwsvt <71233413+jasonwsvt@users.noreply.github.com>
2021-08-11 16:34:01 -0400

    Create form submission issue

--- AppBuilder9000/BoardGames/urls.py
+++ AppBuilder9000/BoardGames/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.BoardGames_home, name='BoardGames_home'),
-#    path('games/', views.BoardGames_games, name='BoardGames_games'),
-    path('addGame/', views.BoardGames_newGameEntry, name='BoardGames_NewGameEntry'),
+    path('games/', views.BoardGames_games, name='BoardGames_games'),
+    path('create/', views.BoardGames_create, name='BoardGames_create'),
     path('<int:pk>/game/', views.BoardGames_game, name='BoardGames_game'),
 ]
--- AppBuilder9000/BoardGames/views.py
+++ AppBuilder9000/BoardGames/views.py
@@ -1,30 +1,36 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import BoardGame
-#from .forms import BoardGameForm
+from .forms import BoardGameForm
+
 
 def BoardGames_home(request):
-    return render(request, 'BoardGames/BoardGames_home.html')
+    return render(request, 'BoardGames/home.html')
 
 
-#def BoardGames_games(request):
-#    content = get_object_or_404(BoardGame, pk=pk)
-#    return render(request, 'BoardGames/BoardGames_game.html', content)
+def BoardGames_games(request):
+    return render(request, 'BoardGames/games.html')
 
 
 def BoardGames_game(request, pk):
     content = get_object_or_404(BoardGame, pk=pk)
-    return render(request, 'BoardGames/BoardGames_game.html', content)
+    return render(request, 'BoardGames/details.html', content)
 
 
-def BoardGames_newGameEntry(request):
-    return render(request, 'BoardGames/BoardGames_newGameEntry.html')
+def BoardGames_create(request):
+    form = BoardGameForm(data=request.POST or None)
+    if request.method == "POST":
+        if form.is_valid():
+            form.save()
+            return redirect('entryCreated.html')
+    content = {'form': form}
+    return render(request, 'BoardGames/newGameEntry.html', content)
 
 
-#def BoardGames_insertGame(request):
-#    form = BoardGameForm(data=request.POST or None)
-#    if request.method == "POST":
-#        if form.is_valid():
-#            form.save()
-#            return redirect('home')
-#    content = {'form': form}
-#    return render(request, 'BoardGames/BoardGames_entryCreated.html', content)
+def BoardGames_insertGame(request):
+    form = BoardGameForm(data=request.POST or None)
+    if request.method == "POST":
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    content = {'form': form}
+    return render(request, 'BoardGames/entryCreated.html', content)
