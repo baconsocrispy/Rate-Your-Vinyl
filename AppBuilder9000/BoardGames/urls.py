f1cabb2c6b0735ad263fd95cbb21b4f9bf9035fe
jasonwsvt <71233413+jasonwsvt@users.noreply.github.com>
2021-08-12 16:37:21 -0400

    Nothing yet done to story 3

--- AppBuilder9000/BoardGames/urls.py
+++ AppBuilder9000/BoardGames/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.BoardGames_home, name='BoardGames_home'),
-    path('games/', views.BoardGames_games, name='BoardGames_games'),
     path('create/', views.BoardGames_create, name='BoardGames_create'),
-    path('<int:pk>/details/', views.BoardGames_details, name='BoardGames_details'),
+    path('<int:pk>/details/', views.BoardGames_get, name='BoardGames_get'),
 ]
