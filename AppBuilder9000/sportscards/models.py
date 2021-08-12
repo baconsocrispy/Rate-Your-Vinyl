4c394b2c0becd2089bddf35f8c514a06ea4ce542
Vince Ceni <splitdesishin@aol.com>
2021-08-11 20:01:01 -0400

    create form revisions

--- AppBuilder9000/sportscards/models.py
+++ AppBuilder9000/sportscards/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Card(models.Model):
+    Player = models.CharField(max_length=70, default='')
+    Year = models.IntegerField(default='')
+    Team = models.CharField(max_length=50, default='')
+    Brand = models.CharField(max_length=750, default='')
+
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.Player
--- AppBuilder9000/sportscards/views.py
+++ AppBuilder9000/sportscards/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Card
+
 
 def home(request):
     types = ["Baseball", "Basketball", "Hockey", "Football"]
@@ -9,4 +11,4 @@ def home(request):
     return render(request, "sportscards/sportscards_home.html", context)
 
 def join(request):
-    return render(request, 'sportscards/sportscards/join.html', {})
+    return render(request, 'sportscards/join.html', {})
