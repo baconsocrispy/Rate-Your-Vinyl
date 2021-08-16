8cc79140e1ab44dbe9a88052c0c8b588d843e24e
jasonwsvt <71233413+jasonwsvt@users.noreply.github.com>
2021-08-16 13:57:31 -0400

    Make the app look nicer - soft effects for hover, rounded card corners, fix a few visual bugs.

--- AppBuilder9000/BoardGames/models.py
+++ AppBuilder9000/BoardGames/models.py
@@ -3,11 +3,11 @@ from django.db import models
 
 class BoardGame(models.Model):
     Name = models.CharField(max_length=50, null=False, blank=False)
-    Publisher = models.CharField(max_length=50, null=False, blank=False) #ForeignKey(Publisher, on_delete=models.CASCADE)
+    Publisher = models.CharField(max_length=50, null=False, blank=False)
     Year = models.IntegerField(default=2021)
     Description = models.TextField(default="Type your review here (optional)")
-    Thumbnail = models.CharField(max_length=100, null=False, blank=True)
-    Image = models.CharField(max_length=100, null=False, blank=True)
+    Thumbnail = models.CharField(max_length=200, null=False, blank=True)
+    Image = models.CharField(max_length=200, null=False, blank=True)
 
     objects = models.Manager()
 
