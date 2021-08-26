ee5fbe496408857593d0b5c6a2df9fd3c280f1a8
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-08-25 22:08:30 -0700

    Create 'Player' model

--- AppBuilder9000/BlazerStats/models.py
+++ AppBuilder9000/BlazerStats/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+class Player(models.Model):
+    Name = models.CharField(max_length=30)
+    Win_Shares = models.DecimalField(max_digits=5, decimal_places=1)
+    Player_Efficiency_Rating = models.DecimalField(max_digits=5, decimal_places=1)
+    Points_Per_Game = models.DecimalField(max_digits=5, decimal_places=1)
+    Seasons = models.IntegerField()
+
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.Name
