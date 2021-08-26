fcf2e6d855246b325c281e928d70024762b3fede
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-08-26 16:11:41 -0700

    Update model 'Player' field list

--- AppBuilder9000/BlazerStats/models.py
+++ AppBuilder9000/BlazerStats/models.py
@@ -4,10 +4,10 @@ from django.db import models
 # Create your models here.
 class Player(models.Model):
     Name = models.CharField(max_length=30)
-    Win_Shares = models.DecimalField(max_digits=5, decimal_places=1)
+    Seasons = models.IntegerField(default='')
     Player_Efficiency_Rating = models.DecimalField(max_digits=5, decimal_places=1)
     Points_Per_Game = models.DecimalField(max_digits=5, decimal_places=1)
-    Seasons = models.IntegerField(default='')
+    Win_Shares = models.DecimalField(max_digits=5, decimal_places=1)
     Hall_of_Fame = models.BooleanField(default=False, null=False, blank=False)
 
     objects = models.Manager()
