883dfb659be8dc76151306933d13b00dbdea7a3a
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-08-26 22:24:41 -0700

    Create yes or no choice for 'Hall of Fame' field

--- AppBuilder9000/BlazerStats/models.py
+++ AppBuilder9000/BlazerStats/models.py
@@ -2,13 +2,19 @@ from django.db import models
 
 
 # Create your models here.
+HALL_OF_FAME = [
+    ('YES', 'Yes'),
+    ('NO', 'No'),
+]
+
+
 class Player(models.Model):
     Name = models.CharField(max_length=30)
     Seasons = models.IntegerField(default='')
     Player_Efficiency_Rating = models.DecimalField(max_digits=5, decimal_places=1)
     Points_Per_Game = models.DecimalField(max_digits=5, decimal_places=1)
     Win_Shares = models.DecimalField(max_digits=5, decimal_places=1)
-    Hall_of_Fame = models.BooleanField(default=False, null=False, blank=False)
+    Hall_of_Fame = models.CharField(max_length=3, choices=HALL_OF_FAME)
 
     objects = models.Manager()
 
