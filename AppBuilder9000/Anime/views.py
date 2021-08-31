e16dd8b9b76248a424b592d4e83de85c4fbc99f9
trancor000 <83535306+trancor000@users.noreply.github.com>
2021-08-31 13:46:00 -0700

    CT-9786-Story#2 updated

--- AppBuilder9000/Anime/views.py
+++ AppBuilder9000/Anime/views.py
@@ -1,15 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Description
+from .forms import DescriptionForm
 
 
 def anime_home(request):
     descriptions = Description.objects.all()
     return render(request, 'Anime/Anime_index.html', {'descriptions': descriptions})
 
-# #def Anime_create(request):
-# #    form = DescriptionForm(data=request.POST or None)
-# #    if request.method == "POST" and form.is_valid():
-# #        result = form.save()
-#         return redirect('Description_get', pk=result.id)
-#     else:
-#         return render(request, 'Description/create.html', {'form': form})
+
+def Anime_create(request):
+    form = DescriptionForm(data=request.POST or None)
+    if request.method == "POST" and form.is_valid():
+        result = form.save()
+        return redirect('Description_get', pk=result.id)
+    else:
+        return render(request, 'Description/create.html', {'form': form})
