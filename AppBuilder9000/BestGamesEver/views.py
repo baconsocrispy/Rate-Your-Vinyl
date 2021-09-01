816f2cc7dfeed52833d15a2f20d4111116556680
pmarino92 <86321304+pmarino92@users.noreply.github.com>
2021-09-01 16:00:18 -0700

    Created a Database list-Story #3

--- AppBuilder9000/BestGamesEver/views.py
+++ AppBuilder9000/BestGamesEver/views.py
@@ -6,6 +6,7 @@ from .models import Game
 def BestGamesEver_Home(request):
     return render(request, 'BestGamesEver/home.html')
 
+#Function for user to create a Db entry
 def Game_Create(request):
     form = GameForm(data=request.POST or None)
     if request.method == 'POST' and form.is_valid():
@@ -13,3 +14,10 @@ def Game_Create(request):
         return redirect("Game_Create")
 
     return render(request, 'BestGamesEver/Gamecreate.html', {"form": form})
+
+# Function to retrieve all items from our Db
+def Game_View(request):
+    #gets all objects of the Game database unsorted
+    game_list = Game.objects.all()
+
+    return render(request, 'BestGamesEver/ViewGames.html', {'game_list': game_list})
