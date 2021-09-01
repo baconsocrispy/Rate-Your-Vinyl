38d6a5d525eaee774f0ae4d61dbd5fca0c2d6d9e
pmarino92 <86321304+pmarino92@users.noreply.github.com>
2021-09-01 10:54:16 -0700

    Creating my model and forms.py-Story #2

--- AppBuilder9000/BestGamesEver/models.py
+++ AppBuilder9000/BestGamesEver/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Game(models.Model):
+    name = models.CharField(max_length=60)
+    genre = models.CharField(max_length=60)
+    year = models.IntegerField(default='2000')
+    description = models.TextField(max_length=200)
+
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.name
--- AppBuilder9000/BestGamesEver/views.py
+++ AppBuilder9000/BestGamesEver/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import GameForm
+from .models import Game
 
 # Displays the home page
 def BestGamesEver_Home(request):
     return render(request, 'BestGamesEver/home.html')
+
+def Game_Create(request):
+    form = GameForm(data=request.POST or None)
+    if request.method == 'POST' and form.is_valid():
+        form.save()
+        return redirect("Game_Create")
+
+    return render(request, 'BestGamesEver/Gamecreate.html', {"form": form})
