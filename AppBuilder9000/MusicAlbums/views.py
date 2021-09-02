ad48c686ec87c128f0c8a72d6eade39f0d84bcfc
aldairespiritu19 <84372346+aldairespiritu19@users.noreply.github.com>
2021-09-02 16:44:22 -0700

    Add music albums app.

--- AppBuilder9000/MusicAlbums/views.py
+++ AppBuilder9000/MusicAlbums/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Album
+from .forms import AlbumForm
 # Create your views here.
 def MusicAlbums_home(request):
     return render(request, "MusicAlbums/MusicAlbums_home.html")
+
+def MusicAlbums_add(request):
+    form = AlbumForm(data=request.POST or None)
+    if request.method =='POST':
+        if form.is_valid():
+            form.save()
+            return redirect("MusicAlbums_add")
+        content={'form': form}
+        return render(request, "MusicAlbums/MusicAlbums_add.html", content)
