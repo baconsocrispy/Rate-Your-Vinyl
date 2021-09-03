79b95f5e2787d7d13377840b1c26b8623d39cbda
aldairespiritu19 <84372346+aldairespiritu19@users.noreply.github.com>
2021-09-03 16:53:58 -0700

    Story #4 added details.

--- AppBuilder9000/MusicAlbums/views.py
+++ AppBuilder9000/MusicAlbums/views.py
@@ -22,3 +22,8 @@ def MusicAlbums_add(request):
 def MusicAlbums_display(request):
     all_albums = Album.objects.all
     return render(request, 'MusicAlbums/MusicAlbums_display.html', {'all': all_albums})
+
+def MusicAlbums_details(request, pk):
+    music_details = get_object_or_404(Album, pk=pk)
+    content = {'music_details': music_details}
+    return render(request, 'MusicAlbums/MusicAlbums_details.html', content)
