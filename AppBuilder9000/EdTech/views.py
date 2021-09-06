5f4dc514e6b31acbb844681a78fc9fa17db4c5e0
Amanda Bull <ms.amandabull@outlook.com>
2021-09-06 18:12:42 -0500

    Fixed naming errors

--- AppBuilder9000/EdTech/views.py
+++ AppBuilder9000/EdTech/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Displays the Home page
 def EdTech_Home(request):
-    return render(request, 'EdTech/home.html')
+    return render(request, 'EdTech/EdTech_Home.html')
