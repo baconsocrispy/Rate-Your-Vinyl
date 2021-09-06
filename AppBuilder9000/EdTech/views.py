211b1fb468ec1e1fe6ad2fa4e334dfe52c528883
Amanda Bull <ms.amandabull@outlook.com>
2021-09-06 17:16:47 -0500

    Urls and home page image link

--- AppBuilder9000/EdTech/views.py
+++ AppBuilder9000/EdTech/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import EdTechForm
+from .models import EdTech
 
 # Displays the Home page
 def EdTech_Home(request):
