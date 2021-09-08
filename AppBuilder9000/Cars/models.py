6da03f7b673d832ab3e3cb8083008c372fb66388
Raleigh Johnson <84159459+Raleigh-Johnson@users.noreply.github.com>
2021-09-08 17:59:32 -0500

    Story 2  Updated

--- AppBuilder9000/Cars/models.py
+++ AppBuilder9000/Cars/models.py
@@ -3,9 +3,9 @@ from django.db import models
 class description(models.Model):
     make = models.CharField(max_length=60, default='', null=False)
     model = models.CharField(max_length=60, default='', null=False)
-    first_name = models.CharField(max_length=60)
-    last_name = models.CharField(max_length=60)
-    description = models.CharField(max_length=300)
+    first_name = models.CharField(max_length=60, default='', null=False)
+    last_name = models.CharField(max_length=60, default='', null=False)
+    description = models.CharField(max_length=300, default='', null=False)
 
     objects = models.Manager()
 
--- AppBuilder9000/Cars/views.py
+++ AppBuilder9000/Cars/views.py
@@ -4,10 +4,15 @@ def Cars(request):
     return render(request, 'CarsHome.html')
 
 
-def Car_Create(request):
+def CarCreate(request):
     form = DescriptionForm(data=request.POST or None)
     if request.method == "POST" and form.is_valid():
         result = form.save()
         return redirect('Add_Car')
     else:
         return render(request, 'templates/CarCreate.html', {'form': form})
+
+def CarMessages(request):
+    entries = Description.objects.all()
+    content = {'entries': entries}
+    return render(request, 'templates/CarsEntries.html', content)
