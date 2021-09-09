364f4460710f424b343f65f4be1029fb1206c983
childsm <73154214+childsm@users.noreply.github.com>
2021-09-08 18:01:37 -0700

    #9747 Edited to meet requirements of Story 2.

--- AppBuilder9000/RecipeConvert/admin.py
+++ AppBuilder9000/RecipeConvert/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Convert
+
+admin.site.register(Convert)
--- AppBuilder9000/RecipeConvert/views.py
+++ AppBuilder9000/RecipeConvert/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import ConvertForm
+from .models import Convert
+import requests
 
 # Create your views here.
 def home(request):
     return render(request, "RecipeConvert/RecCon_home.html")
+
+def convert(request):
+    form = ConvertForm(request.POST or None)
+    if request.method == "POST":
+        if form.is_valid():
+            form.save()
+            return redirect('RecCon_home')
+    # else:
+    #     print(form.errors)
+    #     form = ConvertForm()
+    context = {
+        'form': form,
+    }
+    return render(request, "RecipeConvert/RecCon_convert.html", context)
