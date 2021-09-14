743179744c36448d83a5f46a5ddbff64b11aa81c
childsm <73154214+childsm@users.noreply.github.com>
2021-09-13 20:45:35 -0700

    Made edits to complete story 3 #9861

--- AppBuilder9000/RecipeConvert/views.py
+++ AppBuilder9000/RecipeConvert/views.py
@@ -8,15 +8,27 @@ def home(request):
     return render(request, "RecipeConvert/RecCon_home.html")
 
 def convert(request):
-    form = ConvertForm(request.POST or None)
     if request.method == "POST":
+        form = ConvertForm(request.POST or None)
         if form.is_valid():
             form.save()
             return redirect('RecCon_home')
-    # else:
-    #     print(form.errors)
-    #     form = ConvertForm()
-    context = {
-        'form': form,
-    }
-    return render(request, "RecipeConvert/RecCon_convert.html", context)
+    else:
+        form = ConvertForm()
+        context = {
+            'form': form,
+        }
+        return render(request, "RecipeConvert/RecCon_convert.html", context)
+
+def myrecipesList(request):
+    dataset = Convert.objects.all()
+    return render(request, "RecipeConvert/RecCon_myrecipes.html", {'dataset': dataset})
+
+def myrecipesDetail(request,_id):
+    try:
+        data = ConvertForm.objects.get(id = _id)
+    except ConvertForm.DoesNotExist:
+        raise Http404('Data does not exist')
+
+    return  render(request, "RecipeConvert/RecCon_myrecipes.html",{'data':data})
+
