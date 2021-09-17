de65c2dfab7c93d4d97eada82361ed4da0d8d755
heyitspham <viquph@gmail.com>
2021-09-17 19:44:56 -0400

    VP-9980-Story#2: Creating my model.

--- AppBuilder9000/VP_TrackShows/urls.py
+++ AppBuilder9000/VP_TrackShows/urls.py
@@ -6,7 +6,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('home', views.home, name='home'),
     path('admin_console', views.admin_console, name='admin_console'),
     path('<int:pk>/details/', views.details, name="details"),
     path('<int:pk>/delete/', views.delete, name="delete"),
