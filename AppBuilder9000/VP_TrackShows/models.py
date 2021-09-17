b8d9797428958d457fbb145936889b37c5da9a98
heyitspham <viquph@gmail.com>
2021-09-17 18:29:04 -0400

    VP-9980-Story#2: For Cindy to look at RunServer error.

--- AppBuilder9000/VP_TrackShows/models.py
+++ AppBuilder9000/VP_TrackShows/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+
+Day_Of_Week = [
+    ('01', 'Sunday'),
+    ('02', 'Monday'),
+    ('03', 'Tuesday'),
+    ('04', 'Wednesday'),
+    ('05', 'Thursday'),
+    ('06', 'Friday'),
+    ('07', 'Saturday'),
+]
+
+
+Show_Status = [
+    ('Continuing', 'Continuing'),
+    ('Ended', 'Ended'),
+]
+
+
+class MyShows(models.Model):
+    show_title = models.CharField(max_length=75, null=False)
+    status = models.CharField(max_length=10, choices=Show_Status, null=True)
+    day_of_week = models.CharField(max_length=20, choices=Day_Of_Week)
+    airtime = models.TimeField()
+    network = models.CharField(max_length=20)
+
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.show_title
+
+
+class ShowInfo(models.Model):
+    title = models.CharField(max_length=75)
+    description = models.CharField(max_length=500, default="", blank=True)
+    year_first_aired = models.SmallIntegerField()
+    number_of_seasons = models.SmallIntegerField()
+    image = models.CharField(max_length=255, default="", blank=True)
+
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.title
--- AppBuilder9000/VP_TrackShows/views.py
+++ AppBuilder9000/VP_TrackShows/views.py
@@ -1,16 +1,62 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import MyShows
+from .forms import ShowForm
 
 
-# Renders our home page
 def home(request):
-    return render(request, 'VP_TrackShows/home.html')
+    return render(request, 'VP_TrackShows/home.html',)
 
 
-# Adds a show to our collection
-def add_show(request):
-    return render(request, 'VP_TrackShows/add_show.html')
+def admin_console(request):
+    shows = MyShows.objects.all()
+    return render(request, 'VP_TrackShows/shows_page.html', {'shows': shows})
 
 
-# Views our shows in a table
-def view_shows(request):
-    return render(request, 'VP_TrackShows/view_shows.html')
+def details(request, pk):
+    pk = int(pk)
+    item = get_object_or_404(MyShows, pk=pk)
+    form = ShowForm(data=request.POST or None, instance=item)
+    if request.method == 'POST':
+        if form.is_valid():
+            form2 = form.save(commit=False)
+            form2.save()
+            return redirect('admin_console')
+        else:
+            print(form.errors)
+    else:
+        return render(request, 'VP_TrackShows/present_show.html', {'form': form})
+
+
+def delete(request, pk):
+    pk = int(pk)
+    item = get_object_or_404(MyShows, pk=pk)
+    if request.method == 'POST':
+        item.delete()
+        return redirect('admin_console')
+    context = {"item": item,}
+    return render(request, "VP_TrackShows/confirm_delete.html", context)
+
+
+def confirmed(request):
+    if request.method == 'POST':
+        # creates form instance and binds data to it
+        form = ShowForm(request.POST or None)
+        if form.is_valid():
+            form.delete()
+            return redirect('admin_console')
+    else:
+        return redirect('admin_console')
+
+
+def create_record(request):
+    form = ShowForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('admin_console')
+    else:
+        print(form.errors)
+        form = ShowForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'VP_TrackShows/create_record.html', context)
