271559201af4e14173248df177ba6fc3fbbab012
hafke88 <hafke88@gmail.com>
2021-09-21 13:40:48 -0400

    finishing story 5

--- AppBuilder9000/Exercises/forms.py
+++ AppBuilder9000/Exercises/forms.py
@@ -1,7 +1,8 @@
 from django.forms import ModelForm
 from .models import Exercises
 
-
+# shortcut template
+# queries everything from dB all at once, ModelForm does that
 class ExercisesForm(ModelForm):
     class Meta:
         model = Exercises
--- AppBuilder9000/Exercises/models.py
+++ AppBuilder9000/Exercises/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-
+# provides drop down on adding exercises
 TARGET_MUSCLE_CHOICES = (
     ('Chest','Chest'),
     ('Back','Back'),
@@ -9,7 +9,7 @@ TARGET_MUSCLE_CHOICES = (
     ('Arms','Arms'),
 )
 
-
+# this is the model for everything that gets entered into the database
 class Exercises(models.Model):
     target_muscle = models.CharField(max_length=60, choices=TARGET_MUSCLE_CHOICES)
     exercise_name = models.CharField(max_length=60, default="", blank=True, null=False)
