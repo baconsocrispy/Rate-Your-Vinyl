5440942eb95d3a1f268586a55ae0c830321e6ce3
EChristian74 <75714720+EChristian74@users.noreply.github.com>
2021-09-24 13:55:59 -0500

    Second commit for instructor review.

--- AppBuilder9000/InlineSpeedSkates/views.py
+++ AppBuilder9000/InlineSpeedSkates/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 # Displays the Home page
 def InlineSpeedSkates_Home(request):
-    return render(request, 'InlineSpeedSkates/InlineSpeedSkates_home.html')
+    return render(request, 'skates_home.html')
