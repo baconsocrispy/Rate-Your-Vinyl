4bb8a3a7d14fefd41de1299aef2dc85f7424e8a2
nicktheperkins <nicktheperkins@gmail.com>
2021-09-24 11:50:06 -0600

    Displayed all items from database

--- AppBuilder9000/Reading/urls.py
+++ AppBuilder9000/Reading/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns=[
     path('', views.Reading_Home, name='Reading_Home'),
     path('Reading_Create/', views.Reading_Create, name='Reading_Create'),
+    path('Reading_Records/', views.Reading_Records, name='Reading_Records'),
 ]
