5c655bd7081f2688b06bd1227644d63569088a63
MtSoftDevSolutions <87912940+MtSoftDevSolutions@users.noreply.github.com>
2021-09-28 14:47:47 -0400

    Finished story 1

--- AppBuilder9000/EuroTrip/urls.py
+++ AppBuilder9000/EuroTrip/urls.py
@@ -6,12 +6,7 @@ from . import views
 # you invoke the name while the views.'method' page occurs on your browser
 urlpatterns = [
     path('', views.eurotrip_home, name="eurotrip_home"),
-    path('exercises_names', views.exercises_names, name="exercises_names"),
-    path('<int:pk>/exercises_details/', views.exercises_details, name="exercises_details"),
-    path('<int:pk>/exercises_delete/', views.exercises_delete, name="exercises_delete"),
-    path('exercises_delete/', views.exercises_confirm, name="exercises_confirm"),
-    path('exercises_create/', views.exercises_create, name="exercises_create"),
-]
+   ]
 
 
 
---
+++ AppBuilder9000/EuroTrip/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+def eurotrip_home (request):
+    return render(request, 'eurotrip_home.html')
+
+
