59b1607ec272f948325e3afab4c1b9b138a472c4
Whitatt <60752908+Whitatt@users.noreply.github.com>
2021-09-30 14:41:50 -0700

    completed step 2

--- AppBuilder9000/cultclassicsapp/models.py
+++ AppBuilder9000/cultclassicsapp/models.py
@@ -10,9 +10,9 @@ TYPE_CHOICES = {
 }
 
 class CultClassics(models.Model):
-    type = models.CharField(max_length=15, choices=TYPE_CHOICES)
+    type = models.CharField(max_length=25, choices=TYPE_CHOICES)
     user_name = models.CharField(max_length=25, default="", blank=True, null=False)
-    Movie_Title = models.CharField(max_length=30, default="", blank=True, null=False)
+    Movie_Title = models.CharField(max_length=25, default="", blank=True, null=False)
     Actor = models.CharField(max_length=25, default="", blank=True, null=False)
     Actress = models.CharField(max_length=25, default="", blank=True, null=True)
     Description = models.TextField(max_length=500, default="", blank=True)
