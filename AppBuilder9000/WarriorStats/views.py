d3b96288c4360cdbc9ccaeaa6482a7a9b2e65295
Jesse Kuykendall <84601365+techyjesse@users.noreply.github.com>
2021-10-04 12:15:32 -0700

    buildApp story complete - JK-10104 buildApp - Added base and home pages, css, linked app to homepage.

--- AppBuilder9000/WarriorStats/views.py
+++ AppBuilder9000/WarriorStats/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Player
+from .forms import PlayerForm
 
 
 def stats_Home(request):
     return render(request, 'WarriorStats/stats_Home.html')
+
+
+def add_player(request):
+    form = PlayerForm(data=request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return redirect('stats_home')
+    content = {'form': form}
+    return render(request, 'WarriorStats/create.html', content)
