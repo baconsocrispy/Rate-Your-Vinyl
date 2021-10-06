d2e531f500b9e546aa7aef599cba1da6afe6acac
codhharris713 <83795096+codhharris713@users.noreply.github.com>
2021-10-06 13:46:33 -0700

    new commit

--- AppBuilder9000/SushiRecipes/views.py
+++ AppBuilder9000/SushiRecipes/views.py
@@ -1,12 +1,6 @@
 from django.shortcuts import render
-from .forms import SushiForm
+
 
 
 def sushi_recipes_home(request):
     return render(request, 'SushiRecipes/Sushi_Recipes_Home.html')
-
-
-def sushi_view(request):
-
-
-
