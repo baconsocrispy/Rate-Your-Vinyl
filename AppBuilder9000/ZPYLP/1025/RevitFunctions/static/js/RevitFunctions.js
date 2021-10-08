3788df6819d579eb37b7052b37bcec7914192970
gitMeX3 <87834092+gitMeX3@users.noreply.github.com>
2021-10-07 18:24:47 -0700

    story9 attempt

--- AppBuilder9000/ZPYLP/1025/RevitFunctions/static/js/RevitFunctions.js
+++ AppBuilder9000/ZPYLP/1025/RevitFunctions/static/js/RevitFunctions.js
@@ -1,3 +1,21 @@
 // Story8: JS
 // popup window on every page,
 window.alert("Welcome! Please select one of the following: \n     HOME to return to this page \n     +USER to add a new User \n     +RVT FUNC to add new Revit Functions \n     RECORDS to access Revit Function features\n          - DETAILS to see associated Revit Function info \n          - EDIT to edit the Revit Function's info \n          - DELETE to remove the Revit Function & data \n          - SEARCH with predefined google search keywords");
+
+// Story9: for Checkbox
+//function getFavorites() {
+//    favoritesArray = document.getElementsByClassName("Favorites");
+//
+//    (var i=0; i <favoritesArray.length; i++) {
+//        for (var i = 0; i < favoritesArray; )
+//    }
+//
+//    if (checkbox.checked == 1) {
+//        var selectedFavorties = document.getElementsByClassName("Favorites");
+//    }
+//    else {
+//        checkbox = 0;
+//    }
+//}
+
+// save the getFavorites to dB
