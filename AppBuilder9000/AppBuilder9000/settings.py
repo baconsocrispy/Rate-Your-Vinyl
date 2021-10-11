66e156239caf61e6c44816134a91994bfd572507
Cindy Lara <74997705+cindy123jl@users.noreply.github.com>
2021-10-11 10:45:25 -0700

    changes

--- AppBuilder9000/AppBuilder9000/settings.py
+++ AppBuilder9000/AppBuilder9000/settings.py
@@ -40,8 +40,6 @@ INSTALLED_APPS = [
     'rest_framework',
     'bootstrap4',
     'crispy_forms',
-    'BoardGames',
-    'Exercises',
     'Reading',
     'InlineSpeedSkates',
     'WarriorStats',
