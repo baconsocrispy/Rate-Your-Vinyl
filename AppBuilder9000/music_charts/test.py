4adedaea0b8889186ee59782df2886493b4679d5
michaelmtz10 <m.mtz1088@gmail.com>
2021-10-14 16:12:58 -0700

    made some test.py changes

--- AppBuilder9000/music_charts/test.py
+++ AppBuilder9000/music_charts/test.py
@@ -3,17 +3,15 @@ from bs4 import BeautifulSoup
 import time
 
 
-def hot_hundred():
-    URL = 'https://www.billboard.com/charts/hot-100'
-    page = requests.get(URL)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    section = soup.find_all('article', class_='ye-chart-item piano-content-overlay__gated-item')
-    for index, piece in enumerate(section):
-        artist = section.find_all('div', class_='ye-chart-item__artist')
-        song = section.find('div', class_='ye-chart-item__title').div.text
-        position = section.find('span', class_='decade-end-chart-item__peak-info-data').span.text
-        peak_date = section.find('span', class_='decade-end-chart-item__peak-info-data').span.text
-        print(f"""Song: {song}""")
-        print(f"""Song: {artist}""")
-        print(f"""Song: {position}""")
-        print(f"""Song: {peak_date}""")
+URL = 'https://www.billboard.com/charts/hot-100'
+page = requests.get(URL)
+soup = BeautifulSoup(page.content, 'html.parser')
+song_title = []
+song_Artist = []
+song_number = []
+
+song_data = soup.find_all('li', attrs={"class": 'chart-list__element display-flex'})
+
+for store in song_data:
+    number = store.button.span.span.text
+    print(number)
