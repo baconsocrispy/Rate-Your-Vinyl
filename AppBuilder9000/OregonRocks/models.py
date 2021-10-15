53c19ef1a1653acd90b811ca61ca23819f03fcd6
JacobDrawhorn <jake.drawhorn@gmail.com>
2021-10-15 11:56:54 -0700

    2st commit

--- AppBuilder9000/OregonRocks/models.py
+++ AppBuilder9000/OregonRocks/models.py
@@ -1,10 +1,22 @@
 from django.db import models
 
 class RockLoc(models.Model):
-    name = models.CharField(max_length=255)
-    address = models.CharField(max_length=255)
-    created_at = models.DateTimeField(auto_now_add=True)
-    location_description = models.CharField(max_length=255)
+    created_at = models.DateField()
+    name = models.CharField(max_length=60, default="", blank=True, null=False)
+    address = models.CharField(max_length=60, default="", blank=True)
+    location_description = models.TextField(max_length=300, default="", blank=True)
+
+    objects = models.Manager()
 
     def __str__(self):
         return self.name
+
+
+# class RockLoc(models.Model):
+#     name = models.CharField(max_length=255)
+#     address = models.CharField(max_length=255)
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     location_description = models.CharField(max_length=255)
+#
+#     def __str__(self):
+#         return self.name
--- AppBuilder9000/OregonRocks/views.py
+++ AppBuilder9000/OregonRocks/views.py
@@ -1,34 +1,33 @@
-from django.shortcuts import render, get_object_or_404
-
-from rest_framework import generics
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import RockLoc
-from.forms import RockForm
-from django.shortcuts import redirect
-
-from geopy.geocoders import Nominatim
+from .forms import RockForm
 
 def Oregon_Rocks_Home(request):
     return render(request, 'OregonRocks/OregonRocksHome.html')
 
 def Rocks_Create(request):
-    form = RockLoc(request)
-    if form == 'POST':
+    form = RockForm(request.POST or None)
+    if form.is_valid():
         form.save()
+        return redirect('Rocks_Create')
     else:
-        form = RockLoc()
+        print(form.errors)
+        form = RockForm()
     context = {
         'form': form,
     }
     return render(request, 'OregonRocks/Rocks_Create.html', context)
 
 def Rock_Locations(request):
-    locations = get_object_or_404(RockLoc, pk=pk)
+    locations = RockLoc.objects.all()
     return render(request, 'OregonRocks/Rock_Locations.html', {'locations': locations})
 
 def Rock_Details(request, pk):
     details = get_object_or_404(RockLoc, pk=pk)
     context = {'details': details}
     return render(request, "OregonRocks/Rock_Details.html", context)
+
+
 #geolocator = Nominatim(user_agent="location")
 
 
