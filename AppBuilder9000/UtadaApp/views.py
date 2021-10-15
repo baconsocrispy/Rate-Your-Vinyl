aa8b21890caeb2f155d5f77b82257a4e5b1a65b2
NamiTu117 <86804169+NamiTu117@users.noreply.github.com>
2021-10-14 20:17:42 -0400

    Sign-in made

--- AppBuilder9000/UtadaApp/views.py
+++ AppBuilder9000/UtadaApp/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Music
+
+
+
 
 # Create your views here.
 def home(request):
       return render(request,'hiki_home.html')
+
+
+
+def music(request):
+      songs = Music.objects.all
+      return render(request,'songs/hiki_music.html', {'songs':songs})
