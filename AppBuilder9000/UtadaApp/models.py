f42e0740f8aaca5fe76e3f9d9295303544083764
NamiTu117 <86804169+NamiTu117@users.noreply.github.com>
2021-10-15 15:58:46 -0400

    Sign-in made

--- AppBuilder9000/UtadaApp/models.py
+++ AppBuilder9000/UtadaApp/models.py
@@ -11,6 +11,8 @@ SONG_CHOICES = (('Kiss & Cry', 'Beautiful World'),
 class Music(models.Model):
     songs = models.CharField(max_length=70, choices=SONG_CHOICES)
     #user will select a song from the list and it will play
+    year = models.IntegerField()
+    image = models.CharField(max_length=500)
 
     Songs = models.Manager()
 
--- AppBuilder9000/UtadaApp/views.py
+++ AppBuilder9000/UtadaApp/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Music
-
-
+from .forms import MusicForm
 
 
 # Create your views here.
@@ -11,5 +10,11 @@ def home(request):
 
 
 def music(request):
-      songs = Music.objects.all
-      return render(request,'songs/hiki_music.html', {'songs':songs})
+      form = MusicForm(data=request.POST or None)
+      if form.is_valid():
+            form.save()
+            return redirect('hiki_music')
+      else:
+            print(form.errors)
+      context = {'form': form}
+      return render(request,'hiki_music.html',context)
