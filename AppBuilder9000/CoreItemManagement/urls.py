a3b796dced68ab6209ebd9bb9fdb80be22147849
Jason <AarsZach@yahoo.com>
2021-10-15 16:35:20 -0600

    Created and add new model and . Updated:  + cim_base.html white space
    Created:
     + Create your model and add a migration, make sure to plan out all the categories you want to track for your object. Include an objects manager for accessing the database.
     + Create a model form that will include any inputs the user needs to make
     + Add a template to your app folder for creating a new item.
     + Add a views function that renders the create page and utilizes the model form to save the collection item to the database.
     + Check the database to make sure your item saves without errors.
     + Add whatever styling is appropriate to your templates.
    
    Issues & Bugs:
     - When adding a new item the system does not check the existing list for the item. This can cause duplicates of items and needs to be fixed soon.
    
    Future Plans: (if given the chance)
     = Add more field types for items like; discounts, membership deals, and stock quantity.

--- AppBuilder9000/CoreItemManagement/urls.py
+++ AppBuilder9000/CoreItemManagement/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.cim_home, name="cim_home"),
+    path('add/', views.cim_new, name="cim_new"),
+    path('edit/', views.cim_edit, name="cim_edit"),
+    path('items/', views.cim_list, name="cim_list"),
+    path('items/<int:pk>', views.cim_item, name="cim_item"),
+    path('addVendor/', views.cim_newVendor, name="cim_newVendor"),
 ]
--- AppBuilder9000/CoreItemManagement/views.py
+++ AppBuilder9000/CoreItemManagement/views.py
@@ -1,6 +1,83 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import Http404
+from .models import Item, Vendor
+from .forms import ItemForm, VendorForm
 
 # Create your views here.
 def cim_home(request):
     return render(request, 'cim_home.html')
+
+
+def cim_new(request):
+    form = ItemForm(data=request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return redirect('cim_home')
+    content = {'form': form}
+    return render(request, 'cim_new.html', content)
+
+
+def cim_newVendor(request):
+    form = VendorForm(data=request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return redirect('cim_home')
+    content = {'form': form}
+    return render(request, 'cim_newVendor.html', content)
+
+
+
+def cim_edit(request):
+    form = Item(data=request.POST)
+    if request.method == 'POST':
+        if form.is_valid():
+            pk = request.POST['id']
+            form.save()
+            return cim_item(request, pk)
+    return render(request, 'cim_edit.html')
+
+
+
+def cim_editVendor(request):
+    form = Vendor(data=request.POST)
+    if request.method == 'POST':
+        if form.is_valid():
+            pk = request.POST['id']
+            form.save()
+            return cim_item(request, pk)
+    return render(request, 'cim_editVendor.html')
+
+
+def cim_list(request):
+    _items = get_object_or_404(Item)
+    try:
+        items = Item.objects.all()
+
+    except Item.DoesNotExist:
+        raise Http404("No items found")
+
+    for item in items:
+        print(item)
+
+    content = {'items': _items}
+    return render(request, 'cim_list.html', content)
+
+
+def cim_item(request, pk):
+    _item = get_object_or_404(Item, pk=pk)
+
+    item = Item
+
+
+    content = {
+        'part_num': item.item_pn,
+        'name': item.item_name,
+        'count': item.item_count,
+        'price': item.item_price,
+        'category': item.item_category,
+        'vendor': item.item_vendor,
+    }
+    return render(request, 'cim_item.html', content)
 
