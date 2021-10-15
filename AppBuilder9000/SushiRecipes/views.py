e510fb4bfb7f501f009fb713cd3ea85e502048b7
codhharris713 <83795096+codhharris713@users.noreply.github.com>
2021-10-14 18:25:08 -0700

    Added Edit and Delete functions

--- AppBuilder9000/SushiRecipes/views.py
+++ AppBuilder9000/SushiRecipes/views.py
@@ -28,3 +28,26 @@ def sushi_recipes_details(request, pk):
     details = get_object_or_404(SushiRecipes, pk=pk)
     context = {'details': details}
     return render(request, "SushiRecipes/Sushi_Recipes_Details.html", context)
+
+
+def sushi_recipes_edit(request, pk):
+    edit = get_object_or_404(SushiRecipes, pk=pk)
+    form = SushiForm(request.POST or None, instance=edit)
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return redirect('Sushi_Recipes_View')
+    else:
+        return render(request, 'SushiRecipes/Sushi_Recipes_Edit.html', {'form': form})
+
+
+def sushi_recipes_delete(request, pk):
+    obj = get_object_or_404(SushiRecipes, pk=pk)
+    form = SushiForm(request.POST or None, instance=obj)
+    if request.method == 'POST':
+        obj.delete()
+        return redirect('../../')
+    context = {
+        "object": obj
+    }
+    return render(request, 'SushiRecipes/Sushi_Recipes_Delete.html', context)
