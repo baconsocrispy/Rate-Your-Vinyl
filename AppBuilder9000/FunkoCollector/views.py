f9645c663607cc3c43b14211dbb621320dab319f
Kelinz74 <76086747+Kelinz74@users.noreply.github.com>
2021-10-15 20:12:49 -0700

    Created a model and added a migration which included an objects manager for accessing the database.  Created a model form that included inputs the user can use to make an entry into the database.  Created a views function that renders the create page and utilizes the model form to save the collection item to the database.  Added a template to be able to view saved objects in the database.

--- AppBuilder9000/FunkoCollector/views.py
+++ AppBuilder9000/FunkoCollector/views.py
@@ -1,5 +1,32 @@
 from django.shortcuts import render
+from .models import FunkoPopName
+from .forms import CollectionForm
+from django.http import HttpResponseRedirect
 
-# function linking the html file to the url.py
+
+
 def funkocollectorhome(request):
     return render(request, 'funkocollectorhome.html')
+
+# function for rendering a form for adding to the CollectionForm database from the web page addcollection.html
+
+
+def addcollection(request):
+    submitted = False
+    if request.method == "POST":
+        form = CollectionForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('addcollection?submitted=True')
+    else:
+        form = CollectionForm
+        if 'submitted' in request.GET:
+            submitted = True
+    return render(request, 'addcollection.html', {'form': form, 'submitted': submitted})
+
+# function for rendering a information saved from the CollectionForm database to the web page collection.html
+
+
+def collection(request):
+    collectionpop = FunkoPopName.objects.all()
+    return render(request, 'collection.html', {'collectionpop': collectionpop})
