f9aa38223c71b959a83c49e67b54fe90562e9d76
michaelmtz10 <m.mtz1088@gmail.com>
2021-10-17 22:15:43 -0700

    made some test.py changes

--- AppBuilder9000/music_charts/views.py
+++ AppBuilder9000/music_charts/views.py
@@ -56,28 +56,34 @@ def delete_event(request, event_id):  # deletes the selected row, and redirects 
     return redirect("chart_data")
 
 
-def edit_chart(request, event_id):
-    changes = Charts.objects.get(pk=event_id)
-    form = ChartsForm(request.POST or None, instance=changes)
+def edit_chart(request, event_id): # creating form instance per submission
+    changes = Charts.objects.get(pk=event_id)  # assigned var to object from charts class
+    form = ChartsForm(request.POST or None, instance=changes) # def edit template form
     if form.is_valid():
         form.save()
-        return redirect("chart_data")
-    return render(request, 'music_charts/edit_chart.html', {'changes': changes, 'form': form})
+        return redirect("chart_data") # return to 'Charts'
+    return render(request, 'music_charts/edit_chart.html', {'changes': changes, 'form': form}) # using key word vars to
+                                                                                                # call function
 
 
 def hot_one_hundred(request):
-    URL = 'https://www.billboard.com/charts/hot-100'
-    response = requests.get(URL)
-    soup = bs(response.content, 'html.parser')
-    song_data = soup.findAll('li', attrs={"class": 'chart-list__element display--flex'})
+    URL = 'https://www.billboard.com/charts/hot-100' # grab url
+    response = requests.get(URL) # assign response to var
+    soup = bs(response.content, 'html.parser') # grabbing entire html, calling parser
+    song_data = soup.findAll('li', attrs={"class": 'chart-list__element display--flex'})    # got first parent class of
+                                                                                             # html tag
+    song_list = [] # starting instance count at 0 or none
 
-    for store in song_data:
-        number = store.button.span.span.text
-        titles = store.button.find('span', class_='chart-element__information')
-        title2 = titles.find('span', class_='chart-element__information__song text--truncate color--primary').text
-        artist = titles.find('span', class_='chart-element__information__artist text--truncate color--secondary').text
-        context = {'song_data': song_data, 'titles': titles, 'rank': number, 'song': title2, 'artist': artist}
+    for store in song_data: # instance in list of song_data
+        number = store.button.span.span.text # retrieving rank of item
+        titles = store.button.find('span', class_='chart-element__information') # parent tag of desired content but child of 'song_data
+        title2 = titles.find('span', class_='chart-element__information__song text--truncate color--primary').text # get text with song
+        artist = titles.find('span', class_='chart-element__information__artist text--truncate color--secondary').text # get text with artist name
+        song_array = (number, title2, artist) # make array with vars
 
-        return render(request, 'music_charts/hot_100.html', context)
+        song_list.append(song_array)  # made list for every instance of array containing number, title, title 2
+
+    return render(request, 'music_charts/hot_100.html', {'song_list': song_list}) # call song_list from html song_list
+
 
 
