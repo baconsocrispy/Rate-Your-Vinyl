7bb7edd74e1df0967ca3e15ba9585b8b6b09de8d
NamiTu117 <86804169+NamiTu117@users.noreply.github.com>
2021-10-19 13:04:54 -0400

    Sign-in made

--- AppBuilder9000/UtadaApp/models.py
+++ AppBuilder9000/UtadaApp/models.py
@@ -9,7 +9,7 @@ SONG_CHOICES = (('Kiss & Cry', 'Beautiful World'),
                 ('Hikari', 'Passion'))
 
 class Music(models.Model):
-    songs = models.CharField(max_length=70, choices=SONG_CHOICES)
+    songs = models.CharField(max_length=500, choices=SONG_CHOICES)
     #user will select a song from the list and it will play
     year = models.IntegerField()
     image = models.CharField(max_length=500)
@@ -18,4 +18,16 @@ class Music(models.Model):
 
     def __str__(self):#intake
         return self.songs
+
+
+#class Entries(models.Model):
+ #   title = models.CharField(max_length=75, choices=SONG_CHOICES)
+  #  date = models.IntegerField()
+   # picture = models.CharField(max_length=500)
+
+    #Entries = models.Manager()
+
+
+
+
 
