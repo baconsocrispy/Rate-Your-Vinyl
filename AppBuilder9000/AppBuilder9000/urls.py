b264a67aae345b1fa31f727f1886a8cb26bfda21
Cindy Lara <74997705+cindy123jl@users.noreply.github.com>
2021-10-25 17:06:35 -0700

    cleaning up the apps

--- AppBuilder9000/AppBuilder9000/urls.py
+++ AppBuilder9000/AppBuilder9000/urls.py
@@ -24,10 +24,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/v1/", include("OregonRocks.urls")),
     path('', views.home, name='home'),
-    path('WarriorStats/', include('WarriorStats.urls')),
-    path('EuroTrip/', include('EuroTrip.urls')),
-    path('RevitFunctions/', include('RevitFunctions.urls')),            #Story1,Step5: Register urls with Mainapp.
-    path('Recipes/', include('Recipes.urls')),
     path('CultClassics/', include('cultclassicsapp.urls')),
     path('SushiRecipes/', include('SushiRecipes.urls')),
     path('ACNHdb/', include('ACNHdb.urls')),
