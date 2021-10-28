b211688b41840f30b089fefee35cae8fe5b6d720
konadan808 <88794280+konadan808@users.noreply.github.com>
2021-10-28 11:15:34 -0700

    Committing creation of model, forms, create content pages, additional CSS styling to input forms.

--- AppBuilder9000/KeyMaster/urls.py
+++ AppBuilder9000/KeyMaster/urls.py
@@ -3,4 +3,9 @@ from .import views
 
 urlpatterns = [
     path('', views.KeyMaster_home, name="KeyMaster_home"),
+    path('addgame/', views.KeyMasterAddGame, name='addgame'),
+    path('adddlc/', views.KeyMasterAddDLC, name='adddlc'),
+    path('addwish/', views.KeyMasterAddWishlist, name='addwish'),
+
+
 ]
--- AppBuilder9000/KeyMaster/views.py
+++ AppBuilder9000/KeyMaster/views.py
@@ -1,5 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
+from .models import Games, Dlc, Wishlist
+from .forms import GameForm, DlcForm, WishlistForm
+
 
 def KeyMaster_home(request):
     return render(request, 'KeyMaster_home.html')
+
+def KeyMasterAddGame(request):
+    form = GameForm(data=request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+    content = {'form': form}
+    return render(request, 'KeyMasterAddGame.html', content)
+
+def KeyMasterAddDLC(request):
+    form = DlcForm(data=request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+    content = {'form': form}
+    return render(request, 'KeyMasterAddDLC.html', content)
+
+def KeyMasterAddWishlist(request):
+    form = WishlistForm(data=request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+    content = {'form': form}
+    return render(request, 'KeyMasterWishlist.html', content)
