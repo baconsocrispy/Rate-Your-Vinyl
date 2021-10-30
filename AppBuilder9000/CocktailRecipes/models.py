865bec17491c516ad28b67a6c682a5b7ed8bb4f4
Matt Fuller <63671581+mdfull01@users.noreply.github.com>
2021-10-29 20:41:07 -0400

    Create models Cocktail and Review

--- AppBuilder9000/CocktailRecipes/models.py
+++ AppBuilder9000/CocktailRecipes/models.py
@@ -1,3 +1,56 @@
 from django.db import models
 
-# Create your models here.
+
+class Cocktail(models.Model):
+    cocktail_name = models.CharField(max_length=50, unique=True)
+    ingredient_1 = models.CharField(max_length=50)
+    quantity_1 = models.DecimalField(max_digits=3, decimal_places=1)
+    unit_1 = models.CharField(max_length=15)
+    ingredient_2 = models.CharField(max_length=50, blank=True)
+    quantity_2 = models.DecimalField(max_digits=3, decimal_places=1, blank=True)
+    unit_2 = models.CharField(max_length=15, blank=True)
+    ingredient_3 = models.CharField(max_length=50, blank=True)
+    quantity_3 = models.DecimalField(max_digits=3, decimal_places=1, blank=True)
+    unit_3 = models.CharField(max_length=15, blank=True)
+    ingredient_4 = models.CharField(max_length=50, blank=True)
+    quantity_4 = models.DecimalField(max_digits=3, decimal_places=1, blank=True)
+    unit_4 = models.CharField(max_length=15, blank=True)
+    ingredient_5 = models.CharField(max_length=50, blank=True)
+    quantity_5 = models.DecimalField(max_digits=3, decimal_places=1, blank=True)
+    unit_5 = models.CharField(max_length=15, blank=True)
+    ingredient_6 = models.CharField(max_length=50, blank=True)
+    quantity_6 = models.DecimalField(max_digits=3, decimal_places=1, blank=True)
+    unit_6 = models.CharField(max_length=15, blank=True)
+    ingredient_7 = models.CharField(max_length=50, blank=True)
+    quantity_7 = models.DecimalField(max_digits=3, decimal_places=1, blank=True)
+    unit_7 = models.CharField(max_length=15, blank=True)
+    ingredient_8 = models.CharField(max_length=50, blank=True)
+    quantity_8 = models.DecimalField(max_digits=3, decimal_places=1, blank=True)
+    unit_8 = models.CharField(max_length=15, blank=True)
+    ingredient_9 = models.CharField(max_length=50, blank=True)
+    quantity_9 = models.DecimalField(max_digits=3, decimal_places=1, blank=True)
+    unit_9 = models.CharField(max_length=15, blank=True)
+    ingredient_10 = models.CharField(max_length=50, blank=True)
+    quantity_10 = models.DecimalField(max_digits=3, decimal_places=1, blank=True)
+    unit_10 = models.CharField(max_length=15, blank=True)
+    directions = models.TextField()
+    avg_rating = models.DecimalField(max_digits=2, decimal_places=1, blank=True)
+    # photo = models.FileField(upload_to='static/CocktailRecipes/user_images', blank=True)
+
+    Cocktails = models.Manager()
+
+    def __str__(self):
+        return str(self.cocktail_name)
+
+
+rating_choices = [(1, '1'), (2, '2'), (3, '3'), (4, '4'), (5, '5')]
+
+
+class Review(models.Model):
+    user_name = models.CharField(max_length=50)
+    rating = models.PositiveSmallIntegerField()
+    comments = models.TextField(null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    cocktail = models.ForeignKey(Cocktail, on_delete=models.CASCADE)
+
+    Reviews = models.Manager()
--- AppBuilder9000/CocktailRecipes/urls.py
+++ AppBuilder9000/CocktailRecipes/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.cocktail_recipes_home, name='cocktail_recipes_home'),
+    path('add-cocktail/', views.add_cocktail, name='cocktail_recipes_add_cocktail'),
 ]
