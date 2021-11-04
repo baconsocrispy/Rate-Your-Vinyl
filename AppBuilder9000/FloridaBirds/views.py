9cec7a2abaa5c0f144a4ee65c58eae48974c1652
Vicky Jones <vlj8008@gmail.com>
2021-11-03 20:29:47 -0400

    Changes to file that satisfy story board 1 requirements.

--- AppBuilder9000/FloridaBirds/views.py
+++ AppBuilder9000/FloridaBirds/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def florida_birds_home(request):
-    return render(request, 'FloridaBirds_base.html')
+    return render(request, 'FloridaBirds/FloridaBirds_home.html')
