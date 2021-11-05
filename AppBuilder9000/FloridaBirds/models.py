5274ee749cdb5c3f7b376bb4dfeaa6b1f5acbd02
Vicky Jones <vlj8008@gmail.com>
2021-11-05 16:03:08 -0400

    Story Board 2 completed. Added styling to the form.

--- AppBuilder9000/FloridaBirds/models.py
+++ AppBuilder9000/FloridaBirds/models.py
@@ -7,12 +7,15 @@ from django.db import models
 class BirdDescription(models.Model):
     bird_name = models.CharField(max_length=30, blank=True)
     date_seen = models.DateField(blank=True)
-    habitat = models.TextField(max_length=300)
-    description = models.TextField(max_length=300)
+    habitat = models.TextField(max_length=100)
+    description = models.TextField(max_length=100)
     image = models.ImageField(blank=True)
 
 # BirdDescription is the models manager for accessing the dB.
     BirdDescriptions = models.Manager()
+
+# Below dunder method represents the class object as a string.
+# The bird name ie "stork" will appear in output which makes it easier to read.
 
     def __str__(self):
         return str(self.bird_name)
--- AppBuilder9000/FloridaBirds/views.py
+++ AppBuilder9000/FloridaBirds/views.py
@@ -8,18 +8,21 @@ from .forms import BirdDescriptionForm
 # View to display home page.
 
 def florida_birds_home(request):
+    # render method takes the request object and template name as arguments
+    # returns httpResponse object with rendered text.
     return render(request, 'FloridaBirds/FloridaBirds_home.html')
 
 # View to add a bird to database.
 
 
 def add_bird(request):
+    # declare variable called "form".
     form = BirdDescriptionForm(data=request.POST or None)
-    if form.is_valid():
+    if form.is_valid():  # method is used to validate all fields in the user form.
         form.save()
-        return redirect('florida_birds_add_bird')
+        return redirect('florida_birds_add_bird')  # go back to "add bird url"
     else:
         print(form.errors)
         form = BirdDescriptionForm()
-        context = {'form': form}
+        context = {'form': form}  # dictionary item
     return render(request, 'FloridaBirds/FloridaBirds_add_bird.html', context)
