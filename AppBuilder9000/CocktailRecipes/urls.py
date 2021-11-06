8beda48d3fab3385fb9a9465ba6f40b5ff2428f2
Matt Fuller <63671581+mdfull01@users.noreply.github.com>
2021-11-05 20:02:32 -0400

    Create details.html

--- AppBuilder9000/CocktailRecipes/urls.py
+++ AppBuilder9000/CocktailRecipes/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.cocktail_recipes_home, name='cocktail_recipes_home'),
     path('add-cocktail/', views.add_cocktail, name='cocktail_recipes_add_cocktail'),
     path('cocktail-list/', views.cocktail_list, name='cocktail_recipes_cocktail_list'),
+    path('<int:pk>/cocktail-detail/', views.cocktail_details, name='cocktail_recipes_details'),
 ]
