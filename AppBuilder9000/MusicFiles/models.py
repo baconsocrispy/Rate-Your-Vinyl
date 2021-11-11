dc87330397e482b2198eac55224bc27955b2bdf0
Cindy Lara <74997705+cindy123jl@users.noreply.github.com>
2021-11-11 10:28:12 -0800

    curly braces to parentheses

--- AppBuilder9000/MusicFiles/models.py
+++ AppBuilder9000/MusicFiles/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 # Create your models here.
 
-TYPE_CHOICES = {
+TYPE_CHOICES = (
     ('pre-mix', 'pre-mix'),
     ('post-mix/pre-master', 'post-mix/pre-master'),
     ('post-master', 'post-master'),
 
-}
+)
 
 class Files(models.Model):
     type = models.CharField(max_length=60, choices=TYPE_CHOICES)
