38af73e16c59d7fcdb0e5c71b9ebedb6c4537a06
JMC1027 <87082922+JMC1027@users.noreply.github.com>
2021-11-11 14:38:58 -0500

    Story 1

--- AppBuilder9000/Snowboarding/models.py
+++ AppBuilder9000/Snowboarding/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+Ryder_Name = [
+('Style','Style'),
+]
+
+class Ryder(models.Model):
+    First_Name = models.CharField(max_length=60, default="", blank=True, null=False)
+    Last_Name = models.CharField(max_length=60, default="", blank=True, null=False)
+    Style = models.CharField(max_length=60, default="", blank=True, null=False)
+    Sponsor = models.CharField(max_length=60, default="", blank=True, null=False)
+
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.First_Name + ' ' + self.Last_Name
+
--- AppBuilder9000/Snowboarding/views.py
+++ AppBuilder9000/Snowboarding/views.py
@@ -1,4 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Ryder
+from .forms import Ryder_Forms
 
 def home(request):
     return render(request, "SnowboardingHome.html")
+
+def AddNewRyder(request):
+    form = Ryder(data=request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return redirect('AddNewRyder')
+    content = {'form': form}
+    return render(request, 'New_Ryder.html', content)
+
