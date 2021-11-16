361838f111cdd949c61e372bfe7f716574e04de5
alishawillman <72734094+alishawillman@users.noreply.github.com>
2021-11-15 22:08:31 -0700

    Created model, objects manager, and model form.

--- AppBuilder9000/HotSprings/views.py
+++ AppBuilder9000/HotSprings/views.py
@@ -1,7 +1,28 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse
+from .form import hotspringsForm
+from .models import HotSprings
+
 
 
 def hotsprings_home(request):
     # render method takes the request object and template name as arguments
     # returns httpResponse object with rendered text.
     return render(request, 'hotsprings_home.html')
+
+
+def add_hotsprings(request):
+    form = hotspringsForm(data=request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('hotsprings_list')
+    else:
+        print(form.errors)
+        form = hotspringsForm()
+        context = {'form' : form}
+    return render(request, 'hotsprings_create.html', context)
+
+def list_hotsprings(request):
+    hotsprings = HotSprings.objects.all()
+    return render(request, 'hotsprings_list.html', {'hotsprings': hotsprings})
+
