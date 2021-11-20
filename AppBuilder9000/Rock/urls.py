38ae24cd8b44bf22d1f0d2525c800f852a6615df
rogue1882 <88951456+rogue1882@users.noreply.github.com>
2021-11-20 17:45:44 -0500

    Created App for the site 80's hard rock

--- AppBuilder9000/Rock/urls.py
+++ AppBuilder9000/Rock/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('RocksHome', views.RocksHome, name='RocksHome'),
     path('Rock_Create', views.Rock_Create, name='Rock_Create'),
-
+    path('List', views.HardRock_List, name='List'),
 
 ]
--- AppBuilder9000/Rock/views.py
+++ AppBuilder9000/Rock/views.py
@@ -10,6 +10,10 @@ def RocksHome(request):
     return render(request, 'Rock/RocksHome.html')
 
 
+def HardRock_List(request):
+    return render(request, 'Rock/HardRock_List.html')
+
+
 def Rock_Create(request):
     form = HardRockForm(data=request.POST or None)
     if request.method == 'POST':
