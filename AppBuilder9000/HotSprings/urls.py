f38d859e4c8c025017e350ba52efa0b86a958619
alishawillman <72734094+alishawillman@users.noreply.github.com>
2021-11-22 17:54:19 -0700

    Clicking a link on the list page to go to the details page

--- AppBuilder9000/HotSprings/urls.py
+++ AppBuilder9000/HotSprings/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('', views.hotsprings_home, name='hotsprings_home'),
     path('hotsprings_create/', views.add_hotsprings, name='hotsprings_create'),
     path('hotsprings_list/', views.list_hotsprings, name='hotsprings_list'),
-    path('hotsprings_details/', views.hotsprings_details, name='hotsprings_details'),
+    path('<int:pk>/hotsprings_details/', views.hotsprings_details, name='hotsprings_details'),
 
 ]
