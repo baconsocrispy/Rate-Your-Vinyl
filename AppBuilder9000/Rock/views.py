500c85b1bedf7bd12c4468968ff84b202ad95f5d
rogue1882 <88951456+rogue1882@users.noreply.github.com>
2021-11-23 20:59:04 -0500

    Created App for the site 80's hard rock

--- AppBuilder9000/Rock/views.py
+++ AppBuilder9000/Rock/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, redirect
+from tkinter import Entry
+
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import HardRock
 from .forms import HardRockForm
 
@@ -6,12 +8,14 @@ from .forms import HardRockForm
 # Create your views here.
 
 
+
 def RocksHome(request):
     return render(request, 'Rock/RocksHome.html')
 
 
 def HardRock_List(request):
-    return render(request, 'Rock/HardRock_List.html')
+    list = HardRock.objects.all()
+    return render(request, 'Rock/HardRock_List.html', {'list':list})
 
 
 def Rock_Create(request):
@@ -22,3 +26,25 @@ def Rock_Create(request):
             return redirect('RocksHome')
     content = {'form': form}
     return render(request, 'Rock/Rock_Create.html', content)
+
+def HardRock_Details(request):
+    return render(request, 'Rock/HardRock_Details.html')
+
+def admin_console(request):
+    from AppBuilder9000 import Rock
+    HardRock = Rock.objects.all()
+    return render(request, 'Rock/HardRock_List.html', {'Rock': Rock})
+
+def details(request, pk):
+    pk = int(pk)
+    item = get_object_or_404(HardRock, pk=pk)
+    form = HardRockForm(data=request.POST or None, instance=item)
+    if request.method == 'POST':
+        if form.is_valid():
+            form2 = form.save(commit=False)
+            form2.save()
+            return redirect('admin_console')
+        else:
+            print(form.errors)
+    else:
+        return render(request, 'Rock/HardRock_List.html', {'form' : form})
