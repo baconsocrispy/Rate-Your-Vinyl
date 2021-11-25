9451704829d3d8f754f3f1e02c7de987588727d3
DevXerxes <87164130+DevXerxes@users.noreply.github.com>
2021-11-24 17:30:15 -0800

    Finished Story #1- Build the basic app

--- AppBuilder9000/Masonry/views.py
+++ AppBuilder9000/Masonry/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
+from .forms import QuoteForm
+from .models import Quotes
 
 
 # Create your views here.
@@ -7,3 +9,38 @@ def masonry_home(request):
     # This render function will take the request argument, and return the html document as a response
 
     return render(request, 'Masonry/Masonry_home.html')
+
+
+def quote_console(request):
+    quotes = Quotes.objects.all()
+    return render(request, 'Masonry/quotes_page.html', {'quotes': quotes})
+
+
+def details(request, pk):
+    pk = int(pk)
+    item = get_object_or_404(Quotes, pk=pk)
+    form = QuoteForm(data=request.POST or None, instance=item)
+    if request.method == 'POST':
+        if form.is_valid():
+            form2 = form.save(commit=False)
+            form2.save()
+            return redirect('quote_console')
+        else:
+            print(form.errors)
+    else:
+        return render(request, 'Masonry/present_quote.html', {'form': form})
+
+
+def createQuote(request):
+    form = QuoteForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('quote_console')
+    else:
+        print(form.errors)
+        form = QuoteForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'Masonry/createQuote.html', context)
+
