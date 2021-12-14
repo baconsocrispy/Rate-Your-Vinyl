40f29191fed00f59401abab7cc1931767dabe186
Cindy Lara <74997705+cindy123jl@users.noreply.github.com>
2021-12-13 16:43:08 -0800

    Added name and topic to sign in template

--- AppBuilder9000/CampingSupplies/views.py
+++ AppBuilder9000/CampingSupplies/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import get_object_or_404, redirect, render
 
 
 def Camping_Supplies_Home(request):
-    return render(request, 'Camping_Supplies_Home.html'),
+    return render(request, 'Camping_Supplies_Home.html')
