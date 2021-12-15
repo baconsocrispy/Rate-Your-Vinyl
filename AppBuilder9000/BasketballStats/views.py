cbffa0954aefe502a2a23313ac9550f0a6d812ad
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-12-15 14:16:16 -0800

    Change location of render request to home.html

--- AppBuilder9000/BasketballStats/views.py
+++ AppBuilder9000/BasketballStats/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 
 # Create your views here.
-def index(request):
-    return render(request, 'index')
+def home(request):
+    return render(request, 'BasketballStats/BasketballStats_home.html')
