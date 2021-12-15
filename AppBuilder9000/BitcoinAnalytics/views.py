ca461bfa3cff4ca7817dfbcfaa7bbff8998795de
CrewsControlSolutions <92337423+CrewsControlSolutions@users.noreply.github.com>
2021-12-15 12:52:10 -0700

    Added a new model and made model-related changes to other files

--- AppBuilder9000/BitcoinAnalytics/views.py
+++ AppBuilder9000/BitcoinAnalytics/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .forms import CompetitorForm
 
 
 def home(request):
     return render(request, 'BitcoinAnalytics/bitcoin_analytics_home.html')
+
+
+def create_competitor(request):
+    form = CompetitorForm(data=request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return redirect('bitcoin_analytics_home')
+    content = {'form': form}
+    return render(request, 'BitcoinAnalytics/bitcoin_analytics_add_competitor.html', content)
