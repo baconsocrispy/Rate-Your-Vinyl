f018082ee4ebd68b11a74f190eeadab616b88063
CrewsControlSolutions <92337423+CrewsControlSolutions@users.noreply.github.com>
2021-12-15 17:18:53 -0700

    Added html file and modified other files to display all database items.

--- AppBuilder9000/BitcoinAnalytics/models.py
+++ AppBuilder9000/BitcoinAnalytics/models.py
@@ -6,7 +6,7 @@ class Competitor(models.Model):
     price = models.DecimalField(max_digits=15, decimal_places=2)
     linkedIn = models.CharField(max_length=100)
 
-    Accounts = models.Manager()
+    Competition = models.Manager()
 
     # allows references to a specific competitor as the person's name, not the primary key
     def __str__(self):
--- AppBuilder9000/BitcoinAnalytics/urls.py
+++ AppBuilder9000/BitcoinAnalytics/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='bitcoin_analytics_home'),
-    path('add_competitor/', views.create_competitor, name='create')
+    path('add_competitor/', views.create_competitor, name='create'),
+    path('board/', views.show_competition, name='board')
 ]
