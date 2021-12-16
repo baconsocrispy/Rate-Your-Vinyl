f88450d05f4edcd30ad9a9ddc38e406cbd2282e7
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-12-16 12:15:41 -0800

    Add player stats page to url path

--- AppBuilder9000/BasketballStats/urls.py
+++ AppBuilder9000/BasketballStats/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='basketball_stats_home'),
     path('add_player/', views.create_player, name='basketball_stats_create'),
+    path('players/', views.player_stats, name='basketball_stats_players'),
 ]
