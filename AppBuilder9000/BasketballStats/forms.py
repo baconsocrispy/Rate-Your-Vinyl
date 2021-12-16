8e90a19c082a88f5825f1c662b716491b646a9eb
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-12-15 17:19:01 -0800

    Change fields to '__all__' to simplify code

--- AppBuilder9000/BasketballStats/forms.py
+++ AppBuilder9000/BasketballStats/forms.py
@@ -5,4 +5,4 @@ from AppBuilder9000.BasketballStats.models import Players
 class PlayersForm(ModelForm):
     class Meta:
         model = Players
-        fields = ['name', 'seasons_played', 'mvp_awards', 'championships', 'hall_of_fame']
+        fields = '__all__'
