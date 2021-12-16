6dd9625510c2a7ae4c7a7e49da9b62c7bb856e71
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-12-15 18:29:35 -0800

    Update integer fields in model to have default values, so that they appear in database

--- AppBuilder9000/BasketballStats/models.py
+++ AppBuilder9000/BasketballStats/models.py
@@ -4,9 +4,9 @@ from django.db import models
 # Create your models here.
 class Players(models.Model):
     name = models.CharField(max_length=50)
-    seasons_played = models.IntegerField
-    mvp_awards = models.IntegerField
-    championships = models.IntegerField
+    seasons_played = models.IntegerField(default='')
+    mvp_awards = models.IntegerField(default='')
+    championships = models.IntegerField(default='')
 
     Player = models.Manager()
 
