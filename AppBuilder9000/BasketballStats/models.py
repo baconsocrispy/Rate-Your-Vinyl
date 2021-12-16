863575fdfb4a4b917c52f3f9d57a2b4e4c640944
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-12-15 17:43:27 -0800

    Update model

--- AppBuilder9000/BasketballStats/models.py
+++ AppBuilder9000/BasketballStats/models.py
@@ -2,18 +2,11 @@ from django.db import models
 
 
 # Create your models here.
-HALL_OF_FAME = [
-    ('YES', 'Yes'),
-    ('NO', 'No'),
-]
-
-
 class Players(models.Model):
     name = models.CharField(max_length=50)
-    seasons_played = models.IntegerField(default='')
-    mvp_awards = models.IntegerField(default='')
-    championships = models.IntegerField(default='')
-    hall_of_fame = models.CharField(max_length=3, choices=HALL_OF_FAME)
+    seasons_played = models.IntegerField
+    mvp_awards = models.IntegerField
+    championships = models.IntegerField
 
     Player = models.Manager()
 
