1ecb3f1c8fa843207b2d3d84ae890fbadb89d439
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-12-15 17:04:36 -0800

    Create Players model

--- AppBuilder9000/BasketballStats/models.py
+++ AppBuilder9000/BasketballStats/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+class Players(models.Model):
+    name = models.CharField(max_length=50)
+    seasons_played = models.IntegerField
+    mvp_awards = models.IntegerField
+    championships = models.IntegerField
+    hall_of_fame = models.BooleanField
+
+    Player = models.Manager()
+
+    def __str__(self):
+        return self.name
