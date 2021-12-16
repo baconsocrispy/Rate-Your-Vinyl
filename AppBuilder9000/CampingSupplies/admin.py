f8d33d72a24d587267937ec07426163a74449c1e
Henry Lee <63754132+CometSoftwareDesign@users.noreply.github.com>
2021-12-15 21:08:37 -0500

    done with story

--- AppBuilder9000/CampingSupplies/admin.py
+++ AppBuilder9000/CampingSupplies/admin.py
@@ -2,9 +2,6 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-
-# Register your models here.
 from .models import Tent
-
-
 admin.site.register(Tent)
+
