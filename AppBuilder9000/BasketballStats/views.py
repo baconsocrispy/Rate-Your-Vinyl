2c599c1445ab46cc28e3a3a338672155d678c350
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-12-16 16:45:11 -0800

    Add player delete view

--- AppBuilder9000/BasketballStats/views.py
+++ AppBuilder9000/BasketballStats/views.py
@@ -39,3 +39,12 @@ def player_edit(request, pk):
             return redirect('basketball_stats_players')
     context = {'form': form}
     return render(request, 'BasketballStats/BasketballStats_edit.html', context)
+
+
+def player_delete(request, pk):
+    item = get_object_or_404(Players, pk=pk)
+    form = PlayersForm(data=request.POST or None, instance=item)
+    if request.method == 'POST':
+        item.delete()
+        return redirect('basketball_stats_player')
+    return render(request, 'BasketballStats/BasketballStats_delete.html', {'item': item, 'form': form})
