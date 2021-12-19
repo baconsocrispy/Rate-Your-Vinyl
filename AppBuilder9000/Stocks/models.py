e3c5c27d0dd4bb34467ca6ad772673737ad58f96
TnTien <nguyen.tien25@gmail.com>
2021-12-19 13:22:46 -0700

    Created model and modelForm. Created page for user input and to save to database.

--- AppBuilder9000/Stocks/models.py
+++ AppBuilder9000/Stocks/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Stocks(models.Model):
+    ticker = models.CharField(primary_key=True, max_length=5)
+    name = models.CharField(max_length=100)
+
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.ticker
+
--- AppBuilder9000/Stocks/urls.py
+++ AppBuilder9000/Stocks/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.stocks_home, name='stocks_home'),
+    path('stocks_favorites/', views.favorites, name='stocks_favorites'),
+    path('stocks_add_favorites/', views.add_favorites, name='add_favorites'),
 ]
+
