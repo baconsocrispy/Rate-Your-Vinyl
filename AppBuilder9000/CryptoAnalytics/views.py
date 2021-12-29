5fbdf04a48fe5efb1abe71ea446a9b30198da271
mrwillis32 <83623388+mrwillis32@users.noreply.github.com>
2021-12-29 13:44:46 -0500

    Templates build update

--- AppBuilder9000/CryptoAnalytics/views.py
+++ AppBuilder9000/CryptoAnalytics/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.contrib import messages
+
+from django.shortcuts import render, redirect
 from .models import Post
+from .forms import UserRegisterForm
 
 
 def home(request):
@@ -11,3 +14,17 @@ def home(request):
 
 def about(request):
     return render(request, 'CryptoAnalytics/about.html', {'title': 'About'})
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('crypto_home')
+        else:
+            form = UserRegisterForm()
+        return render(request, 'user/register.html', {'form': form})
+
