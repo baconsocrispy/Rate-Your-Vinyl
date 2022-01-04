709c9a0a6ee99cc8ae7d85a3c4462cc58b1f3199
JaqLaw <93956690+JaqLaw@users.noreply.github.com>
2022-01-04 13:43:06 -0800

    Created a model, modelform, and MVT structure for saving items to database.

--- AppBuilder9000/PetAdoption/models.py
+++ AppBuilder9000/PetAdoption/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+# defining sex choices
+SEX_CHOICES = (
+    ('Male', 'Male'),
+    ('Female', 'Female')
+)
+
+
+class Pet(models.Model):
+    name = models.CharField(max_length=50)
+    species = models.CharField(max_length=50)
+    breed = models.CharField(max_length=50)
+    color = models.CharField(max_length=50)
+    weight = models.IntegerField()
+    sex = models.CharField(max_length=10, choices=SEX_CHOICES)
+    description = models.TextField(max_length=500)
+
+    # reference the pet by name
+    def __str__(self):
+        return self.name
+
+    # objects manager
+    Pets = models.Manager()
+
+
