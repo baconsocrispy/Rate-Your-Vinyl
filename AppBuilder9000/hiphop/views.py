28923ee10b1d376ab7643b7c50713cca7680d918
salsandoval <76705210+salsandoval@users.noreply.github.com>
2022-01-06 12:59:51 -0700

    fixed basic app problems.

--- AppBuilder9000/hiphop/views.py
+++ AppBuilder9000/hiphop/views.py
@@ -5,5 +5,5 @@ import json
 
 def hiphop_home(request):
     # this will return user to hip hop home page
-    return render(request, 'hiphop_home.html')
+    return render(request, 'Hiphop/hiphop_home.html')
 
