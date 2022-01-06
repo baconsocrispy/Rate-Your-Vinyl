41b1771fc481d9be07962a2594eaa5f0cf63c711
mjh2493 <mjh2493@gmail.com>
2022-01-05 21:34:23 -0500

    Story 2

--- AppBuilder9000/NYC_Guide/urls.py
+++ AppBuilder9000/NYC_Guide/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='nyc_guide_home'),
+    path('add/', views.add_rest, name='add_rest'),
 ]
--- AppBuilder9000/NYC_Guide/views.py
+++ AppBuilder9000/NYC_Guide/views.py
@@ -1,4 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Restaurants
+from .forms import RestaurantForm
 
 def home(request):
     return render(request, 'NYC_Guide/nyc_home.html')
+
+def add_rest(request):
+    form= RestaurantForm(data=request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return redirect('nyc_guide_home')
+    content = {'form': form}
+    return render(request, 'NYC_Guide/add_rest.html', content)
