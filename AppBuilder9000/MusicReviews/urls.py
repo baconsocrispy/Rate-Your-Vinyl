a906f0f7f2c26dc566be0edf3a19ddb980007d25
CarterT101 <92358332+CarterT101@users.noreply.github.com>
2022-01-05 18:52:24 -0800

    Created multiple views including a function display reviews, and a function to send the user back to the original home page of the appbuilder9000. Fixed up some CSS as well as adding new CSS to make the website look better. The user can now view all reviews made on the website by accessing the database.

--- AppBuilder9000/MusicReviews/urls.py
+++ AppBuilder9000/MusicReviews/urls.py
@@ -6,5 +6,9 @@ urlpatterns = [
     path('', views.music_reviews_home, name='music_reviews_home'),
     path('Create/', views.createReview, name='createReview'),
     path('review_view/', views.review_view, name='review_view'),
+    path('back_home/', views.back_home, name='back_home'),
+    path('<int:pk>/delete/', views.delete, name='delete'),
+    path('confirmDelete/', views.confirmDelete, name='confirmDelete'),
+    path('<int:pk>/display_reviews/', views.display_reviews, name='display_reviews'),
 
 ]
