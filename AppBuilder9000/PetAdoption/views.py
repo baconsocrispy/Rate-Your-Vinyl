748ae7a9f34e3d4a007901349ac804474172733d
JaqLaw <93956690+JaqLaw@users.noreply.github.com>
2022-01-07 13:45:10 -0800

    Story #6 - Beautiful soup setup

--- AppBuilder9000/PetAdoption/views.py
+++ AppBuilder9000/PetAdoption/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Pet
 from .forms import PetForm
+import requests
+from bs4 import BeautifulSoup
 
 
 def pet_adoption_home(request):
@@ -66,4 +68,27 @@ def pet_adoption_delete(request, pk):
         return redirect('pet_adoption_all')
     context = {'item': item, 'form': form}
     return render(request, 'PetAdoption/PetAdoption_deleteconfirm.html', context)
+
+
+# Use BeautifulSoup to scrape adoption data
+def pet_adoption_statistics(request):
+    pet_list = []
+    page = requests.get("https://humanepro.org/page/pets-by-the-numbers")
+    soup = BeautifulSoup(page.content, 'html.parser')
+    # find the correct table (Shelter & Rescue group Estimates at bottom of page
+    table = soup.find_all('table')[4]
+    # choose the rows we want (just cats & Dogs)
+    rows = table.find_all('tr')[2:]
+    # get only the cells we want out of the row (columns 1,2,3,5)
+    for tr in rows:
+        td = []
+        for j in [0, 1, 2, 4]:
+            td.append(tr.find_all('td')[j])
+        strings = [i.text for i in td]
+        cells = strings
+        pet_list.append(cells)
+    context = {'pet_list': pet_list}
+    print(pet_list)
+    return render(request, 'PetAdoption/PetAdoption_statistics.html', context)
+
 
