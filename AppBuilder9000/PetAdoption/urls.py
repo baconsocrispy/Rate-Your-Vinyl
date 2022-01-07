cff446259eef1370bb2aea186298f46366a31d62
JaqLaw <93956690+JaqLaw@users.noreply.github.com>
2022-01-06 17:38:34 -0800

    Delete function

--- AppBuilder9000/PetAdoption/urls.py
+++ AppBuilder9000/PetAdoption/urls.py
@@ -4,10 +4,12 @@ from . import views
 
 urlpatterns = [
     path('', views.pet_adoption_home, name='pet_adoption_home'),
-    path('/list/', views.pet_adoption_list, name='pet_adoption_list'),
-    path('/dogs/', views.pet_adoption_dogs, name='pet_adoption_dogs'),
-    path('/cats/', views.pet_adoption_cats, name='pet_adoption_cats'),
-    path('/other/', views.pet_adoption_other, name='pet_adoption_other'),
-    path('/all/', views.pet_adoption_all, name='pet_adoption_all'),
+    path('list/', views.pet_adoption_list, name='pet_adoption_list'),
+    path('dogs/', views.pet_adoption_dogs, name='pet_adoption_dogs'),
+    path('cats/', views.pet_adoption_cats, name='pet_adoption_cats'),
+    path('other/', views.pet_adoption_other, name='pet_adoption_other'),
+    path('all/', views.pet_adoption_all, name='pet_adoption_all'),
     path('<int:pk>/details/', views.pet_adoption_details, name='pet_adoption_details'),
+    path('<int:pk>/edit/', views.pet_adoption_edit, name='pet_adoption_edit'),
+    path('<int:pk>/delete/', views.pet_adoption_delete, name='pet_adoption_delete'),
 ]
