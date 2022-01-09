215d1b23af263f2a2e5f53ee2aec69ff07089250
mjh2493 <mjh2493@gmail.com>
2022-01-09 17:35:37 -0500

    Story 3

--- AppBuilder9000/NYC_Guide/urls.py
+++ AppBuilder9000/NYC_Guide/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='nyc_guide_home'),
     path('add/', views.add_rest, name='add_rest'),
+    path('all/', views.all_rest, name='all_rest'),
 ]
