1769707bc265cb800fb0f1feb77805091ff9a5b6
salsandoval <76705210+salsandoval@users.noreply.github.com>
2022-01-10 12:45:55 -0700

    changed my

--- AppBuilder9000/hiphop/views.py
+++ AppBuilder9000/hiphop/views.py
@@ -1,31 +1,24 @@
-from django.shortcuts import render, redirect, get_object_or_404
 import requests
 import json
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
-
-from .models import choose
-import requests
-import json
+from .models import Choice
+from .forms import ChooseForm
 
 def hiphop_home(request):
     # this will return user to hip hop home page
     return render(request, 'Hiphop/hiphop_home.html')
 
-def createChoose(request):
-    form = ChooseForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-        return redirect('hiphop_home')
-    else:
-        print(form.errors)
-        form = ChooseForm()
-    context = {
-        'form': form,
-    }
+def create_choose(request):
+    form = ChooseForm(data=request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return redirect('hiphop_home')
+    context = {'form': form}
     return render(request, 'Hiphop/hiphop_create.html', context)
 
 
-def choose_view(request):
-    choose = choose.objects.all()
-    return render(request, 'hiphop_view.html', {'choose': choose})
+#def choose_view(request):
+ #   choose = choose.objects.all()
+  #  return render(request, 'hiphop_view.html', {'choose': choose})
