bbc3ec1f9a3cdcd17285ef64ed4850780d4d284d
HOLMIJO <91238285+HOLMIJO@users.noreply.github.com>
2022-01-11 16:53:17 -0600

    Updated html tags renamed folder

--- AppBuilder9000/WeatherBall/urls.py
+++ AppBuilder9000/WeatherBall/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('WeatherBall/weatherhome.html', views.weather_home),
+    path('', views.weather_home, name='weather_home'),
 ]
