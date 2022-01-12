1e44e499941f76894b9a669870d85d5327a5dd1c
Daniel Alvarez <dannymalvarez@gmail.com>
2022-01-12 10:02:52 -0500

    Add a way to submit your city and other relevant info to save to db

--- AppBuilder9000/BestCities/urls.py
+++ AppBuilder9000/BestCities/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.Best_Cities_home, name='Best_Cities_home'),
+    path('', views.Best_Cities_home, name='Best_Cities_home'), #if there is no path in the url it send them to my site's home page
+    path('create/', views.Best_Cities_create, name='Best_Cities_create'), #this calls the function to create the create page
     ]
