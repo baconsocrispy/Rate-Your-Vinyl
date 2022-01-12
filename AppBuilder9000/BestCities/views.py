f8fadac563bb67f133c21cb046a0b0eb3e9f816a
Daniel Alvarez <dannymalvarez@gmail.com>
2022-01-12 16:29:50 -0500

    Pushing commits for instructor review

--- AppBuilder9000/BestCities/views.py
+++ AppBuilder9000/BestCities/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import PlacesForm
-
 from .models import Places
 
 def Best_Cities_home(request): #function to render the home page
@@ -22,3 +21,7 @@ def Best_Cities_create(request): #function to add a city to the database
 def Best_Cities_topcities(request):
     topC = Places.objects.all()
     return render(request, 'BestCities/Best_Cities_topcities.html', {'topC': topC})
+
+def Best_Cities_details(request, pk):
+    item = get_object_or_404(Places, pk=pk)
+    return render(request, 'BestCities/Best_Cities_details.html', {'item': item})
