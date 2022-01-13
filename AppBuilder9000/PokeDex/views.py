0e24c99c69b08ca1e07d5b9c7070bd439dce6003
Canadianfaller7 <canadianfaller7@gmail.com>
2022-01-12 18:08:03 -0700

    Made a details page to show info about pokemon when their name is selected and updated some styling

--- AppBuilder9000/PokeDex/views.py
+++ AppBuilder9000/PokeDex/views.py
@@ -1,12 +1,14 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import PokemonForm
 from .models import Pokemon
 
 
-# Create your views here.
+# All of these functions here will need to be added to the urls.py files to be able to make them work and to call them
 
+# this is just so we can go back to the home page when you click home on the navbar
 def pokeDexHome(request):
     return render(request, 'PokeDex/PokeDex_home.html')
+
 # here we are making it so that we are getting the info from the PokemonForm and saving the info to the database
 # if the request.method == post and the form info is valid and then redirecting to the home page for now
 def addPokemon(request):
@@ -18,8 +20,16 @@ def addPokemon(request):
     content = { 'form': form }
     return render(request, 'PokeDex/AddPokemon_form.html', content)
 
+# this is how we are going to get the info from our models.py and then have that info returned and displayed onto
+# hour showPokemon.html file
 def show_pokemon(request):
     show_pokemon = Pokemon.object.all()
 
     context = {'show_pokemon': show_pokemon}
-    return render(request, "PokeDex/PokeDex_showPokemon.html", context)
+    return render(request, 'PokeDex/PokeDex_showPokemon.html', context)
+
+# this is how we end up getting the primary key from our DB and use it to get and show the details when the pokemon name is selected
+def pokemon_details(request, pk):
+    details = get_object_or_404(Pokemon, pk=pk)
+    context = {'details': details}
+    return render(request, 'PokeDex/pokemonDetails.html', context)
