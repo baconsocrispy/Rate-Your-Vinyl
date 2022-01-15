9ca842626b97550f4d7b9e8a19029f1385549d1f
HOLMIJO <91238285+HOLMIJO@users.noreply.github.com>
2022-01-14 20:40:34 -0600

    Updated html pages and edited views and urls.

--- AppBuilder9000/WeatherBall/views.py
+++ AppBuilder9000/WeatherBall/views.py
@@ -37,11 +37,21 @@ def weather_edit(request, pk):
     if request.method=='POST':
         if form.is_valid():
             form.save()
-            return redirect('weather_details')
-    else:
-        print(form.errors)
-        form = UsersForm()
+            return redirect('weather_home')
+        else:
+            print(form.errors)
+            form = UsersForm()
     context = {
         'form': form
     }
     return render(request, 'WeatherBall/weatheredit.html', context)
+
+
+def weather_delete(request, pk):
+    pk = int(pk)
+    item = get_object_or_404(Users, pk=pk)
+    if request.method == 'POST':
+        item.delete()
+        return redirect('weather_db')
+    context = {"item": item,}
+    return render(request, "WeatherBall/weatherdelete.html", context)
