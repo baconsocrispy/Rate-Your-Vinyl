a90457a0ec6f3de58d80b4d45734938f1c56739e
HOLMIJO <91238285+HOLMIJO@users.noreply.github.com>
2022-01-18 13:24:52 -0600

    Updated edited views

--- AppBuilder9000/WeatherBall/views.py
+++ AppBuilder9000/WeatherBall/views.py
@@ -3,6 +3,7 @@ from .models import Users
 from .forms import UsersForm
 from bs4 import BeautifulSoup
 import requests
+import pandas as pd
 
 def weather_home(request):
     return render(request, 'WeatherBall/weatherhome.html')
@@ -56,13 +57,14 @@ def weather_delete(request, pk):
     return render(request, 'WeatherBall/weatherdelete.html', {'item': item, 'form': form})
 
 def weather_scraping(request):
-    seven_day_forecast = []
+    seven_day_forecast = [] #List to store days of the week
     page = requests.get("https://forecast.weather.gov/MapClick.php?lat=35.3434&lon=-90.2983")
     soup = BeautifulSoup(page.content, 'html.parser')
     seven_day = soup.find(id="seven-day-forecast")
     forecast_items = seven_day.find_all(class_="tombstone-container")
     for i in forecast_items:
-        forecast = i.find('p').get_text()
-        seven_day_forecast.append(forecast)
+        i = forecast_items.find('p').get_text()
+        seven_day_forecast.append(i)
+
     print(seven_day_forecast)
     return render(request, 'WeatherBall/weatherscraping.html')
