2883ab710764c526b51af6143a74e0f111b85ce8
HOLMIJO <91238285+HOLMIJO@users.noreply.github.com>
2022-01-17 19:46:30 -0600

    Updated edited views and html for scraping

--- AppBuilder9000/WeatherBall/views.py
+++ AppBuilder9000/WeatherBall/views.py
@@ -56,10 +56,11 @@ def weather_delete(request, pk):
     return render(request, 'WeatherBall/weatherdelete.html', {'item': item, 'form': form})
 
 def weather_scraping(request):
-    page = requests.get("https://forecast.weather.gov/MapClick.php?lat=37.7772&lon=-122.4168")
+    page = requests.get("https://forecast.weather.gov/MapClick.php?lat=35.3434&lon=-90.2983")
     soup = BeautifulSoup(page.content, 'html.parser')
     seven_day = soup.find(id="seven-day-forecast")
     forecast_items = seven_day.find_all(class_="tombstone-container")
     tonight = forecast_items[0]
+    soup.find(tonight)
     print(tonight)
     return render(request, 'WeatherBall/weatherscraping.html')
