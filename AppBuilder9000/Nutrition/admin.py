8615c8d8ad582c0e021f246441bc21057c6973fc
Alain Lipowicz <alainsoftwaredev@gmail.com>
2022-01-18 16:34:19 -0500

    signed in and added topic

--- AppBuilder9000/Nutrition/admin.py
+++ AppBuilder9000/Nutrition/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import PersonalizedNutrition, Account
+from .forms import AccountForm, NutritionalQuery
+
--- AppBuilder9000/Nutrition/views.py
+++ AppBuilder9000/Nutrition/views.py
@@ -1,9 +1,20 @@
 
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
+
+from .models import Account, PersonalizedNutrition
+from .forms import AccountForm, NutritionalQuery
 # Create your views here.
 
 def nutrition_home(request):
     return render(request, 'Nutrition/Nutrition_home.html')
 
 
+def create_account(request):
+    form = AccountForm(data=request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    content = {'form': form}
+    return render(request, 'Nutrition/Nutrition_createnewaccount.html', content)
