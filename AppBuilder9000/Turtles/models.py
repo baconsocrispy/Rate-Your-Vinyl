62d10635cd470d3e224db795b0c8521cd181dcca
NateC85 <90124755+NateC85@users.noreply.github.com>
2022-01-28 20:52:14 -0700

    Finished model to create inputs.

--- AppBuilder9000/Turtles/models.py
+++ AppBuilder9000/Turtles/models.py
@@ -3,21 +3,22 @@ from django.db import models
 # Defining a new tuple object(species of turtles)
 TYPE_CHOICES = (
     ('Turtle', 'Turtle'),
-    ('Tortoise','Tortoise'),
-    ('Terrapin','Terrapin')
+    ('Tortoise', 'Tortoise'),
+    ('Terrapin', 'Terrapin')
 )
 
 # Defining a new tuple object(native region of turtles)
-LOCALITY_CHOICES =(
+LOCALITY_CHOICES = (
     ('Americas', 'Americas'),
     ('Europe', 'Europe'),
     ('Asia', 'Asia')
 )
 
+
 class Turtles(models.Model):
     name = models.CharField(max_length=30)
     species = models.CharField(max_length=15, choices=TYPE_CHOICES)
-    size = models.
+    description = models.TextField(max_length=150)
     locality = models.CharField(max_length=15, choices=LOCALITY_CHOICES)
 
     # Using dunder method to reference Turtles class
@@ -25,5 +26,4 @@ class Turtles(models.Model):
         return self.name
 
     # Objects manager
-    Turtles = models.Model()
-
+    Turtles = models.Manager()
