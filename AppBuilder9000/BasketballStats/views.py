99d44ffd495e23262860fe4da185c0072abf9209
jeffstop <84537804+jeffstop@users.noreply.github.com>
2022-02-04 13:46:38 -0800

    Update favorite team details function to display scrapped data. Will only scrap data for first item in database

--- AppBuilder9000/BasketballStats/views.py
+++ AppBuilder9000/BasketballStats/views.py
@@ -102,11 +102,14 @@ def abbreviate_name():
 def favorite_team_details(request, pk):
     details = get_object_or_404(Teams, pk=pk)
     team_list = Teams.Team.all()
-    atl = []
     for team in team_list:
+        name = team.team_name
         abbrev = abbreviate_name()
         abr_name = abbrev[team.team_name]
-        if details.team_name == abbrev[team.team_name]:
+        print(details.team_name)
+        print(name)
+        if details.team_name == name:
+            atl = []
             page = requests.get("https://www.basketball-reference.com/teams/" + str(abr_name) + "/2022.html")
             soup = BeautifulSoup(page.content, 'html.parser')
             meta = soup.find('div', id='meta')
