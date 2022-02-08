36d533a3e4d6cb2078795af70d4f5aa87c60fd89
PyLawrence <89617029+PyLawrence@users.noreply.github.com>
2022-02-08 12:36:56 -0500

    Created a model and gave the user the ability to add objects to the model.

--- AppBuilder9000/GameStats/urls.py
+++ AppBuilder9000/GameStats/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-path('', views.homepage, name="gamestats_home"),
+    path('', views.homepage, name="gamestats_home"),
+    path('add_game', views.addgame, name="gamestats_form"),
 ]
