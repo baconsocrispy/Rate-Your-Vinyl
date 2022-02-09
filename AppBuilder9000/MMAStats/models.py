ab16c36c094417672717d563b110ea035cbac35d
ruizd2218 <95021801+ruizd2218@users.noreply.github.com>
2022-02-09 14:27:49 -0800

    Added basic form information

--- AppBuilder9000/MMAStats/models.py
+++ AppBuilder9000/MMAStats/models.py
@@ -2,8 +2,11 @@ from django.db import models
 
 # creating initial model, this will allow us to input information in our
 # database about
-class Champion(models.Model):
+class Champions(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     defenses = models.IntegerField(default=0)
-    
+    p4p_rank = models.IntegerField(default=0)
+    record = models.CharField(max_length=20)
+
+    promotion = models.Manager()
--- AppBuilder9000/MMAStats/views.py
+++ AppBuilder9000/MMAStats/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+from .forms import ChampForm
 
 def MMAHome(request):
     return render(request, 'MMA_home.html')
+
+def MMACreate(request):
+    context = {}
+    form = ChampForm (request.POST or None, request.FILES or None)
+
+    # if form data is valid
+    if form.is_valid():
+        # save form data to our model
+        form.save()
+
+    context['form']= form
+    return render(request, "MMA_create.html", context)
