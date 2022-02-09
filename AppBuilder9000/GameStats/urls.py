7fc06d684a26ea2278a2b662e43b5e83a31ea419
PyLawrence <89617029+PyLawrence@users.noreply.github.com>
2022-02-09 16:52:37 -0500

    Added the ability to edit and delete elements from the database.

--- AppBuilder9000/GameStats/urls.py
+++ AppBuilder9000/GameStats/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('add_game', views.add_game, name="gamestats_form"),
     path('view_games', views.view_games, name="gamestats_viewall"),
     path('<int:pk>/details/', views.game_details, name="gamestats_one"),
+    path('<int:pk>/edit/', views.game_edit, name="gamestats_edit"),
+    path('<int:pk>/delete/', views.game_delete, name="gamestats_delete"),
 ]
