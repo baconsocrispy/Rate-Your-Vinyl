84933f3cd4eb8b6202789b2c1197160fca31abc8
DennisCodeGuru <denniskalmykovv@gmail.com>
2022-02-08 16:16:45 -0800

    initial commit

--- AppBuilder9000/MoveState/url.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.movestate_home, name='movestate_home'),
-]
--- AppBuilder9000/MoveState/views.py
+++ AppBuilder9000/MoveState/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 
-# Create your views here.
+
+def movestate_home(request):
+    return render(request, "MoveState/movestate_home.html")
