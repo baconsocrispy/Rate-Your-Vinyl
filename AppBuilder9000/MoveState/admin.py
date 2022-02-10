061cd6a60f9a2c89406abc9fdd57bdcd5e2a5661
DennisCodeGuru <denniskalmykovv@gmail.com>
2022-02-10 10:44:21 -0800

    Add model

--- AppBuilder9000/MoveState/admin.py
+++ AppBuilder9000/MoveState/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 
+from .models import Movestate
 
+admin.site.register(Movestate)
 # Register your models here.
--- AppBuilder9000/MoveState/urls.py
+++ AppBuilder9000/MoveState/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.movestate_home, name="movestate_home"),
-    # path('state_create/', views.state_create, name="state_create"),
-    # path('list_state/', views.all_state, name="list_state"),
+    path('add_state/', views.add_state, name="add_state"),
+    path('list_state/', views.list_state, name="list_state"),
+    # path('movestate_details/', views.movestate_details, name="movestate_details"),
     # path('admin_console/', views.admin_console, name="admin_console"),
 ]
