62454e77581a3a8622253e32eeee284704eda0e5
ruizd2218 <95021801+ruizd2218@users.noreply.github.com>
2022-02-10 13:03:20 -0800

    added the "stats" page which allows us to see all stats from the database.

--- AppBuilder9000/MMAStats/urls.py
+++ AppBuilder9000/MMAStats/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from django.conf import settings
+from django.conf.urls import url, include
 from . import views
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.MMAHome, name='MMA_Home'),
-    path('create', views.MMACreate, name='MMA_Create')
+    path('create/', views.MMACreate, name='MMA_Create'),
+    path('stats/', views.DisplayStats, name='MMA_Stats'),
 ]
 
 if settings.DEBUG:
