bd443bab0f7c08ed7cb3997fb60a84dbd4a51813
ruizd2218 <95021801+ruizd2218@users.noreply.github.com>
2022-02-09 17:16:16 -0800

    Fixed block tags and added UX feature

--- AppBuilder9000/MMAStats/views.py
+++ AppBuilder9000/MMAStats/views.py
@@ -1,17 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import ChampForm
 
 def MMAHome(request):
-    return render(request, 'MMA_home.html')
+    return render(request, 'MMAStats/MMA_home.html')
 
 def MMACreate(request):
-    context = {}
-    form = ChampForm (request.POST or None, request.FILES or None)
-
+    form = ChampForm (data=request.POST or None)
     # if form data is valid
     if form.is_valid():
         # save form data to our model
         form.save()
+        return redirect('MMA_Create')
 
-    context['form']= form
-    return render(request, "MMA_create.html", context)
+    context = {'form': form}
+    return render(request, "MMAStats/MMA_create.html", context)
