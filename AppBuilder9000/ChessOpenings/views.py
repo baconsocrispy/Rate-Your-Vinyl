5adf2bff184af2fe5e059c5417712555378bc1e4
djkalb <71739290+djkalb@users.noreply.github.com>
2022-02-15 13:27:11 -0800

    first commit for Create Model strange bug need to save so i can compare to master

--- AppBuilder9000/ChessOpenings/views.py
+++ AppBuilder9000/ChessOpenings/views.py
@@ -1,10 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import GamesForm
 
 
 def homepage(request):
     return render(request, 'ChessOpenings/chess_index.html')
 
 
+def add_game(request):
+    form = GamesForm(request.POST or None)
+    context = {'form': form}
+
+    if request.method == 'POST':
+        if form.is_valid():
+            temp = form.cleaned_data.get("GamesForm.__all__")
+            form.save(temp)
+            return redirect(homepage)
+    return render(request, "ChessOpenings/add_game.html", context)
+
+# Create your views here.
 
 
 # Create your views here.
