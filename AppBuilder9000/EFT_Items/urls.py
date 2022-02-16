bfaa4811c94139a9f4bd398ac77826f0a7b037a6
Borregito22 <93361779+Borregito22@users.noreply.github.com>
2022-02-16 13:22:31 -0700

    Created model form to input data

--- AppBuilder9000/EFT_Items/urls.py
+++ AppBuilder9000/EFT_Items/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.eft_home, name="eft_items_home"),
+    path('create/', views.eft_create_record, name="eft_create_record"),
 ]
--- AppBuilder9000/EFT_Items/views.py
+++ AppBuilder9000/EFT_Items/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import EFTItems
+from .forms import EFTForm
 
 
 # Create your views here.
 def eft_home(request):
     return render(request, 'EFT_Items/EFT_Items_home.html')
+
+
+def eft_create_record(request):
+    form = EFTForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('eft_create_record')
+    else:
+        print(form.errors)
+        form = EFTForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'EFT_Items/EFT_Items_create.html', context)
