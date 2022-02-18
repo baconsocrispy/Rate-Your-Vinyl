d3af06637ca3968a577e28587d544493a1abfde8
Borregito22 <93361779+Borregito22@users.noreply.github.com>
2022-02-17 20:43:11 -0700

    Created table to display items from database

--- AppBuilder9000/EFT_Items/urls.py
+++ AppBuilder9000/EFT_Items/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.eft_home, name="eft_items_home"),
     path('create/', views.eft_create_record, name="eft_create_record"),
+    path('display_items/', views.eft_all_items, name="eft_all_items"),
 ]
