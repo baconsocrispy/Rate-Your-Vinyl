7b405e98b9ed0718a71fa2d5b2a73c270a9a792f
Borregito22 <93361779+Borregito22@users.noreply.github.com>
2022-02-21 12:49:52 -0700

    Created view function to display an individually selected item to a new template

--- AppBuilder9000/EFT_Items/urls.py
+++ AppBuilder9000/EFT_Items/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.eft_home, name="eft_items_home"),
     path('create/', views.eft_create_record, name="eft_create_record"),
     path('display_items/', views.eft_all_items, name="eft_all_items"),
+    path('<int:pk>/details/', views.eft_details, name="eft_item_details"),
 ]
