72173f60d2db82c24b530ab51f97603a9e2331ad
Albert Tulo <atuloiv@gmail.com>
2022-02-25 15:14:51 -0600

    Created template Vehicles_details.html to display more information on a vehicle when you click on a vehicle make while on vehicles_view.html.

--- AppBuilder9000/Vehicles/urls.py
+++ AppBuilder9000/Vehicles/urls.py
@@ -4,5 +4,6 @@ from .import views
 urlpatterns = [
     path('', views.vehiclesHome, name='Vehicles_home'),
     path('addVehicles/', views.addVehicles, name='Vehicles_add'),
-    path('/vehicles', views.vehicles_view, name='Vehicles_view'),
+    path('vehicles/', views.vehicles_view, name='Vehicles_view'),
+    path('<int:pk>/details/', views.vehicle_details, name='vehicle_details'),
 ]
