8f50f26e87b6a4f8ebd0b5d5ff3405a53c677202
Carson Cook <73494842+CarsonCookCode@users.noreply.github.com>
2022-03-08 12:47:47 -0700

    Working with home page, create page, and compare page with basic styling.

--- AppBuilder9000/Personality/urls.py
+++ AppBuilder9000/Personality/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.personality_home, name='personality_home'),
+    path('create/', views.personality_create, name='personality_create'),
+    path('compare/', views.personality_compare, name='personality_compare'),
 ]
--- AppBuilder9000/Personality/views.py
+++ AppBuilder9000/Personality/views.py
@@ -1,6 +1,15 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Person, ComparedPerson
+from .forms import PersonForm
 
 
 def personality_home(request):
     return render(request, 'Personality/personality_home.html')
+
+
+def personality_create(request):
+    return render(request, 'Personality/personality_create.html')
+
+
+def personality_compare(request):
+    return render(request, 'Personality/personality_compare.html')
