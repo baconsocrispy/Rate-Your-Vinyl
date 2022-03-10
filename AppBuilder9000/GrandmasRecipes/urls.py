8cc41570bc52f352a0afce6da15cef5d414607d0
atwitty1 <96742376+atwitty1@users.noreply.github.com>
2022-03-10 09:23:29 -0500

    Fixed Error with URL.py links

--- AppBuilder9000/GrandmasRecipes/urls.py
+++ AppBuilder9000/GrandmasRecipes/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from . import views
 
+# the directory of . is our current directory
+# so the above says import views.py from the current directory
+# ANATOMY OF A URL ROUTE:
+# ('pattern to watch for',method to call,"shortcut name")
 
 urlpatterns = [
-    path('', views.grandmas_recipes_home, name='GrandmasRecipes_home'),
-    path('/library', views.grandmas_recipes_library, name='GrandmasRecipes_library'),
-    path('/AddNew', views.grandmas_recipes_addNew, name='GrandmasRecipes_addnew'),
+    path('', views.grandmas_home, name='GrandmasRecipes_home'),
+    path('library', views.grandmas_library, name='GrandmasRecipes_library'),
+    path('AddNew', views.grandmas_addnew, name='GrandmasRecipes_addnew'),
 ]
--- AppBuilder9000/GrandmasRecipes/views.py
+++ AppBuilder9000/GrandmasRecipes/views.py
@@ -3,25 +3,23 @@ from .models import Recipes
 from .forms import RecipesForm
 
 
-def grandmas_recipes_home(request):
+def grandmas_home(request):
     return render(request, 'GrandmasRecipes/GrandmasRecipes_home.html')
 
 
-def grandmas_recipes_library(request):
-    grandmasrecipes_list = Recipes.GrandmasRecipes.all()
+def grandmas_library(request):
+    grandmasrecipes_list = Recipes.Recipe.all()
     context = {
         'Grandmasrecipes_list': grandmasrecipes_list
     }
     return render(request, 'GrandmasRecipes/GrandmasRecipes_Library.html', context)
 
 
-def grandmas_recipes_addNew(request):
+def grandmas_addnew(request):
     form = RecipesForm(data=request.POST or None)
     if request.method == 'POST':
         if form.is_valid():
             form.save()
             return redirect('GrandmasRecipes_home')
-    context = {
-        'form': form,
-    }
+    context = {'form': form}
     return render(request, 'GrandmasRecipes/GrandmasRecipes_AddNew.html', context)
