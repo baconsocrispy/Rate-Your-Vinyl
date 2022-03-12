a092d0a92d0ac778519a74ed75dc5054abcf6938
Sachi0216 <davis.elijah15@hotmail.com>
2022-03-11 18:24:57 -0600

    Models

--- AppBuilder9000/SuperCars/urls.py
+++ AppBuilder9000/SuperCars/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.SuperCarsHome, name='SuperCars_Home'),
+    path('CreateRecord/', views.CreateRecord, name='CreateRecord'),
 
 ]
--- AppBuilder9000/SuperCars/views.py
+++ AppBuilder9000/SuperCars/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse
+from .forms import SuperCarsForm
+from .models import SuperCars
 
 # Create your views here.
 
 def SuperCarsHome(request):
     return render(request, 'SuperCars/SuperCarsHome.html')
+
+
+
+def CreateRecord(request):
+    form = SuperCarsForm(data=request.POST or None)
+    if request.method=='POST':
+        if form.is_valid():
+            form.save()
+            return redirect('SuperCars_Home')
+        else:
+            print(form.errors)
+            form = SuperCarsForm()
+    context = {
+         'form': form,
+        }
+
+    return render(request, 'SuperCars/CreateRecord.html', context)
