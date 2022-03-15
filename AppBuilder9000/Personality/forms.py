136d1e660949123a15186c1f7fa2ace1391b353f
Carson Cook <73494842+CarsonCookCode@users.noreply.github.com>
2022-03-14 21:18:07 -0700

    Saved front end changes. Added test form functionality. Added extra buttons. Added chart image api.

--- AppBuilder9000/Personality/forms.py
+++ AppBuilder9000/Personality/forms.py
@@ -6,7 +6,7 @@ from .models import Person, SelectPerson
 class PersonForm(ModelForm):
     class Meta:
         model = Person
-        fields = '__all__'
+        fields = ['name', 'age', 'sex']
 
 
 class SelectPersonForm(ModelForm):
@@ -16,5 +16,25 @@ class SelectPersonForm(ModelForm):
 
 
 class JobSearchForm(forms.Form):
-    query = forms.CharField(label='Search:', max_length=50)
     location = forms.CharField(label='Location:', max_length=50)
+
+
+CHOICES = [(2, 'Strongly Agree'),
+           (1, 'Agree'),
+           (0, 'Neutral'),
+           (-1, 'Disagree'),
+           (-2, 'Strongly Disagree')]
+
+
+class TestForm(forms.Form):
+    questionO1 = forms.ChoiceField(label='I am full of ideas.', choices=CHOICES, widget=forms.RadioSelect)
+    questionC1 = forms.ChoiceField(label='I am always prepared.', choices=CHOICES, widget=forms.RadioSelect)
+    questionE1 = forms.ChoiceField(label='I am the life of the party.', choices=CHOICES, widget=forms.RadioSelect)
+    questionA1 = forms.ChoiceField(label='I have a soft heart.', choices=CHOICES, widget=forms.RadioSelect)
+    questionN1 = forms.ChoiceField(label='I get stressed out easily.', choices=CHOICES, widget=forms.RadioSelect)
+    questionO2 = forms.ChoiceField(label='I have a vivid imagination.', choices=CHOICES, widget=forms.RadioSelect)
+    questionC2 = forms.ChoiceField(label='I am exacting in my work.', choices=CHOICES, widget=forms.RadioSelect)
+    questionE2 = forms.ChoiceField(label='I talk to a lot of different people at parties.', choices=CHOICES, widget=forms.RadioSelect)
+    questionA2 = forms.ChoiceField(label='I take time out for others.', choices=CHOICES, widget=forms.RadioSelect)
+    questionN2 = forms.ChoiceField(label='I worry about things.', choices=CHOICES, widget=forms.RadioSelect)
+
--- AppBuilder9000/Personality/views.py
+++ AppBuilder9000/Personality/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Person
-from .forms import PersonForm, JobSearchForm
+from .forms import PersonForm, JobSearchForm, TestForm
+from .forms import CHOICES
 import requests
 import json
 from bs4 import BeautifulSoup
@@ -26,7 +27,8 @@ def personality_trait_info(request):
     print(extraversion)
     print(agreeableness)
     print(neuroticism)
-    content = {'info': info, 'openness': openness, 'conscientiousness': conscientiousness, 'extraversion': extraversion, 'agreeableness': agreeableness, 'neuroticism': neuroticism}
+    content = {'info': info, 'openness': openness, 'conscientiousness': conscientiousness,
+               'extraversion': extraversion, 'agreeableness': agreeableness, 'neuroticism': neuroticism}
     return render(request, 'Personality/personality_trait_info.html', content)
 
 
@@ -36,11 +38,60 @@ def personality_home(request):
 
 def personality_create(request):
     form = PersonForm(data=request.POST or None)
+    test = TestForm(data=request.POST or None)
+    print("1")
     if request.method == 'POST':
+        print("2")
         if form.is_valid():
-            form.save()
-            return redirect('../compare')
-    content = {'form': form}
+            print("3")
+            if test.is_valid():
+                print("4")
+                person_instance = form.save(commit=False)
+                o_score = 4
+                c_score = 4
+                e_score = 4
+                a_score = 4
+                n_score = 4
+                o1 = int(test.cleaned_data["questionO1"])
+                o2 = int(test.cleaned_data["questionO2"])
+                c1 = int(test.cleaned_data["questionC1"])
+                c2 = int(test.cleaned_data["questionC2"])
+                e1 = int(test.cleaned_data["questionE1"])
+                e2 = int(test.cleaned_data["questionE2"])
+                a1 = int(test.cleaned_data["questionA1"])
+                a2 = int(test.cleaned_data["questionA2"])
+                n1 = int(test.cleaned_data["questionN1"])
+                n2 = int(test.cleaned_data["questionN2"])
+                o_score += o1 + o2
+                c_score += c1 + c2
+                e_score += e1 + e2
+                a_score += a1 + a2
+                n_score += n1 + n2
+                print('o_score: ' + str(o_score))
+                print('c_score: ' + str(c_score))
+                print('e_score: ' + str(e_score))
+                print('a_score: ' + str(a_score))
+                print('n_score: ' + str(n_score))
+                o_average = (o_score/8) * 100
+                c_average = (c_score / 8) * 100
+                e_average = (e_score / 8) * 100
+                a_average = (a_score / 8) * 100
+                n_average = (n_score / 8) * 100
+                print(o_average)
+                print(c_average)
+                print(e_average)
+                print(a_average)
+                print(n_average)
+                person_instance.o_average_score = o_average
+                person_instance.c_average_score = c_average
+                person_instance.e_average_score = e_average
+                person_instance.a_average_score = a_average
+                person_instance.n_average_score = n_average
+                form.save()
+                print(person_instance.id)
+                url = '../' + str(person_instance.id)
+                return redirect(url)
+    content = {'form': form, 'test': test}
     return render(request, 'Personality/personality_create.html', content)
 
 
@@ -55,7 +106,17 @@ def personality_details(request, pk):
     form = JobSearchForm(data=request.POST or None)
     if request.method == 'POST':
         if form.is_valid():
-            query = form.cleaned_data['query']
+            if person.o_average_score >= person.c_average_score and person.o_average_score >= person.e_average_score and person.o_average_score >= person.a_average_score and person.o_average_score >= person.n_average_score:
+                query = 'creative'
+            elif person.c_average_score >= person.o_average_score and person.c_average_score >= person.e_average_score and person.c_average_score >= person.a_average_score and person.c_average_score >= person.n_average_score:
+                query = 'overtime'
+            elif person.e_average_score >= person.c_average_score and person.e_average_score >= person.o_average_score and person.e_average_score >= person.a_average_score and person.e_average_score >= person.n_average_score:
+                query = 'social'
+            elif person.a_average_score >= person.c_average_score and person.a_average_score >= person.e_average_score and person.a_average_score >= person.o_average_score and person.a_average_score >= person.n_average_score:
+                query = 'care'
+            elif person.n_average_score >= person.c_average_score and person.n_average_score >= person.e_average_score and person.n_average_score >= person.a_average_score and person.n_average_score >= person.o_average_score:
+                query = 'low stress'
+            #query = form.cleaned_data['query']
             location = form.cleaned_data['location']
 
             url = "https://indeed12.p.rapidapi.com/jobs/search"
@@ -75,7 +136,17 @@ def personality_details(request, pk):
                 job_list.append([job['title'], job['company_name'], job['location']])
             content = {'job_list': job_list, 'job_url': job_url}
             return render(request, 'Personality/personality_job_api.html', content)
-    content = {'person': person, 'form': form}
+
+    url = "https://image-charts.com/chart?chan=1200%2CeaseInSine&chbr=5&chco=3366FF&chd=t%3A"\
+          + str(person.o_average_score) + "%2C" + str(person.c_average_score) + "%2C" + str(person.e_average_score)\
+          + "%2C" + str(person.a_average_score) + "%2C" + str(person.n_average_score)\
+          + "&chf=b0%2Clg%2C90%2CEA469EFF%2C1%2C03A9F47C%2C0.4&chl=" + str(person.o_average_score)\
+          + "%7C" + str(person.c_average_score) + "%7C" + str(person.e_average_score) + "%7C"\
+          + str(person.a_average_score) + "%7C" + str(person.n_average_score)\
+          + "&chma=50%2C50%2C50%2C50&chs=700x450&cht=bvs&chtt=Big%205%20Personality&chxl=0%3A%7COpenness%7C" \
+            "Conscientiousness%7CExtroversion%7CAgreeableness%7CNeuroticism&chxt=x%2Cy"
+
+    content = {'person': person, 'form': form, 'url': url}
     return render(request, 'Personality/personality_details.html', content)
 
 
@@ -85,7 +156,7 @@ def personality_edit(request, pk):
     if request.method == 'POST':
         if form.is_valid():
             form.save()
-            return redirect('../../compare')
+            return redirect('..')
     content = {'person': person, 'form': form}
     return render(request, 'Personality/personality_edit.html', content)
 
