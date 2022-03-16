4c81f296d6cab1604f5abf11dc5395cf68fedda3
Sachi0216 <davis.elijah15@hotmail.com>
2022-03-15 19:45:01 -0500

    Adding created items to the base page.

--- AppBuilder9000/SuperCars/views.py
+++ AppBuilder9000/SuperCars/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render,get_object_or_404, redirect
 from django.http import HttpResponse
 from .forms import SuperCarsForm
 from .models import SuperCars
@@ -6,9 +6,24 @@ from .models import SuperCars
 
 
 def SuperCarsHome(request):
-    return render(request, 'SuperCars/SuperCarsHome.html')
+    products = SuperCars.objects.all()
+    return render(request, 'SuperCars/SuperCarsHome.html',{'products': products})
 
 
+
+def details(request, pk):
+    pk = int(pk)
+    item = get_object_or_404(SuperCars, pk=pk)
+    form = SuperCarsForm(data=request.POST or None, instance=item)
+    if request.method == 'POST':
+        if form.is_valid():
+            form2 = form.save(commit=False)
+            form2.save()
+            return redirect('SuperCars_Home')
+        else:
+            print(form.errors)
+    else:
+        return render(request, 'SuperCars/PresentRecord.html', {'form': form})
 
 def CreateRecord(request):
     form = SuperCarsForm(data=request.POST or None)
