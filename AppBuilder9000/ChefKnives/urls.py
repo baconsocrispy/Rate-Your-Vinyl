d92614172385c29342ac8b1411e55907526946ed
codyharris713 <83795096+codhharris713@users.noreply.github.com>
2022-03-17 10:01:27 -0700

    create model and views function

--- AppBuilder9000/ChefKnives/urls.py
+++ AppBuilder9000/ChefKnives/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="ChefKnives_Home"),
+    path(r'^$', views.home, name="ChefKnives_Home"),
     path('ChefKnives_Create/', views.chefknives_create, name="ChefKnives_Create"),
     path('ChefKnives_View/', views.chefknives_view, name="ChefKnives_View"),
 
--- AppBuilder9000/ChefKnives/views.py
+++ AppBuilder9000/ChefKnives/views.py
@@ -2,11 +2,21 @@ import requests
 from django.shortcuts import render, redirect
 from .forms import KnifeForm
 from .models import ChefKnives
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 # Create your views here.
 
 def home(request):
+    numbers_list = range(1, 1000)
+    page = request.GET.get('page', 1)
+    paginator = Paginator(numbers_list, 20)
+    try:
+        numbers = paginator.page(page)
+    except PageNotAnInteger:
+        numbers = paginator.page(1)
+    except EmptyPage:
+        numbers = paginator.page(paginator.num_pages)
     return render(request, 'ChefKnives/ChefKnives_Home.html')
 
 
