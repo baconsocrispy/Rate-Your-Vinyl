088f026e6c2c82719f2ba122b1aaa057c418ec5d
salxvador <sal.salvador@gmail.com>
2022-03-22 15:48:24 -0400

    Added model and updated styling

--- AppBuilder9000/HousingCosts/models.py
+++ AppBuilder9000/HousingCosts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class House(models.Model):
+    tagline = models.CharField(max_length=60)
+    city = models.CharField(max_length=25)
+    state = models.CharField(max_length=2)
+    bedrooms = models.PositiveSmallIntegerField()
+    bathrooms = models.PositiveSmallIntegerField()
+    squareFootage = models.PositiveIntegerField()
+    price = models.CharField(max_length=15)
+    notes = models.TextField()
+
+    def __str__(self):
+        return self.tagline
--- AppBuilder9000/HousingCosts/views.py
+++ AppBuilder9000/HousingCosts/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import House
+
 
 def housing_costs_home(request):
     return render(request, "HousingCosts/HousingCosts_home.html")
