6c8e26cc7f0d4b84a72824130e75cb7a1a5f124c
salxvador <sal.salvador@gmail.com>
2022-03-23 11:33:26 -0400

    added comments and Form Reset for HouseForm

--- AppBuilder9000/HousingCosts/forms.py
+++ AppBuilder9000/HousingCosts/forms.py
@@ -4,4 +4,5 @@ from .models import House
 class HouseForm(ModelForm):
     class Meta:
         model = House
+        # This indicates that we want all model fields in the form:
         fields = '__all__'
--- AppBuilder9000/HousingCosts/views.py
+++ AppBuilder9000/HousingCosts/views.py
@@ -6,13 +6,15 @@ from .forms import HouseForm
 def housing_costs_home(request):
     return render(request, "HousingCosts/HousingCosts_home.html")
 
-
+# Contains the modelForm
 def housing_costs_create(request):
     form = HouseForm()
     if request.method == 'POST':
         form = HouseForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('housing_costs_home')
+            # This resets the form contents so it is empty after clicking 'submit':
+            form = HouseForm()
+    # This points to our HouseForm modelForm:
     context = {'form': form}
     return render(request, "HousingCosts/HousingCosts_create.html", context)
