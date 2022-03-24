9c51eeae3280c7f82090515db861656275757f03
Josephbacona <85919565+Josephbacona@users.noreply.github.com>
2022-03-24 13:06:47 -0700

    create files

--- AppBuilder9000/HikingTrails/urls.py
+++ AppBuilder9000/HikingTrails/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.HikingTrails_home, name='HikingTrails_home'),
     path('HikingTrails_create', views.HikingTrails_create, name='HikingTrails_create'),
+    path('HikingTrails_display', views.HikingTrails_display, name='HikingTrails_display'),
 ]
--- AppBuilder9000/HikingTrails/views.py
+++ AppBuilder9000/HikingTrails/views.py
@@ -15,3 +15,9 @@ def HikingTrails_create(request):
             return redirect('HikingTrails_home')
     content = {'form': form}
     return render(request, 'HikingTrails/HikingTrails_create.html', content)
+
+
+def HikingTrails_display(request):
+    hiking_trails = Trails.Trail.all()
+    content = {'trails': hiking_trails}
+    return render(request, 'HikingTrails/HikingTrails_display.html', content)
