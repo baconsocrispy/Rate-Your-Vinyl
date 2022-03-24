025488b40a8fb3fd98bec0a9fd081302fc46e2b6
salxvador <sal.salvador@gmail.com>
2022-03-24 12:57:05 -0400

    Added working delete protocol

--- AppBuilder9000/HousingCosts/views.py
+++ AppBuilder9000/HousingCosts/views.py
@@ -39,12 +39,14 @@ def housing_costs_details(request, pk):
     return render(request, 'HousingCosts/HousingCosts_details.html', context)
 
 
+# This view uses the same template as the create view, and pre-populates the form with
+# the selected house's details. The submit button updates the info stored in DB.
 def housing_costs_edit(request, pk):
     house = get_object_or_404(House, pk=pk)
     form = HouseForm(instance=house)
 
     if request.method == 'POST':
-        #this is necessary so that we update existing record, rather than create a new one:
+        # this is necessary so that we update existing record, rather than create a new one:
         form = HouseForm(request.POST, instance=house)
         if form.is_valid():
             form.save()
@@ -53,3 +55,12 @@ def housing_costs_edit(request, pk):
 
     context = {'form': form}
     return render(request, 'HousingCosts/HousingCosts_create_update.html', context)
+
+
+def housing_costs_delete(request, pk):
+    house = get_object_or_404(House, pk=pk)
+    if request.method == 'POST':
+        house.delete()
+        return redirect(housing_costs_list)
+    context = {'details': house}
+    return render(request, 'HousingCosts/HousingCosts_delete.html', context)
