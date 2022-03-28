42a3b9c01c289063b1f9bde64907003042ae60c7
kevintzaw <88251025+kevintzaw@users.noreply.github.com>
2022-03-28 12:01:24 -0400

    Made a model and added migraion

--- AppBuilder9000/prowrestlers/models.py
+++ AppBuilder9000/prowrestlers/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.forms import ModelForm
+
 
 # Create your models here.
+class Wrestler(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    win_number = models.IntegerField()
