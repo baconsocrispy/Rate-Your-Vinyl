8f3418eedc6f81ef5ea33241a79d7f715bcb273c
Ny <nyarce805@gmail.com>
2022-03-27 21:09:33 -0500

    Story #4 - Details Page - Simple page to render individual idems details

--- AppBuilder9000/RecordCollection/urls.py
+++ AppBuilder9000/RecordCollection/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('Records_View/', views.records_view, name='records_view'),
     path('Records_Add/', views.records_add, name='records_add'),
     path('Records_Random/', views.records_random, name='records_random'),
+    path('<int:pk>/Records_Details/', views.records_details, name='records_details'),
 ]
