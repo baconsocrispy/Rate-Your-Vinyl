a284e03d52865437c3210e395ca3199b3e7bc9e1
kevintzaw <88251025+kevintzaw@users.noreply.github.com>
2022-03-29 14:25:15 -0400

    views and template app created

--- AppBuilder9000/prowrestlers/views.py
+++ AppBuilder9000/prowrestlers/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def wrestlers_home(request):
     return render(request, 'Prowrestlers/ProWrestling_home.html')
+
+def add_prowrestler(request):
+    form = ProWrestler_Form(request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+        return redirect('wrestlers_home')
+    else:
+        print(form.errors)
+        form = Prowrestler_Form
+        context = {'form': form}
+    return render(request, 'ProWrestling_create.html', context)
