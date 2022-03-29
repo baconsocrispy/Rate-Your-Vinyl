35527501c6ee5cf7398f6c895d76a37b58cafa0b
kevintzaw <88251025+kevintzaw@users.noreply.github.com>
2022-03-28 23:00:21 -0400

    views and template app created

--- AppBuilder9000/Prowrestlers/urls.py
+++ AppBuilder9000/Prowrestlers/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.wrestlers_home, name='wrestlers_home'),
+    path('', views.wrestlers_create, home='wrestlers_create'),
 ]
--- AppBuilder9000/prowrestlers/models.py
+++ AppBuilder9000/prowrestlers/models.py
@@ -7,3 +7,5 @@ class Wrestler(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     win_number = models.IntegerField()
+
+    object = models.manager()
