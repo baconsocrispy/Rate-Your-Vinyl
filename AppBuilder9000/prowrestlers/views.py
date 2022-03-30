8616abedcff6fd12696da716aefae62f4e0d7f8c
kevintzaw <88251025+kevintzaw@users.noreply.github.com>
2022-03-30 13:43:20 -0400

    Saving what's on Story 3

--- AppBuilder9000/prowrestlers/views.py
+++ AppBuilder9000/prowrestlers/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import wrestlerform
+from models import Wrestler
+
 from .models import Wrestler
 
 # Create your views here.
@@ -18,14 +20,6 @@ def add_prowrestler(request):
         context = {'form': form}
     return render(request, 'Prowrestlers/ProWrestling_createpage.html', context)
 
-def add_prowrestler(request):
-    form = wrestlerform(data=request.POST or None)
-    if request.method == 'POST':
-        if form.is_valid():
-            form.save()
-        return redirect('wrestlers_home')
-    else:
-        print(form.errors)
-        form = wrestlerform
-        context = {'form': form}
-    return render(request, 'Prowrestlers/ProWrestling_create.html', context)
+def prowrestler_details(request, pk):
+    prowrestler_detail = get_object_or_404(Wrestler, pk=pk)
+    return render(request, 'Prowrestlers/ProWrestling_details.html', {'prowrestler_detail': prowrestler_detail})
