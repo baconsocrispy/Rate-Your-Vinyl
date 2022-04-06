95e1f9385381644d408539ce498f3088ce901b69
Nathan Saxton <72226252+NathanSaxton@users.noreply.github.com>
2022-04-06 10:05:20 -0600

    Created the Heroes Class to be used for the db

--- AppBuilder9000/HeroAbility/models.py
+++ AppBuilder9000/HeroAbility/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+THREAT = { #establish threat levels
+    ('0', '0'),
+    ('1', '1'),
+    ('2', '2'),
+    ('3', '3'),
+    ('4', '4'),
+    ('5', '5'),
+}
+
+#set up the Heroes class for the db.
+class Heroes(models.Model):
+    alias = models.CharField(max_length=30, default="", blank=False, null=False)
+    secretIdentity = models.CharField(max_length=50, default="", blank=True, null=False)
+    abilityName = models.CharField(max_length=25, default="", blank=False, null=False)
+    abilitySummary = models.CharField(max_length=100, default="", blank=False, null=False)
+    weakness = models.CharField(max_length=25, default="", blank=True, null=False)
+    threatLevel = models.CharField(max_length=2, default="", choices=THREAT)
+
+    #assign a manager
+    heroes = models.Manager()
+
+    def __str__(self):
+        return self.alias
