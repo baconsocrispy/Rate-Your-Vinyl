3af4a33cc7547af2fc0c118762b71cb9c504428f
bpochon87 <83652607+bpochon87@users.noreply.github.com>
2022-04-06 16:45:23 -0600

    Story two completion pending project manager review.

--- AppBuilder9000/MTB_Trails/models.py
+++ AppBuilder9000/MTB_Trails/models.py
@@ -1,3 +1,55 @@
 from django.db import models
 
-# Create your models here.
+
+TRAIL_DIFFICULTY = [
+    ("1", "1 - Easy"),
+    ("2", "2 - Easy/Intermediate"),
+    ("3", "3 - Intermediate"),
+    ("4", "4 - Intermediate/Difficult"),
+    ("5", "5 - Difficult"),
+    ("6", "6 - Very Difficult"),
+]
+
+US_STATES = [
+    ("AL", "Alabama"), ("AK", "Alaska"), ("AZ", "Arizona"), ("AR", "Arkansas"),
+    ("CA", "California"), ("CO", "Colorado"), ("CT", "Connecticut"), ("DE", "Delaware"),
+    ("FL", "Florida"), ("GA", "Georgia"), ("HI", "Hawaii"), ("ID", "Idaho"),
+    ("IL", "Illinois"), ("IN", "Indiana"), ("IA", "Iowa"), ("KS", "Kansas"),
+    ("KY", "Kentucky"), ("LA", "Louisiana"), ("ME", "Maine"), ("MD", "Maryland"),
+    ("MA", "Massachusetts"), ("MI", "Michigan"), ("MN", "Minnesota"), ("MS", "Mississippi"),
+    ("MO", "Missouri"), ("MT", "Montana"), ("NE", "Nebraska"), ("NH", "New Hampshire"),
+    ("NJ", "New Jersey"), ("NM", "New Mexico"), ("NY", "New York"), ("NC", "North Carolina"),
+    ("ND", "North Dakota"), ("NV", "Nevada"), ("OH", "Ohio"), ("OK", "Oklahoma"),
+    ("OR", "Oregon"), ("PA", "Pennsylvania"), ("RI", "Rhode Island"), ("SC", "South Carolina"),
+    ("SD", "South Dakota"), ("TN", "Tennessee"), ("TX", "Texas"), ("UT", "Utah"),
+    ("VA", "Virginia"), ("VT", "Vermont"), ("WA", "Washington"), ("WV", "West Virginia"),
+    ("WI", "Wisconsin"), ("WY", "Wyoming"),
+]
+
+
+class ReviewTrail(models.Model):
+    trail_name = models.CharField(max_length=60)
+    nearest_city = models.CharField(max_length=60, blank=True)
+    state = models.CharField(max_length=60, choices=US_STATES)
+    review = models.TextField()
+    difficulty = models.CharField(max_length=60, choices=TRAIL_DIFFICULTY)
+
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.trail_name
+
+
+class CreateTrail(models.Model):
+    trail_name = models.CharField(max_length=60)
+    nearest_city = models.CharField(max_length=60, blank=True)
+    state = models.CharField(max_length=60, choices=US_STATES)
+    directions = models.TextField(blank=True, default="How do we get there?")
+    need_to_know = models.TextField(blank=True, default="Sketchy sections, flooded areas. Whatever"
+                                                              " fellow bikers need to know.")
+    difficulty = models.CharField(max_length=60, choices=TRAIL_DIFFICULTY)
+
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.trail_name
--- AppBuilder9000/MTB_Trails/views.py
+++ AppBuilder9000/MTB_Trails/views.py
@@ -1,5 +1,30 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from .forms import TrailReview
+from .models import ReviewTrail
+
 
 def mtb_trails_home(request):
     return render(request, "MTB_Trails/mtb_trails_home.html")
+
+
+# User data from form to be saved and then user redirected to submitted_review
+def mtb_trails_review(request):
+    # If this is a POST request we need to process the form data
+    form = TrailReview(data=request.POST or None)
+    if request.method == 'POST':
+        # Check if data is valid:
+        if form.is_valid():
+            form.save()
+            # Process data in form.cleaned_data as required
+            # Redirect to new URL
+            return redirect("submitted_review")
+    else:
+        form = TrailReview()
+    context = {'form': form}
+    return render(request, "MTB_Trails/mtb_trails_review.html", context)
+
+
+# Simple rendering for success review submission
+def submitted_review(request):
+    return render(request, "MTB_Trails/submitted_review.html")
