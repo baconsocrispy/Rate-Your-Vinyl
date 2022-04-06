5e343c708540a0adb124ce5ba024174beac4c284
Nathan Saxton <72226252+NathanSaxton@users.noreply.github.com>
2022-04-06 11:36:51 -0600

    Changed the THREAT set to a list for a uniform order on the page. Then changed the ability_Summary from a charfield to a textfield

--- AppBuilder9000/HeroAbility/models.py
+++ AppBuilder9000/HeroAbility/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
-THREAT = {  # establish threat levels
-    ('0', '0'),
-    ('1', '1'),
-    ('2', '2'),
-    ('3', '3'),
-    ('4', '4'),
-    ('5', '5'),
-}
+THREAT = [  # establish threat levels
+    ('0', 'Nonexistent'),
+    ('1', 'Mild Inconvenience'),
+    ('2', 'Minor Threat'),
+    ('3', 'Robin Equivalent'),
+    ('4', 'Major Threat'),
+    ('5', 'World Ending'),
+]
 
 
 # set up the Heroes class for the db.
@@ -15,7 +15,7 @@ class Heroes(models.Model):
     alias = models.CharField(max_length=30, default="", blank=False, null=False)
     secret_Identity = models.CharField(max_length=50, default="", blank=True, null=False)
     ability_Name = models.CharField(max_length=25, default="", blank=False, null=False)
-    ability_Summary = models.CharField(max_length=100, default="", blank=False, null=False)
+    ability_Summary = models.TextField(max_length=200, default="", blank=False, null=False)
     weakness = models.CharField(max_length=25, default="", blank=True, null=False)
     threat_Level = models.CharField(max_length=2, default="0", choices=THREAT)
 
