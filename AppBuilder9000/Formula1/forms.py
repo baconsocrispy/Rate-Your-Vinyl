981945b4701ae2983c917069053f8ba10662ba16
Ian Marshall <96323360+IMarshall@users.noreply.github.com>
2022-04-06 16:22:26 -0600

    Finished Story 2 - Added minimal styling to the Add Result Page. Will do more if I have time later.

--- AppBuilder9000/Formula1/forms.py
+++ AppBuilder9000/Formula1/forms.py
@@ -1,7 +1,31 @@
-from django.forms import ModelForm
+from django import forms
+from django.forms import ModelForm, Select, CheckboxInput
 from .models import Result
 
 class ResultForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['Driver_Name'].label = ''
+        self.fields['Driver_Name'].empty_label = 'Select a Driver'
+        self.fields['Race'].label = ''
+        self.fields['Race'].empty_label = 'Select a Race'
+        self.fields['Fastest_Lap'].label = 'Fastest Lap?'
+
+
     class Meta:
         model = Result
         fields = ['Driver_Name', 'Race', 'Finishing_Position', 'Fastest_Lap']
+        widgets = {
+            'Driver_Name': Select(attrs={
+                'class': 'form-dropdown'
+            }),
+            'Race': Select(attrs={
+                'class': 'form-dropdown'
+            }),
+            'Finishing_Position': Select(attrs={
+                'class': 'small-form-dropdown'
+            }),
+            'Fastest_Lap': CheckboxInput(attrs={
+                'class': 'form-checkbox'
+            }),
+        }
