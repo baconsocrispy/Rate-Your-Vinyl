82818ded238c6f2a59f878dbf5cc6a0d26c82cc0
bpochon87 <83652607+bpochon87@users.noreply.github.com>
2022-04-07 14:42:42 -0600

    Receiving 'raise exceptions.ValidationError( django.core.exceptions.ValidationError: ["'Select Date' value has an invalid date format. It must be in YYYY-MM-DD format."]'

--- AppBuilder9000/MTB_Trails/urls.py
+++ AppBuilder9000/MTB_Trails/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.mtb_trails_home, name='mtb_trails_home'),
     path('trails_review/', views.mtb_trails_review, name='mtb_trails_review'),
     path('submitted_review/', views.submitted_review, name='submitted_review'),
+    path('existing_reviews/', views.existing_reviews, name='existing_reviews'),
 ]
