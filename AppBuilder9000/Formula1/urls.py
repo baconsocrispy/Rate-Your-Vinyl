a4060b0dccae816a13ec4a2a342061df6fff3545
Ian Marshall <96323360+IMarshall@users.noreply.github.com>
2022-04-08 18:23:37 -0600

    Finished Story 3. Added Race results page with some minimal bootstrap styling. Added query with a few sort filters on the views page to display all results.

--- AppBuilder9000/Formula1/urls.py
+++ AppBuilder9000/Formula1/urls.py
@@ -5,6 +5,9 @@ from . import views
 urlpatterns = [
     path('', views.f1_home, name="f1_home"),
     path('add_result/', views.add_result, name="add_result"),
+    path('race_results/', views.race_results, name="race_results"),
+    path('driver_results/', views.driver_results, name="driver_results"),
+    path('team_results/', views.team_results, name="team_results"),
     path('result_submit/', views.result_submit, name="result_submit"),
     path('admin/', admin.site.urls),
 ]
