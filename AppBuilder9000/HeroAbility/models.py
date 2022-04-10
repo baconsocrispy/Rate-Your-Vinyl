c98a454bbfb7919c2030378e7c12e9e953eae997
Nathan Saxton <72226252+NathanSaxton@users.noreply.github.com>
2022-04-09 23:43:23 -0600

    removed the numbers as they didn't have any apparent meaning

--- AppBuilder9000/HeroAbility/models.py
+++ AppBuilder9000/HeroAbility/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
 THREAT = [  # establish threat levels
-    ('0', 'Nonexistent'),
-    ('1', 'Mild Inconvenience'),
-    ('2', 'Minor Threat'),
-    ('3', 'Robin Equivalent'),
-    ('4', 'Major Threat'),
-    ('5', 'World Ending'),
+    ('Nonexistent', 'Nonexistent'),
+    ('Mild Inconvenience', 'Mild Inconvenience'),
+    ('Minor Threat', 'Minor Threat'),
+    ('Robin Equivalent', 'Robin Equivalent'),
+    ('Major Threat', 'Major Threat'),
+    ('World Ending', 'World Ending'),
 ]
 
 
@@ -17,7 +17,7 @@ class Heroes(models.Model):
     ability_Name = models.CharField(max_length=25, default="", blank=False, null=False)
     ability_Summary = models.TextField(max_length=200, default="", blank=False, null=False)
     weakness = models.CharField(max_length=25, default="", blank=True, null=False)
-    threat_Level = models.CharField(max_length=2, default="0", choices=THREAT)
+    threat_Level = models.CharField(max_length=20, default="", choices=THREAT)
 
     # assign a manager
     heroes = models.Manager()
