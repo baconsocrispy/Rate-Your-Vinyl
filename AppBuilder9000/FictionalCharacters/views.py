79911f421fa39224740a0d7bf0288cd17a82b481
Fox <99509225+mfluder24@users.noreply.github.com>
2022-04-12 11:50:43 +1200

    Completed Story 6.5

--- AppBuilder9000/FictionalCharacters/views.py
+++ AppBuilder9000/FictionalCharacters/views.py
@@ -1,10 +1,11 @@
+from bs4 import BeautifulSoup
 from django.shortcuts import render, redirect
 from .forms import CharacterForm, SeriesForm
 from .models import Characters
 import requests
 import json
 
-# Create your views here.
+# Create a function to render home template
 def home(request):
     return render(request,
     'FictionalCharacters/FictionalCharacters_Home.html')
@@ -84,6 +85,7 @@ API will allow users to enter names of two characters they 'ship', and will retu
 calculation of how likely the relationship will succeed. Just for entertainment purposes.
 """
 
+# Create a function that collects user input, uses the API, and delivers input to results page
 def fc_calc(request):
     try:
         if request.method == 'POST':
@@ -96,11 +98,12 @@ def fc_calc(request):
                 'percent': int(res['percentage']),
                 'result': res['result'],
             }
-            return render(request, 'FictionalCharacters/FictionalCharacters_Results.html',context={'data':context})
+            return render(request, 'FictionalCharacters/FictionalCharacters_Results.html', context={'data': context})
         return render(request,'FictionalCharacters/FictionalCharacters_API.html')
     except:
         return render(request, 'FictionalCharacters/FictionalCharacters_Error.html')
 
+# Create a function to hold API code
 def fc_api(f, s):
     url = "https://love-calculator.p.rapidapi.com/getPercentage"
 
@@ -116,3 +119,29 @@ def fc_api(f, s):
     print(response.text)
 
     return test
+
+####### BEAUTIFUL SOUP CODE #######
+"""
+As noted below, additional code had to be added as elements 6 and 7 in chars array came back as '7.' and 'James Bond',
+which caused a visual disruption in the list. I joined the two elements, inserted them into the proper index in the array,
+and then deleted the un-joined elements to make the list proper.
+"""
+
+def fc_soup(request):
+    chars = []  # Character name list
+
+    page = requests.get("https://www.scrolldroll.com/most-famous-fictional-characters-of-all-time/")
+    soup = BeautifulSoup(page.content, 'html.parser')
+    rankings = soup.find_all('strong')
+
+    for i in rankings:
+        rank = i.text.strip()
+        chars.append(rank)
+    l = chars[6], chars[7]  # Grabs the elements that need to be joined.
+    fix = ' '.join(l)  # Joins the two elements and places them in a variable
+    chars.insert(6, fix)  # Inserts joined elements into array at appropriate index
+    del chars[8]  # Removes unnecessary element
+    del chars[7]  # Removes unnecessary element
+
+    context = {'chars': chars}
+    return render(request, 'FictionalCharacters/FictionalCharacters_Soup.html', context)
