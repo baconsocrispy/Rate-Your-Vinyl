41aa5f6e69130c40c57e139592fb5569757e56aa
Nathan Saxton <72226252+NathanSaxton@users.noreply.github.com>
2022-04-10 21:55:43 -0600

    added a function to call the details template

--- AppBuilder9000/HeroAbility/views.py
+++ AppBuilder9000/HeroAbility/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Heroes
 from .forms import HeroForm
 
@@ -11,7 +11,7 @@ def heroability_new_hero(request):
     form = HeroForm(request.POST or None)
     if form.is_valid():
         form.save()
-        return redirect('heroability_home')
+        return redirect('heroability_display_all')
     else:
         print(form.errors)
         form = HeroForm()
@@ -27,3 +27,22 @@ def heroability_display_all(request):
         'heroes': heroes,
     }
     return render(request, 'HeroAbility/heroability_display_all.html', content)
+
+
+def heroability_details(request, pk):
+    pk = int(pk)
+    hero = get_object_or_404(Heroes, pk=pk)
+    form = HeroForm(data=request.POST or None, instance=hero)
+    if request.method == 'POST':
+        if form.is_valid():
+            form2 = form.save(commit=False)
+            form2.save()
+            return redirect('heroability_display_all')
+        else:
+            print(form.errors)
+    else:
+        content = {
+            'form': form,
+            'hero': hero,
+        }
+        return render(request, 'HeroAbility/heroability_details.html', content)
