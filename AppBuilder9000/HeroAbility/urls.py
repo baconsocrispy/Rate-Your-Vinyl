648b80fe231d3da920225a147625bc9ae1cd163a
Nathan Saxton <72226252+NathanSaxton@users.noreply.github.com>
2022-04-12 16:30:14 -0600

    Added a new path for the confirm delete page in the urls.py

--- AppBuilder9000/HeroAbility/urls.py
+++ AppBuilder9000/HeroAbility/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('new_hero/', views.heroability_new_hero, name='heroability_new_hero'),
     path('records/', views.heroability_display_all, name='heroability_display_all'),
     path('<int:pk>/details/', views.heroability_details, name='heroability_details'),
+    path('confirm_delete/', views.heroability_confirm_delete, name='heroability_confirm_delete'),
 
 ]
