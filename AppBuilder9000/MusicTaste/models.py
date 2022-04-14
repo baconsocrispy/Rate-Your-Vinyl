a035bcba31322b7cecd051c8e9782f09f6165e38
KitKat <KathrynY@protonmail.com>
2022-04-13 20:38:45 -0600

    Completion of story #2

--- AppBuilder9000/MusicTaste/models.py
+++ AppBuilder9000/MusicTaste/models.py
@@ -9,14 +9,14 @@ Gender_options = {
 
 class User(models.Model):
     Name = models.CharField(max_length=40)
-    Age = models.IntegerField(default=0)
+    Age = models.IntegerField(default='')
     Gender = models.CharField(default="gender", max_length=40, choices=Gender_options)
     score = models.FloatField(default=0, max_length=4)
 
     Users = models.Manager()
 
     def __str__(self):
-        return self.name
+        return self.Name
 
 
 class ChooseUser(models.Model):
--- AppBuilder9000/MusicTaste/urls.py
+++ AppBuilder9000/MusicTaste/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.musictaste_home, name='MusicTaste_home'),
-    path('test/', views.test, name='test'),
+    path('test/', views.registerform, name='test'),
 ]
