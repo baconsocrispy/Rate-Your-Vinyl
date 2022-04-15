cf8ed0cb39e88fd3a01c55dcbdf4088c2181edc3
Mark Medrano <markumed1@hotmail.com>
2022-04-15 13:27:43 -1000

    commit for code review

--- AppBuilder9000/MuayThai/views.py
+++ AppBuilder9000/MuayThai/views.py
@@ -8,17 +8,46 @@ def Muay_Thai_Home(request):
 
 # calls template and accept the form inputs for adding to the db (the create part of it)
 def MuayThai_fighter_entry(request):
-    form = FighterForm(data=request.POST or None)
+    form = FighterForm(request.POST or None)
     if form.is_valid():
         form.save()
-        return redirect('MuayThai_home')
-
-    context = {'form': form}
-    return render(request, 'MuayThai/MuayThai_fighter_entry.html', context)
+        return redirect('DisplayFighters')
+    else:
+        print(form.errors)
+        form = FighterForm()
+    content = {
+        'form': form,
+    }
+    return render(request, 'MuayThai/MuayThai_fighter_entry.html', content)
 
 
 # Display fighter's names
-def DisplayFighters(request):
+def MuayThai_display_fighters(request):
     fighters = Fighter.Fighter.all()
-    context = {'fighters': fighters}
-    return render(request, 'MuayThai/MuayThai_fighters.html', context)
+    content = {
+        'fighters': fighters,
+    }
+    return render(request, 'MuayThai/MuayThai_display_fighters.html', content)
+
+
+# call the details template
+def MuayThai_fighters_details(request, pk):
+    pk = int(pk)
+    hero = get_object_or_404(Fighter, pk=pk)
+    form = FighterForm(data=request.POST or None, instance=hero)
+    if request.method == 'POST':
+        if form.is_valid():
+            form2 = form.save(commit=False)
+            form2.save()
+            return redirect('MuayThai_display_fighters')
+        else:
+            print(form.errors)
+    else:
+        content = {
+            'form': form,
+            'fighter': fighter,
+        }
+        return render(request, 'MuayThai_fighters_details.html', content)
+
+
+
