b3442eca88d3089b9b499269b7d200dd27580782
Mark Medrano <markumed1@hotmail.com>
2022-04-17 16:49:24 -1000

    updated and Commit for review

--- AppBuilder9000/MuayThai/urls.py
+++ AppBuilder9000/MuayThai/urls.py
@@ -12,8 +12,7 @@ urlpatterns = [
     path('', views.Muay_Thai_Home, name='MuayThai_home'),  # usable link to home page
     path('FighterEntry/', views.MuayThai_fighter_entry, name='MuayThai_fighter_entry'),  # usable link to create page
     path('DisplayFighters/', views.MuayThai_display_fighters, name='MuayThai_display_fighters'),  # link to display fighter's names
-    path('<int:pk>/details/', views.MuayThai_fighters_details, name='MuayThai_fighter_details'),  # link to detail
-    # of fighters
+    path('<int:pk>/details/', views.MuayThai_fighters_details, name='MuayThai_fighter_details'),  # link to detail of fighters
     path('<int:pk>/delete_fighter/', views.MuayThai_delete_fighter, name='MuayThai_delete'),
     path('confirm_delete/', views.MuayThai_delete, name='MuayThai_confirm_delete'),
 ]
