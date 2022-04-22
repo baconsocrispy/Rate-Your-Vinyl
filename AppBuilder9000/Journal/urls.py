4795449fe0f52699f05c06e004b0f60776c4e54f
Carson Cook <73494842+CarsonCookCode@users.noreply.github.com>
2022-04-21 17:46:34 -0700

    Save

--- AppBuilder9000/Journal/urls.py
+++ AppBuilder9000/Journal/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('read/', views.journal_read, name='journal_read'),
     path('<int:pk>/update/', views.journal_update, name='journal_update'),
     path('<int:pk>/delete/', views.journal_delete, name='journal_delete'),
+    path('api/', views.journal_api, name='journal_api'),
+    path('bs/', views.journal_bs, name='journal_bs'),
 ]
--- AppBuilder9000/Journal/views.py
+++ AppBuilder9000/Journal/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect,  get_object_or_404
 from .models import Entry
 from .forms import EntryForm
+import requests
+import json
+from bs4 import BeautifulSoup
 
 
 def journal_home(request):
@@ -8,7 +11,6 @@ def journal_home(request):
 
 
 def journal_create(request):
-    #branch
     form = EntryForm(data=request.POST or None)
     if request.method == 'POST':
         if form.is_valid():
@@ -42,3 +44,29 @@ def journal_delete(request, pk):
         return redirect('../../read')
     content = {'entry': entry}
     return render(request, 'Journal/journal_delete.html', content)
+
+
+def journal_api(request):
+    url = "https://yahoo-weather5.p.rapidapi.com/weather"
+
+    querystring = {"location": "portland", "format": "json", "u": "f"}
+
+    headers = {
+        "X-RapidAPI-Host": "yahoo-weather5.p.rapidapi.com",
+        "X-RapidAPI-Key": "aa3d3af1d8mshe07b323c70b5c20p174de6jsn68e2b0bf2a3b"
+    }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+
+    api_info = json.loads(response.text)
+    current_temperature = str(api_info["current_observation"]["condition"]["temperature"]) + ' \N{DEGREE SIGN}F'
+    content = {"current_temperature": current_temperature}
+    return render(request, 'Journal/journal_api.html', content)
+
+
+def journal_bs(request):
+    page = requests.get("https://www.crummy.com/software/BeautifulSoup/bs4/doc/")
+    soup = BeautifulSoup(page.content, 'html.parser')
+    print(soup)
+    content = {"soup": soup}
+    return render(request, 'Journal/journal_bs.html', content)
