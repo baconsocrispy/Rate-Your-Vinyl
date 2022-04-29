18c40a433bed6c45d0cc286f44c11da1fd8fc8c8
mrg3442 <101129274+mrg3442@users.noreply.github.com>
2022-04-29 19:14:03 -0400

    Started making model for pickup games and making html page for it

--- AppBuilder9000/Basketball/views.py
+++ AppBuilder9000/Basketball/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 def basketballHome(request):
     return render(request, 'Basketball/Basketball_home.html')
+
+
+def basketballPickup(request):
+    return render(request, 'Basketball/Basketball_Pickup.html')
