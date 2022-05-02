243c67bb3aa1a9a9ea4164a3cececc8f82138d3a
mrg3442 <101129274+mrg3442@users.noreply.github.com>
2022-05-02 14:48:05 -0400

    Created Basic App

--- AppBuilder9000/Basketball/views.py
+++ AppBuilder9000/Basketball/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponseRedirect
 from .forms import PickupForm
 from .models import Pickup
+from bs4 import BeautifulSoup
+from django.http import HttpResponseRedirect
 
 
 # Create your views here.
@@ -10,11 +12,32 @@ def basketballHome(request):
 
 
 def basketballPickup(request):
+    # displays page and Pickup model
     form = PickupForm(data=request.POST or None)
     posts = Pickup.games.all()
+    # if method is post saves form if valid
     if request.method == 'POST':
-        if form.is_valid():
-            form.save()
-    content = {'form': form, 'posts': posts}
+        form_valid = form.is_valid()
+        if form_valid:
+            instance = form.save(commit=False)
+            instance.save()
+            return redirect("Pickup_Games")
+    # else if 'GET' displays the page and the Pickup model
+    else:
+        content = {'form': form, 'posts': posts}
+        return render(request, 'Basketball/Basketball_Pickup.html', content)
 
-    return render(request, 'Basketball/Basketball_Pickup.html', content)
+
+def searchPickup(request):
+    #if request.method == 'POST':
+     #   searched = request.POST['searched']
+      #  posts = Pickup.games.filter(name__contains=searched)
+       # content = {'searched': searched, 'posts': posts}
+        #return render(request, 'Basketball/Basketball_Search.html', content)
+    #else:
+        #return render(request, 'Basketball/Basketball_Home.html')
+    searched = request.POST['searched']
+    posts = Pickup.games.filter(name__contains=searched)
+    content = {'posts': posts}
+    return render(request, 'Basketball/Basketball_Search.html', content)
+
