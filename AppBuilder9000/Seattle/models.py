28bd188bb8e1ee8924b1593f3ec35392ce7661b0
Gabriel <GABEHJ2001@GMAIL.COM>
2022-05-03 12:52:49 -0700

    trouble linking to home

--- AppBuilder9000/Seattle/models.py
+++ AppBuilder9000/Seattle/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+Place_Type = [
+    ('Food', 'Food'),
+    ('Hotel', 'Hotel'),
+    ('Parking', 'Parking'),
+    ('Parks', 'Parks'),
+    ('Attraction', 'Attraction')
+]
+
+class Places(models.Model):
+    category = models.CharField(max_length=50, choices=Place_Type, default='')
+    place = models.CharField(max_length=20, default='', blank=True, null=False)
+    desc = models.CharField(max_length=500, default='', blank=True, null=False)
+    duration = models.IntegerField(max_length=2, default='0', blank=True, null=False)
+
+    Places = models.Manager
+
+    def __str__(self):
+        return self.place
+
