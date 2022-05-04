4c094010ebc8be296b6ecffab5679beb8d651f1d
Gabriel <GABEHJ2001@GMAIL.COM>
2022-05-04 16:02:46 -0700

    done, just not styled very well...

--- AppBuilder9000/Seattle/models.py
+++ AppBuilder9000/Seattle/models.py
@@ -9,13 +9,11 @@ Place_Type = [
 ]
 
 class Places(models.Model):
-    category = models.CharField(max_length=50, choices=Place_Type, default='')
-    place = models.CharField(max_length=20, default='', blank=True, null=False)
-    desc = models.CharField(max_length=500, default='', blank=True, null=False)
-    duration = models.IntegerField(max_length=2, default='0', blank=True, null=False)
+    Category = models.CharField(max_length=50, choices=Place_Type, default='')
+    Place = models.CharField(max_length=20, default='', blank=True, null=False)
+    Description = models.CharField(max_length=500, default='', blank=True, null=False)
 
     Places = models.Manager
 
     def __str__(self):
         return self.place
-
