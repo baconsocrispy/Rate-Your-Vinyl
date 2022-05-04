0a2b4b97e545794cca0c303aa3c72300ac47affe
Gabriel <GABEHJ2001@GMAIL.COM>
2022-05-04 11:34:07 -0700

    nto displaying

--- AppBuilder9000/Seattle/urls.py
+++ AppBuilder9000/Seattle/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.seattle_home, name='seattle_home')
+    path('', views.seattle_home, name='seattle_home'),
+    path('create/', views.seattle_create, name='seattle_create'),
 ]
