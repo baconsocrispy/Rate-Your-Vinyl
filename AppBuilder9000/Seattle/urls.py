8a85bacd7171830ae0fc7fafb2c7b71135000f93
Gabriel <GABEHJ2001@GMAIL.COM>
2022-05-05 13:25:14 -0700

    bare-bones styling and finished displaying database

--- AppBuilder9000/Seattle/urls.py
+++ AppBuilder9000/Seattle/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.seattle_home, name='seattle_home'),
     path('create/', views.seattle_create, name='seattle_create'),
+    path('view/', views.seattle_view, name='seattle_view')
 ]
