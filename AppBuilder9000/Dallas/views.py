94509cf2b53a931d15f1697dc281b0a366b490cf
Connor Eliason <connor.eliason@gmail.com>
2022-05-06 09:06:01 -0600

    story 2

--- AppBuilder9000/Dallas/views.py
+++ AppBuilder9000/Dallas/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import DallasForm
 from .models import Dallas
 
@@ -10,5 +10,16 @@ def raffle(request):
     if request.method == 'POST':
         if form.is_valid():
             form.save()
+            return redirect('../list')
     context = {'form': form}
     return render(request, 'Dallas/Dallas_raffle.html', context)
+
+def DisplayRaffle(request):
+    raffle_list = Dallas.Dallas.all()
+    context = {'raffle_list': raffle_list}
+    return render(request, 'Dallas/Dallas_list.html', context)
+
+def RaffleDetails(request, pk):
+    item = get_object_or_404(Dallas, pk=pk)
+    context = {'item': item}
+    return render(request, 'Dallas/Dallas_details.html', context)
