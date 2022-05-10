9766632119a94d3da4a2cf2a6da00ac657403ade
TeknonTheou <99988665+TeknonTheou@users.noreply.github.com>
2022-05-10 18:54:07 -0500

    Create Model

--- AppBuilder9000/TexMex/urls.py
+++ AppBuilder9000/TexMex/urls.py
@@ -1,7 +1,15 @@
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls import include
+from django.contrib import admin
 from . import views
 from django.urls import path
 
 
 urlpatterns = [
-    path('', views.texmex_home, name='texmex_home')
+    path('', views.texmex_home, name='texmex_home'),
+    path('edit_recipe', views.edit_recipe, name="edit_recipe"),
+    path('<int:pk>/details/', views.details, name="details"),
+    path('<int:pk>/delete/', views.delete, name="delete"),
+    path('confirmdelete', views.confirmed, name="confirmed"),
+    path('createRecord', views.createRecord, name="createRecord"),
 ]
