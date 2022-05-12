f03851bb2bb3bb1c17b62c3a2bf65d71338c609d
TeknonTheou <99988665+TeknonTheou@users.noreply.github.com>
2022-05-12 17:01:40 -0500

    Display detail page

--- AppBuilder9000/TexMex/urls.py
+++ AppBuilder9000/TexMex/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('', views.texmex_home, name='texmex_home'),
     path('view_recipes', views.view_recipes, name="view_recipes"),
     path('<int:pk>/details/', views.details, name="details"),
+    path('<int,pk>/edit/', views.edit, name="edit"),
     path('<int:pk>/delete/', views.delete, name="delete"),
     path('confirmdelete', views.confirmed, name="confirmed"),
     path('createRecord', views.createRecord, name="createRecord"),
