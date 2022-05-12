c4036ca44dd7c9e73e4b45fd27daa50e14ba1bc7
JBowersock <jbowersock22@hotmail.com>
2022-05-11 20:28:07 -0400

    Story 9 Submission.

--- AppBuilder9000/StreetLeagueSkateboarding/models.py
+++ AppBuilder9000/StreetLeagueSkateboarding/models.py
@@ -14,3 +14,12 @@ class Skater(models.Model):
     # str() Method;
     def __str__(self):
         return self.first_name + '' + self.last_name
+
+class WeatherMoment(models.Model):
+    temperature = models.IntegerField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    WeatherMoments = models.Manager()
+
+    def __str__(self):
+        return self.date
