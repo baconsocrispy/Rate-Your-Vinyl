d4e4483dfabc0a9b312a21f3a917829f15640032
RomanPaladiy <96402218+RomanPaladiy@users.noreply.github.com>
2022-05-12 11:00:17 -0700

    Code Check

--- AppBuilder9000/CustomPcs/urls.py
+++ AppBuilder9000/CustomPcs/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from .forms import BuildForm
 
 urlpatterns = [
     path('', views.CustomPcs_home, name='CustomPcs_Home'),
+    path('create', views.BuildForm, name='BuildForm.html'),
 
 ]
--- AppBuilder9000/CustomPcs/views.py
+++ AppBuilder9000/CustomPcs/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Builds
+from .forms import BuildForm
 
 
-# Creating the view
+# Creating the views
 
 def CustomPcs_home(request):
     return render(request, 'CustomPcs/CustomPcs_home.html')
+
+
+def CreateEntry(request):
+    form = BuildForm(data=request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return redirect('CustomPcs_home')
+    else:
+        print(form.errors)
+        content = {'form': form}
+        return render(request, 'CustomPcs/BuildForm.html', content)
