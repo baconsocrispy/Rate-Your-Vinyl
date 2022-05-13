554405bdb84929837a79ee1fbd25989f411ae4bc
samjac0 <98563031+samjac0@users.noreply.github.com>
2022-05-12 23:08:51 -0700

    updates

--- AppBuilder9000/Jobs/views.py
+++ AppBuilder9000/Jobs/views.py
@@ -37,23 +37,11 @@ def childRoster(request):
     content = {'rosterList': rosterList}
     return render(request, 'Jobs/coachChildRoster.html', content)
 
-#def childRoster(request, pk):
-    #coach = account.accounts.filter(coach_Grade=pk)
-    #child = singupChild.signupChilds.filter(Grade=pk)
-    #table_contents = { }
-    #content = {'coach': coach, 'table_contents': table_contents, 'child': child}
-    #return render(request, 'Jobs/coachChildSignups.html', content)
-
-#def childRoster(request):
-    #cursor=connection.cursor()
-    #cursor.execute("select singupChild.grade,singupChild.child_Name,account.name from singupChild join account on singupChild.grade=account.coach_Grade")
-    #results=cursor.fetchall()
-    #return render(request,'childRoster.html',{'singupChild':results})
 
 def childDetails(request, pk):
-        details = get_object_or_404(Child, pk=pk)
-        content = {'details': details}
-        return render(request, 'Jobs/coachChildDetails.html', content)
+    details = get_object_or_404(Child, pk=pk)
+    content = {'details': details}
+    return render(request, 'Jobs/coachChildDetails.html', content)
 
 def childUpdate(request, pk):
     details = Child.Children.get(pk=pk)
@@ -64,14 +52,39 @@ def childUpdate(request, pk):
         return redirect('coachChildRoster')
 
     content = {'details': details, 'form': form}
-    return render(request, 'Jobs/coachUpdateChild', content)
+    return render(request, 'Jobs/coachUpdateChild.html')
 
 def childDelete(request, pk):
-    child = get_object_or_404(Child, pk=pk)
+    childForDelete = get_object_or_404(Child, pk=pk)
 
     if request.method == "POST":
-        child.delete()
+        childForDelete.delete()
         return redirect('coachChildRoster')
-    content = {'child': child}
+    content = {'childForDelete': childForDelete}
     return render(request, 'Jobs/coachChildDelete.html', content)
 
+#This code adds some kids to the roster of the roster is empty. Helpful for demo function!
+cFName = ['Aman', 'Vijay', 'Casey', 'Katelyn', 'Jessica', 'Chris', 'Lacy', 'Luis']
+cLName = ['Jones', 'Vitali', 'Adams', 'Flaherty', 'Jones', 'Cringle', 'Lewis', 'Lopez']
+cGrade = ['K-1st', 'K-1st', '2nd-3rd', '2nd-3rd', '4th-5th', '4th-5th', '6th-7th', '6th-7th']
+cJersey = [10,23,45,34,56,67,10,15]
+pName = ['Bob Jones', 'Pchenka Vitali', 'Phil Adams', 'Laura Flaherty', 'Jason Jones', 'Mrs. Cringle', 'Samantha Lewis']
+pPhone = [5033458567,5033678567,5033456798,5032345467,5031459689,5452456567,5034576833,5031234567]
+pEmail = ['Bob@gmail.com', 'Pchenka@gmail.com', 'Phil@gmail.com', 'Laura@gmail.com', 'Jason@gmail.com', 'naughtyornice@gmail.com', 'Lulu@gmail.com']
+notesOrAllergies = ['Bob@gmail.com', 'Pchenka@gmail.com', 'Phil@gmail.com', 'Laura@gmail.com', 'Jason@gmail.com', 'naughtyornice@gmail.com', 'Lulu@gmail.com']
+
+
+def addChildren(request, *args, **kwargs):
+    # Iterate through all the data items
+    for i in range(len(cFName)):
+        # Insert in the database
+        Child.children.create(First_Name=cFName[i], Last_Name=cLName[i], Child_Grade=cGrade[i])
+
+    # Getting all the stuff from database
+    query_results = Student.objects.all();
+
+    # Creating a dictionary to pass as an argument
+    content = {'query_results': query_results}
+
+    # Returning the rendered html
+    return render(request, "home.html", content)
