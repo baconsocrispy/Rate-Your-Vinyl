6e6ae663ebe8b1d8f7af04ca636f96ff1736615b
Markkam2003 <93402961+Markkam2003@users.noreply.github.com>
2022-05-13 17:11:03 -0700

    commit

--- AppBuilder9000/Oregon_City/views.py
+++ AppBuilder9000/Oregon_City/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Activity
 from .forms import ActivityForm
+import requests
+import json
+
 
 
 # Story #1: Build the basic app ---
@@ -58,4 +61,19 @@ def oregon_delete(request, pk):
     content = {'activity': activity}
     return render(request, 'Oregon_City/Oregon_delete.html', content)
 
+
+def oregon_api(request):
+    url = "https://yahoo-weather5.p.rapidapi.com/weather"
+    querystring = {"location": "oregon", "format": "json", "u": "f"}
+    headers = {
+        "X-RapidAPI-Host": "yahoo-weather5.p.rapidapi.com",
+        "X-RapidAPI-Key": "81272c5418msh25156274a12ae48p1c7bdajsn0ce401ff9166"
+    }
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    print(response.text)
+    api_info = json.loads(response.text)
+    temp_int = api_info["current_observation"]["condition"]["temperature"]
+    current_temperature = str(api_info["current_observation"]["condition"]["temperature"]) + ' \N{DEGREE SIGN}F'
+    content = {"current_temperature": current_temperature, "temp_int": temp_int}
+    return render(request, 'Oregon_City/Oregon_api.html', content)
 
