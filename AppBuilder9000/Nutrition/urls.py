02d10bdf8353931084eff10bad1d840cd2cb865a
etashji <99293073+etashji@users.noreply.github.com>
2022-05-18 16:04:15 -0400

    Create Page working, working on database connection.

--- AppBuilder9000/Nutrition/urls.py
+++ AppBuilder9000/Nutrition/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.Nutrition_Home, name = 'Nutrition_Home'),
+    path('Nutrition_create/', views.registerform, name = 'Nutrition_create')
 ]
--- AppBuilder9000/Nutrition/views.py
+++ AppBuilder9000/Nutrition/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+from .forms import UserForm
 
 def Nutrition_Home(request):
     return render(request, "Nutrition/Nutrition_Home.html")
+
+def create(request):
+    return render(request, "Nutrition/Nutrition_create.html")
+
+def registerform(request):
+    form = UserForm(data=request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return redirect('Nutrition_Home')
+    context = {'form':form}
+    return render(request, 'Nutrition/Nutrition_create.html', context)
