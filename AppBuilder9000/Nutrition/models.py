14b0624e273694d48769d1823d3ef85388058456
etashji <99293073+etashji@users.noreply.github.com>
2022-05-19 14:39:39 -0400

    Finished Create page. The create account button on the homepage does not work, but the one in the navbar does.

--- AppBuilder9000/Nutrition/models.py
+++ AppBuilder9000/Nutrition/models.py
@@ -19,50 +19,6 @@ class User(models.Model):
     Calories = models.IntegerField(default='')
     Protein = models.IntegerField(default='')
 
-    def get_BMI(self):
-        User.BMI = (User.Weight * 703)/(User.Height**2)
-
-    def get_FatPercent(self):
-        if User.Gender == 'Male':
-            User.FatPercent = (User.BMI * 1.39) + (User.Age * 0.16) - 19.34
-        else:
-            User.FatPercent = (User.BMI * 1.39) + (User.Age * 0.16) - 9
-
-    def get_Phase(self):
-        if User.BMI >= 25:
-            User.Phase = 'Fat Burning'
-        elif User.BMI < 25 & User.FatPercent > 15 & User.Gender == 'Male':
-            User.Phase = 'Shredding'
-        elif User.BMI < 25 & User.FatPercent > 20 & User.Gender == 'Female':
-            User.Phase = 'Shredding'
-        elif User.BMI < 25 & User.FatPercent <= 15 & User.Gender == 'Male':
-            User.Phase = 'Bulking'
-        elif User.BMI < 25 & User.FatPercent <= 20 & User.Gender == 'Female':
-            User.Phase = 'Bulking'
-
-    def get_Nutrition(self):
-        if User.Phase == 'Fat Burning' & User.Weight >= 220:
-            User.Calories = 2400
-            User.Protein = 180
-        elif User.Phase == 'Fat Burning' & User.Weight < 220 & User.Weight >= 200:
-            User.Calories = 2200
-            User.Protein = 165
-        elif User.Phase == 'Fat Burning' & User.Weight < 200 & User.Weight >= 180:
-            User.Calories = 2000
-            User.Protein = 150
-        elif User.Phase == 'Fat Burning' & User.Weight < 180 & User.Weight >= 160:
-            User.Calories = 1800
-            User.Protein = 135
-        elif User.Phase == 'Fat Burning' & User.Weight < 160:
-            User.Calories = 1600
-            User.Protein = 120
-        elif User.Phase == 'Shredding':
-            User.Calories = User.Weight * 12
-            User.Protein = User.Weight * 0.8
-        elif User.Phase == 'Bulking':
-            User.Calories = User.Weight * 15
-            User.Protein = User.Weight
-
     Users = models.Manager()
 
     def __str__(self):
