31dda404e558dee2ec042a6f59822c12e1e21a7c
Lawrence McCary <98392880+lmccary11@users.noreply.github.com>
2022-05-19 10:59:55 -0400

    Basic app

--- AppBuilder9000/FinancialEvaluator/views.py
+++ AppBuilder9000/FinancialEvaluator/views.py
@@ -5,4 +5,7 @@ from .models import Account
 def fe_Home(request):
     return render(request, 'FinancialEvaluator/fe_Home.html')
 
+def fe_Base(request):
+    return render(request, 'FinancialEvaluator/fe_Base.html')
+
 
