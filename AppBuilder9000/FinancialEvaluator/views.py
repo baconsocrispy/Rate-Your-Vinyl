0751b99cc05398fc4bc039a2d447ef8232da136d
Lawrence McCary <98392880+lmccary11@users.noreply.github.com>
2022-05-26 20:03:26 -0400

    Details Page

--- AppBuilder9000/FinancialEvaluator/views.py
+++ AppBuilder9000/FinancialEvaluator/views.py
@@ -19,7 +19,7 @@ def fe_Evaluation(request):
     if request.method == 'POST':
         if form.is_valid():
             form.save()
-            return redirect('fe_Home')
+            return redirect('fe_Read')
         else:
             print(form.errors)
     content = {'form': form}
@@ -36,11 +36,23 @@ def fe_Approach(request):
 def admin(request):
     return render(request, 'FinancialEvaluator/admin.html')
 
-def fe_Delete(request):
-    return render(request, 'FinancialEvaluator/fe_Delete.html')
+def fe_Delete(request, pk):
+    entry = get_object_or_404(Evaluation, pk=pk)
+    if request.method == 'POST':
+        entry.delete()
+        return redirect('fe_Read')
+    content = {'entry': entry}
+    return render(request, 'FinancialEvaluator/fe_Delete.html', content)
 
-def fe_Update(request):
-    return render(request, 'FinancialEvaluator/fe_Update.html')
+def fe_Update(request, pk):
+    entry = get_object_or_404(Evaluation, pk=pk)
+    form = EvaluationForm(data=request.POST or None, instance=entry)
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return redirect('fe_Read')
+    content = {'form': form, 'entry': entry}
+    return render(request, 'FinancialEvaluator/fe_Update.html', content)
 
 def fe_Details(request, pk):
     entry = get_object_or_404(Evaluation, pk=pk)
