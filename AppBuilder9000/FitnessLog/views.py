59f9fe5cf5d688976b5e5b733293011fed43d5a3
slharris1 <101232137+slharris1@users.noreply.github.com>
2022-05-29 12:49:05 -0400

    Completed user story 3

--- AppBuilder9000/FitnessLog/views.py
+++ AppBuilder9000/FitnessLog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import EntryForm
 from django.http import HttpResponse
+from .models import Entry
 
 
 # user story 1
@@ -8,7 +9,7 @@ def fitness_home(request):
     return render(request, 'FitnessLog/fitness_home.html')
 
 
-# user story 2
+# user story 2: Create and entry
 def create_entry(request):
     form = EntryForm(data=request.POST or None)
     if request.method == 'POST':
@@ -17,3 +18,10 @@ def create_entry(request):
             return redirect('fitness_home')
     content = {'form': form}
     return render(request, 'FitnessLog/fitness_entry_create.html', content)
+
+
+# user story 3: Display all items from the database
+def fitness_read(request):
+    entry = Entry.Entries.all()
+    content = {'entry': entry}
+    return render(request, 'FitnessLog/fitness_read.html', content)
