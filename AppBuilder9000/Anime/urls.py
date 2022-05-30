77a1a9f511b74fd14634a2f540a41b377b88508e
Heather Shultz <theheatherloop@gmail.com>
2022-05-30 18:01:22 -0500

    Created model, form, and a template to create a new item in database.

--- AppBuilder9000/Anime/urls.py
+++ AppBuilder9000/Anime/urls.py
@@ -1,10 +1,10 @@
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path
 from . import views
 
 
 urlpatterns = [
     path('', views.anime_home, name='anime_home'),
+    path('create/', views.create_anime, name='create_anime'),
 ]
 
-urlpatterns += staticfiles_urlpatterns()
+
--- AppBuilder9000/Anime/views.py
+++ AppBuilder9000/Anime/views.py
@@ -1,5 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Ani
+from .forms import AniForm
 
 
+# Story #1: Build the basic app -----------------------------------------------------
 def anime_home(request):
-    return render(request, 'anime/anime_home.html', {})
+    return render(request, 'anime/anime_home.html')
+
+
+# Story #2: Create your model ------------------------------------------------------
+def create_anime(request):
+    form = AniForm(data=request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return redirect('../anime_archive')
+    context = {'form': form}
+    return render(request, 'anime/create_anime.html', context)
+
+
+
+
+
+
