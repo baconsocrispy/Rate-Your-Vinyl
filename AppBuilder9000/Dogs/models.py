87a1ae86caecb827a463e41007d00853bf351a57
Ricardo  Lucero <96747085+RicardoLucero@users.noreply.github.com>
2022-06-02 17:42:38 -0700

    created models.py and forms.py with the create html page. re did some css for the home page and add for create page.

--- AppBuilder9000/Dogs/models.py
+++ AppBuilder9000/Dogs/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+#drop down menu for dog sizes
+DOG_SIZES = (
+    ('X-small', 'Xtra-Small'),
+    ('Small', 'Small'),
+    ('Medium', 'Medium'),
+    ('Large', 'Large'),
+    ('X-large', 'Xtra-Large')
+)
+
+class Dogs(models.Model):
+    name = models.CharField(max_length=60, null=False)
+    breed = models.CharField(max_length=40)
+    color = models.CharField(max_length=30)
+    coat = models.CharField(max_length=30)
+    size = models.CharField(max_length=20, choices=DOG_SIZES)
+
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.name
