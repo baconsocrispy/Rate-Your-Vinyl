b240c12a46018cecfeef8c3f8d2e174061164237
Ransom Cadorette <91761633+RansomDC@users.noreply.github.com>
2022-06-06 16:57:55 -0700

    Starting adding fields for the model.

--- AppBuilder9000/JobScraping/models.py
+++ AppBuilder9000/JobScraping/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+stackChoices = {
+    ('Full-Stack','Full-Stack'),
+    ('Front-End','Front-End'),
+    ('Back-End','Back-End'),
+    ('Unknown','Unknown'),
+}
+
+class Jobs(models.Model):
+    title = models.CharField(max_length=60, default="", blank=True)
+    stack = models.CharField(max_length=15, choices=stackChoices)
+
+    def __str__(self):
+        return self.title
