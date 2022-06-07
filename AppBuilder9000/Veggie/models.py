cbb64f9fe91bdaee63790cfd98159e42331dea55
Gil Furman <gilfurman88@gmail.com>
2022-06-07 19:37:31 -0400

    Story 2

--- AppBuilder9000/Veggie/models.py
+++ AppBuilder9000/Veggie/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Recipe(models.Model):
+    title_name = models.CharField(max_length=50)
+    author_name = models.CharField(max_length=50)
+    number_of_ingredients = models.IntegerField()
+    making_time = models.IntegerField()
+    description = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.title_name
