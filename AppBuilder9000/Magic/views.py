fdc5418294561a2d9e24385baf0c7726c4818939
emilyallen3 <emily.allen8715@gmail.com>
2022-06-08 01:15:53 -0600

    Changed comment form to a pop modal box.

--- AppBuilder9000/Magic/views.py
+++ AppBuilder9000/Magic/views.py
@@ -3,11 +3,15 @@ from .models import Deck, Comment
 from .forms import DeckForm, CommentForm
 
 #Story 1: Build the basic app
+
+
 def magic_home(request):
     return render(request, 'Magic/magic_home.html')
 
 
 #Story 2: Create a model
+
+
 def magic_create(request):
     form = DeckForm(data=request.POST or None)
     if request.method == 'POST':
@@ -18,6 +22,8 @@ def magic_create(request):
     return render(request, 'Magic/magic_create.html', content)
 
 #Story 3: Display items from database
+
+
 def magic_browse(request):
     deck = Deck.Deck.all()
     content = {
@@ -27,6 +33,8 @@ def magic_browse(request):
 
 
 #Story 4: Details Page
+
+
 def magic_details(request, pk):
     deck = get_object_or_404(Deck, pk=pk)
     form = CommentForm(data=request.POST or None)
@@ -34,10 +42,8 @@ def magic_details(request, pk):
         if form.is_valid():
             form.save()
             return redirect('../details')
-    comment = Comment.Comments.all()
     content = {
         'deck': deck,
-        'comment': comment,
         'form': form,
     }
     return render(request, 'Magic/magic_details.html', content)
