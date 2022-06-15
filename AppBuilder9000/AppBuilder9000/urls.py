69860abc49258f4558200b2b0355a54347e8f778
H-Grayson <92130199+H-Grayson@users.noreply.github.com>
2022-06-15 15:26:07 -0600

    Added Name: Hunter Grayson

--- AppBuilder9000/AppBuilder9000/urls.py
+++ AppBuilder9000/AppBuilder9000/urls.py
@@ -39,7 +39,8 @@ urlpatterns = [
     path('Magic/', include('Magic.urls')),
     path('JobScraping/', include('JobScraping.urls')),
     path('Veggie/', include('Veggie.urls')),
-    path('Crypto/', include('Crypto.urls'))
+    path('Crypto/', include('Crypto.urls')),
+    path('CFB/', include('CFB.urls')),
 ]
 
 #urlpatterns +=staticfiles_urlpatterns()
