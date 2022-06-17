2fd6849663a7f945f534fe46d2eb4c09c0d30fd2
DrePorter2 <deondre2016porter@gmail.com>
2022-06-17 13:18:34 -0700

    Display details

--- AppBuilder9000/Crypto/urls.py
+++ AppBuilder9000/Crypto/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.crypto_home, name='Crypto_Home'),
     path('AddCrypto/', views.crypto_addcrypto, name='crypto_add_crypto'),
     path('Ratings/', views.crypto_ratings, name='crypto_ratings'),
+    path('<int:pk>/Details/', views.crypto_details, name='crypto_details'),
 ]
