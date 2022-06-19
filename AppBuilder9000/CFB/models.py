ace95faf8165168c6c8dc97694bd665bd503591f
H-Grayson <92130199+H-Grayson@users.noreply.github.com>
2022-06-18 23:50:47 -0600

    Story 2

--- AppBuilder9000/CFB/models.py
+++ AppBuilder9000/CFB/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class AddFan(models.Model):
+    first_name = models.CharField(max_length=25, null=False)
+    last_name = models.CharField(max_length=25, null=False)
+    phone_number = models.CharField(max_length=11, null=False)
+    email = models.CharField(max_length=50, null=False)
+
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.first_name
