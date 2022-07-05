9a62bfa8aa968522632d24802c801b1651fda65b
brianurzua <98988561+brianurzua@users.noreply.github.com>
2022-07-05 09:44:17 -0700

    css for model and adding sneaker page

--- AppBuilder9000/Sneakers/views.py
+++ AppBuilder9000/Sneakers/views.py
@@ -10,9 +10,9 @@ def create_sneaker(request):
 	if request.method == 'POST':
 		if form.is_valid():
 			form.save()
-			return redirect('index')
+			return redirect('../')
 	content = {'form': form}
-	return render(request, "Sneakers/createSnkr.html", content)
+	return render(request, "Sneakers/Sneakers_createSnkr.html", content)
 	""" Pull all fields from account and puts inside variable form. Then checks if
 		request method is post and if it is it well redirect user to index page. 
 		Then sends them to create a new account"""
