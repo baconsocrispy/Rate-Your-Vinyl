bd33a8113a6b24fdc5b9bb7868246b9bb9a80efc
Tanuj Chowdhury <chowdhury.tanuj@gmail.com>
2022-07-05 22:40:49 -0700

    Added model, create page, and styling for Inventory app

--- AppBuilder9000/Inventory/views.py
+++ AppBuilder9000/Inventory/views.py
@@ -1,4 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Material
+from .forms import MaterialForm
 
 def inventory_home(request):
     return render(request, 'Inventory/Inventory_home.html')
+
+def inventory_create(request):
+    form = MaterialForm(data=request.POST or None)
+    if request.method == "POST":
+        if form.is_valid():
+            form.save()
+            return redirect('..')
+    content = {'form': form}
+    return render(request, 'Inventory/Inventory_create.html', content)
+
