216821e9d11900f58b8dfb17100ea7b9b1efd255
WMorf <wesmorford@outlook.com>
2022-07-06 16:49:06 -0700

    Added display page with paginator and included search bar

--- AppBuilder9000/DwarfFort/urls.py
+++ AppBuilder9000/DwarfFort/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.dfort_home, name='dfort_home'),
-    path('create/', views.dfort_create, name='dfort_create')
+    path('create/', views.dfort_create, name='dfort_create'),
+    path('display/', views.dfort_display, name='dfort_display'),
+    path('search/', views.dfort_search, name='dfort_search'),
 ]
--- AppBuilder9000/DwarfFort/views.py
+++ AppBuilder9000/DwarfFort/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.core.paginator import Paginator
 from .models import Fbeast
 from .forms import FbeastForm
 
@@ -13,5 +14,31 @@ def dfort_create(request):
         if form.is_valid():
             form.save()
             return redirect('dfort_home')
-    content = {'form': form}
-    return  render(request, 'DwarfFort/dfort_create.html', content)
+    context = {'form': form}
+    return  render(request, 'DwarfFort/dfort_create.html', context)
+
+# Story 3 - Display Database
+
+def dfort_display(request):
+    beast = Fbeast.Fbeasts.all().order_by("title")
+
+    # builds collection of models from beast and limits display to 5 at a time
+    paginator = Paginator(beast, 5)
+    page = request.GET.get('page')
+    beastpage = paginator.get_page(page)
+
+
+    context = {'beastpage' : beastpage}
+
+    return render(request, 'DwarfFort/dfort_display.html', context)
+
+# Display search results
+
+def dfort_search(request):
+    if request.method == "POST":
+        name = request.POST.get('usr_query')
+        beast = Fbeast.Fbeasts.filter(name=name)
+
+        context = { 'name' : name, 'beast' : beast}
+        return render(request, 'DwarfFort/dfort_search.html', context)
+
