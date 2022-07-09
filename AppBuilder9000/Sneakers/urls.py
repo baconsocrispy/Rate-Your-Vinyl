1b4f35d928c488955885f5d0e5882541db08fee7
brianurzua <98988561+brianurzua@users.noreply.github.com>
2022-07-08 18:06:15 -0700

    code for details page and styling of page.

--- AppBuilder9000/Sneakers/urls.py
+++ AppBuilder9000/Sneakers/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.Sneakers_home, name="Sneakers_home"),
     path('create/', views.create_sneaker, name='create'),
     path('current/', views.current_sneakers,name='current'),
+    path('sneakDetails/<shoe_id>/', views.sneakersDetails, name='Details'),
 ]
