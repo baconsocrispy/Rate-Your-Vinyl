cad2ee859471e64e14974edd64a5b317600b88bb
beatydaniel <106612152+beatydaniel@users.noreply.github.com>
2022-07-12 11:32:16 -0700

    Added Characters to the database for Marvel app Added CSS

--- AppBuilder9000/Marvel/models.py
+++ AppBuilder9000/Marvel/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+TYPE_CHOICES = {
+    ('Hero', 'Hero'),
+    ('Villain', 'Villain'),
+}
+
+
+class Character(models.Model):
+    type = models.CharField(max_length=60, choices=TYPE_CHOICES)
+    name = models.CharField(max_length=100, default="", blank=True, null=False)
+    description = models.TextField(max_length=300, default="", blank=True)
+    image = models.CharField(max_length=255, default='', blank=True)
+
+    object = models.Manager()
+
+    def __str__(self):
+        return self.name
--- AppBuilder9000/Marvel/urls.py
+++ AppBuilder9000/Marvel/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.marvel_home, name='marvel_home'),
+    path('create/', views.marvel_create, name='marvel_create'),
 ]
