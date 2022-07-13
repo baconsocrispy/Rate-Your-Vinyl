ceabd9b5f04a5b83ee6472f9855e2139761d83e4
julianne-murdock <98274277+julianne-murdock@users.noreply.github.com>
2022-07-13 03:24:31 -0400

    Story 1- Julianne Murdock

--- AppBuilder9000/Expense_Tracker/models.py
+++ AppBuilder9000/Expense_Tracker/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Account(models.Model):
+    firstname = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50, default="")
+
+    Accounts = models.Manager()
--- AppBuilder9000/Expense_Tracker/views.py
+++ AppBuilder9000/Expense_Tracker/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+
 def ET_Home(request):
-    return render(request, 'Expense_Tracker/ET_Home.html')
+	return render(request, 'Expense_Tracker/ET_Home.html')
+
 
 def ET_Account(request):
-    return render(request, 'Expense_Tracker/ET_Account.html')
+	return render(request, 'Expense_Tracker/ET_Account.html')
+
+
+def admin(request):
+	return render(request, 'Expense_Tracker/admin.html')
