7d907f9ffdc17641ba934f5e122a1a1246c9baa3
renatog33 <101902010+renatog33@users.noreply.github.com>
2022-07-14 14:05:59 -0700

    basic app commit

--- AppBuilder9000/Cryptocurrency/urls.py
+++ AppBuilder9000/Cryptocurrency/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.Cryptocurrency_home, name='Cryptocurrency_home'),
     path('AddReview/', views.cryptocurrency_addreview, name='Cryptocurrency_AddReview'),
     path('Reviews/', views.cryptocurrency_reviews, name='Cryptocurrency_Reviews'),
+    path('<int:pk>/Details/', views.cryptocurrency_details, name='Cryptocurrency_Details'),
 ]
