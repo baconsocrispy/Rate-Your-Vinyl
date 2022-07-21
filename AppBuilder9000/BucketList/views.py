59d330abc50d1df6ca715e004aa1d7456b56dedd
brynnpeirce <brynn98607@gmail.com>
2022-07-21 12:17:03 -0700

    Instructor checking code

--- AppBuilder9000/BucketList/views.py
+++ AppBuilder9000/BucketList/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from .models import bucketItem
+from . import views
 
 
 # renders homepage
-def home(request):
-    return render(request, "BucketList_home.html", {})
+def BucketList_home(request):
+    return render(request, "BucketList_home.html")
+
+# create page
+def BucketList_create(request):
+    return render(request, "BucketList_create.html")
+
