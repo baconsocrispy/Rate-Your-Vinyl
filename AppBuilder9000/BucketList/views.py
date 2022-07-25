60f0c83915a52582a87dcea12f1a3dd84be994b6
brynnpeirce <brynn98607@gmail.com>
2022-07-25 12:13:49 -0700

    details page added to show details of individual items in the database. Linked to List page

--- AppBuilder9000/BucketList/views.py
+++ AppBuilder9000/BucketList/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import bucketItem
 from . import views
 from .forms import BucketItemForm
@@ -19,6 +19,7 @@ def BucketList_create(request):
     content = {'form': form}
     return render(request, "BucketList/BucketList_create.html", content)
 
+
 def BucketList_list(request):
     queryset = bucketItem.objects.all()  # list of bucketlist items
     context = {
@@ -27,4 +28,7 @@ def BucketList_list(request):
     return render(request, "BucketList/BucketList_list.html", context)
 
 
-
+def BucketList_details(request, pk):
+    item = get_object_or_404(bucketItem, pk=pk)
+    content = {'item': item}
+    return render(request, "BucketList/BucketList_details.html", content)
