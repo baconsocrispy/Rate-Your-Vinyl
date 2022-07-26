bbf22155ab55555a9eeef0ec39925285bbcb10e1
baconsocrispy <68571497+baconsocrispy@users.noreply.github.com>
2022-07-26 14:40:06 -0700

    Added createRelease form and a page to add records to the database

--- AppBuilder9000/VinylCollection/views.py
+++ AppBuilder9000/VinylCollection/views.py
@@ -1,4 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ReleaseForm, ArtistForm
 
 def home(request):
     return render(request, 'VinylCollection/home.html')
+
+def create_release(request):
+    form = ReleaseForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('VinylCollection_home')
+    else:
+        print(form.errors)
+        form = ReleaseForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'VinylCollection/createRelease.html', context)
