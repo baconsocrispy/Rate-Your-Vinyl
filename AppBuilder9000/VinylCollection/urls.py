73dda65b1c50877081fc357705a6cfc472b00188
baconsocrispy <68571497+baconsocrispy@users.noreply.github.com>
2022-07-27 16:29:08 -0700

    Added details html and linked with views/urls

--- AppBuilder9000/VinylCollection/urls.py
+++ AppBuilder9000/VinylCollection/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='VinylCollection_home'),
     path('createRelease', views.create_release, name='createRelease'),
-    path('collection/', views.collection, name='collection')
+    path('collection/', views.collection, name='collection'),
+    path('<int:pk>/details/', views.details, name='details'),
 ]
