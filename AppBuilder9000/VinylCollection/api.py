5bf63ed88e445a840c1c20d6fd3c64d0d461b08b
baconsocrispy <68571497+baconsocrispy@users.noreply.github.com>
2022-07-28 15:40:52 -0700

    Linked app with Discogs API and created retrieve-and-add-album functionality

---
+++ AppBuilder9000/VinylCollection/api.py
@@ -0,0 +1,46 @@
+import requests
+import discogs_client
+
+# this is my user-generated developer token
+TOKEN = "DBiUmQgyHHBGmXsybviPrgmVVSJmMDdhpxBAGwle"
+
+# catalog number taken from spine of vinyl record
+catalog_number = "aotnlp016"
+
+# url structure to query database
+URL = "https://api.discogs.com/database/search?q=" + catalog_number + "&token=" + TOKEN
+
+# sends api request to discogs
+response = requests.get(URL)
+
+# converts request to json
+json = response.json()
+
+# gathers first release from query results
+release = json['results'][0]
+
+# access to key pieces of data
+print(release['title']) #string
+print(release['year']) #string
+print(release['label'][0]) #string
+print(release['genre']) #list
+print(release['style']) #list
+print(release['id']) #string
+print(release['country']) #string
+
+# accessing discogs client (application name + token required)
+d = discogs_client.Client('VinylApplication', user_token=TOKEN)
+
+# retrieves python release object from id gathered above
+release_object = d.release(int(release['id']))
+
+print(release_object.title)
+
+
+
+
+
+
+
+
+
--- AppBuilder9000/VinylCollection/views.py
+++ AppBuilder9000/VinylCollection/views.py
@@ -3,6 +3,11 @@ from django.views.generic.edit import UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .forms import ReleaseForm, ArtistForm
 from .models import Release, Artist
+import discogs_client
+import requests
+
+# my user token to access discogs API
+DISCOGS_TOKEN = "DBiUmQgyHHBGmXsybviPrgmVVSJmMDdhpxBAGwle"
 
 class ReleaseUpdateView(UpdateView):
     model = Release
@@ -16,19 +21,57 @@ class ReleaseDeleteView(DeleteView):
 def home(request):
     return render(request, 'VinylCollection/home.html')
 
+# Album catalog number entry form
+def add_album(request):
+    return render(request, 'VinylCollection/add_album.html')
+
+
+# displays album info from discogs to ensure it's correct
+def confirm_add(request):
+    cat_number = request.POST['cat_number']
+    release = get_release(cat_number)
+    blank_form = ReleaseForm()
+    release_form = populate_form(release, blank_form)
+    context = {
+        'release': release,
+        'form': release_form,
+    }
+    print(release)
+    return render(request, 'VinylCollection/confirm_add.html', context)
+
+#auto-populates release form with data pulled from discogs
+def populate_form(release, blank_form):
+    form = blank_form
+    form.fields['title'].initial = release['title']
+    form.fields['format'].initial = 'Vinyl'
+    form.fields['genre'].initial = " ".join(release['genre'])
+    form.fields['country'].initial = release['country']
+    form.fields['style'].initial = " ".join(release['style'])
+    form.fields['label'].initial = release['label'][0]
+    return form
+
+
+# pulls release from discogs api based on catalog number
+# and returns dictionary from parsed json
+def get_release(cat_number):
+    url = "https://api.discogs.com/database/search?q=" \
+          + cat_number + "&token=" + DISCOGS_TOKEN
+    response = requests.get(url)
+    json = response.json()
+    release = json['results'][0]
+    return release
+
+# saves releases to database
 def create_release(request):
     form = ReleaseForm(request.POST or None)
     if form.is_valid():
         form.save()
-        return redirect('VinylCollection_home')
+        return redirect('collection')
     else:
         print(form.errors)
-        form = ReleaseForm()
-    context = {
-        'form': form,
-    }
-    return render(request, 'VinylCollection/createRelease.html', context)
+    return render(request, 'VinylCollection/add_album.html')
 
+# displays all releases in the collection
 def collection(request):
     releases = Release.objects.all()
     context = {
@@ -36,6 +79,7 @@ def collection(request):
     }
     return render(request, 'VinylCollection/collection.html', context)
 
+# displays individual release information
 def details(request, pk):
     pk = int(pk)
     release = get_object_or_404(Release, pk=pk)
