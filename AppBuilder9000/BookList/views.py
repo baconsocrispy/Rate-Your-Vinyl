59a5a7a44874046133a6732f98a2eddc1b732f01
Topaz557 <andreww9718@gmail.com>
2022-07-28 14:57:28 -0700

    Third Commit, functioning database checked with DBBrowser

--- AppBuilder9000/BookList/views.py
+++ AppBuilder9000/BookList/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render, redirect,  get_object_or_404
-
-
+from .forms import EntryForm
+from .models import BookEntry
 
 
 # Story #1: Build the basic app ----------------------------------------------------------------------------------------
 
 def BookList_Home(request):
     return render(request, 'BookList/BookList_Home.html')
+
+
+# Story #2: Create your model ------------------------------------------------------------------------------------------
+
+def Book_Entry(request):
+    form = EntryForm(data=request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return redirect('BookList_Home')
+    content = {'form': form}
+    return render(request, 'BookList/BookList_Create.html', content)
